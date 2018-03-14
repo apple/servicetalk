b438e475ae48d1a4818be7b0df36c3406d1d0c6a
Scott Mitchell <scott_mitchell@apple.com>
2018-03-13 17:53:34 -0700

    Move code from mono repo
    Co-authored-by: Nitesh Kant <nitesh_kant@apple.com>

---
+++ src/main/java/io/servicetalk/concurrent/Cancellable.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.concurrent;
+
+/**
+ * An entity that can be cancelled. <p>
+ * Cancellations are a hint from a consumer of any asynchronous result to the producer that it is no more interested in the result.
+ * ServiceTalk does not provide any guarantees that at a certain time, whether the asynchronous execution is cancellable or not.
+ * It is up to the producer of data to take any action on cancellation or ignore the same.
+ * Thus, a consumer of the result must <em>not</em> assume that it will not receive any callback post cancellation i.e. after {@link #cancel()} returns.
+ */
+public interface Cancellable {
+
+    /**
+     * A {@code no-op} instance of {@link Cancellable}.
+     */
+    Cancellable IGNORE_CANCEL = () -> { };
+
+    /**
+     * Sends a hint to the producer of the associated asynchronous execution that the consumer related to this {@code Cancellable} is not interested in the outcome of the execution.
+     */
+    void cancel();
+}
---
+++ src/main/java/io/servicetalk/concurrent/Completable.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.concurrent;
+
+import org.reactivestreams.Publisher;
+
+/**
+ * An asynchronous computation that does not emit any data. It just completes or emits an error.
+ */
+public interface Completable {
+
+    /**
+     * Subscribes to the outcome of this {@code Completable}.
+     *
+     * @param subscriber of the outcome.
+     * @see Publisher#subscribe(org.reactivestreams.Subscriber)
+     */
+    void subscribe(Subscriber subscriber);
+
+    /**
+     * Subscriber of the outcome of a {@link Cancellable}.
+     * <p>
+     * The semantics and threading model of this interface is meant to be the same as {@link org.reactivestreams.Subscriber},
+     * but simplified for the use case where the operations completes or fails with no data.
+     */
+    interface Subscriber {
+        /**
+         * Called when the associated {@link Completable} is subscribed via {@link Completable#subscribe(Subscriber)}.
+         * @param cancellable A {@link Cancellable} that can be used to cancel the asynchronous computation for
+         * this subscriber.
+         */
+        void onSubscribe(Cancellable cancellable);
+
+        /**
+         * Success terminal state.
+         * <p>
+         * No further events will be sent.
+         */
+        void onComplete();
+
+        /**
+         * Failed terminal state.
+         * <p>
+         * No further events will be sent.
+         *
+         * @param t the throwable signaled
+         */
+        void onError(Throwable t);
+    }
+
+    /**
+     * An entity that is both {@link Completable} and {@link Subscriber}.
+     * This is same as {@link org.reactivestreams.Processor} but for {@link Completable}s.
+     */
+    interface Processor extends Completable, Subscriber {
+    }
+}
