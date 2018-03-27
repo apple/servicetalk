ac0e198f4a90816c50db69408cc41cfa8ebd08c7
Idel Pivnitskiy <ipivnitskiy@apple.com>
2018-03-27 16:54:07 -0700

    Reorganize imports
    Motivation:
    
    We enforced a new checkstyle rule, that all `io.servicetalk` imports should be on top, otherwise build fails.
    
    Modifications:
    
    - Regenerate Intellij IDEA project and run Code -> Optimize imports;
    
    Result:
    
    Correct imports ordering.

--- src/main/java/io/servicetalk/annotations/ElementsAreNonnullByDefault.java
+++ src/main/java/io/servicetalk/annotations/ElementsAreNonnullByDefault.java
@@ -20,7 +20,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.meta.TypeQualifierDefault;
