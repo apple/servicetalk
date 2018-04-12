7a4f35c25e1349d74d7820f854f443dbed974c8f
Scott Mitchell <scott_mitchell@apple.com>
2018-04-12 09:00:15 -0700

    Introduce DelayedSubscription and DelayedCancellable
    Motivation:
    We have SequentialSubscription and SequentialCancellable, but these use cases expect continuous switching between objects. In the case of SequentialSubscription there are additional methods which may be ignored in some situations, but not all, and it isn't always clear when (itemReceived). There exists use cases where we don't need the continuous chaining of objects, but instead just a delayed setting of the object. In this case a Delayed variant can provide better semantics and potentially an optimized implementation.
    
    Motivation:
    - Introduce DelayedSubscription
    - Introduce DelayedCancellable
    
    Result:
    DelayedSubscription and DelayedCancellable exist.

---
+++ src/test/java/io/servicetalk/concurrent/internal/DelayedCancellableTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.concurrent.internal;
+
+import io.servicetalk.concurrent.Cancellable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
+import static io.servicetalk.concurrent.internal.ServiceTalkTestTimeout.DEFAULT_TIMEOUT_SECONDS;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class DelayedCancellableTest {
+    @Rule
+    public final ServiceTalkTestTimeout timeout = new ServiceTalkTestTimeout();
+
+    private final DelayedCancellable delayedCancellable = new DelayedCancellable();
+    private Cancellable c1;
+    private Cancellable c2;
+    private ExecutorService executor;
+
+    @Before
+    public void setup() {
+        c1 = mock(Cancellable.class);
+        c2 = mock(Cancellable.class);
+        executor = newCachedThreadPool();
+    }
+
+    @After
+    public void tearDown() throws InterruptedException {
+        executor.shutdown();
+        executor.awaitTermination(DEFAULT_TIMEOUT_SECONDS, SECONDS);
+    }
+
+    @Test
+    public void multipleDelayedSubscriptionCancels() {
+        delayedCancellable.setDelayedCancellable(c1);
+        delayedCancellable.setDelayedCancellable(c2);
+        verifyNoMoreInteractions(c1);
+        verify(c2).cancel();
+    }
+
+    @Test
+    public void delayedCancelIsDelivered() {
+        delayedCancellable.cancel();
+        delayedCancellable.setDelayedCancellable(c1);
+        verify(c1).cancel();
+    }
+
+    @Test
+    public void signalsAfterDelayedArePassedThrough() {
+        delayedCancellable.setDelayedCancellable(c1);
+        delayedCancellable.cancel();
+        verify(c1).cancel();
+    }
+
+    @Test
+    public void setDelayedFromAnotherThreadIsVisible() throws ExecutionException, InterruptedException {
+        delayedCancellable.cancel();
+        executor.submit(() -> {
+            delayedCancellable.setDelayedCancellable(c1);
+        }).get();
+        verify(c1).cancel();
+    }
+}
