821b4ef2c8544ef76c13401f257f4ae17fa47508
Jo Voordeckers <jvoordeckers@apple.com>
2018-04-25 10:42:56 -0700

    Splice and flatten heterogeneous streams with meta
    * Splice and flatten heterogeneous streams with meta
    
    __Motivation__
    
    HTTP Client and Server need a way to group and  flatten a heterogeneous
    stream of objects fronted by a metadata object.
    
    __Modifications__
    
    New operator that can splice and flatten streams.
    
    __Result__
    
    HTTP Client and Server now can combine or flatten
    objects from the HttpObjectEncoder/Decoder.

---
+++ src/main/java/io/servicetalk/http/netty/SpliceFlatStreamToMetaSingle.java
@@ -0,0 +1,258 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.concurrent.api.Executor;
+import io.servicetalk.concurrent.api.Publisher;
+import io.servicetalk.concurrent.api.Single;
+import io.servicetalk.concurrent.internal.ConcurrentSubscription;
+
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static io.servicetalk.concurrent.internal.EmptySubscription.EMPTY_SUBSCRIPTION;
+import static io.servicetalk.concurrent.internal.ThrowableUtil.unknownStackTrace;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * This class is responsible for splicing a {@link Publisher}&lt;{@link Object}&gt; with a common {@code Payload}
+ * into a {@code Data}&lt;{@code Payload}&gt; eg. {@code HttpResponse}&lt;{@code HttpPayloadChunk}&gt;.
+ *
+ * @param <Data> type of container, eg. {@code HttpResponse}&lt;{@code HttpPayloadChunk}&gt;
+ * @param <MetaData> type of meta-data in front of the stream of {@code Payload}, eg. {@code HttpResponseMetaData}
+ * @param <Payload> type of payload inside the {@code Data}, eg. {@code HttpPayloadChunk}
+ */
+final class SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> extends Single<Data> {
+
+    private final Executor executor;
+    private final BiFunction<MetaData, Publisher<Payload>, Data> packer;
+    private final Publisher<?> original;
+
+    /**
+     * Operator splicing a {@link Publisher}&lt;{@link Object}&gt; with a common {@code Payload} and {@code
+     * MetaData} header as first element into a {@code Data}&lt;{@code Payload}&gt; eg. {@code
+     * HttpResponse}&lt;{@code HttpPayloadChunk}&gt;.
+     *
+     * @param executor the executor to use for the new {@link Publisher}
+     * @param original the stream of {@link Object}s to splice in a {@code Data}&lt;{@code Payload}&gt;
+     * @param packer function to pack the {@link Publisher}&lt;{@code Payload}&gt; and {@code MetaData} into a
+     * {@code Data}
+     */
+    SpliceFlatStreamToMetaSingle(Executor executor, Publisher<?> original,
+                                 BiFunction<MetaData, Publisher<Payload>, Data> packer) {
+        this.executor = requireNonNull(executor);
+        this.packer = requireNonNull(packer);
+        this.original = requireNonNull(original);
+    }
+
+    @Override
+    protected void handleSubscribe(Single.Subscriber<? super Data> dataSubscriber) {
+        original.subscribe(new SplicingSubscriber<>(this, dataSubscriber));
+    }
+
+    /**
+     * Operator to flatten a {@code Data}&lt;{@code Payload}&gt; into a {@link Publisher}&lt;{@link Object}&gt;.
+     *
+     * @param executor the executor to use for the new {@link Publisher}
+     * @param data object containing a {@link Publisher}&lt;{@code Payload}&gt;
+     * @param unpack function to unpack the {@link Publisher}&lt;{@code Payload}&gt; from the container
+     * @param <Data> type of container, eg. {@code HttpResponse}&lt;{@code HttpPayloadChunk}&gt;
+     * @param <Payload> type of payload inside the {@code Data}, eg. {@code HttpPayloadChunk}
+     * @return a flattened {@link Publisher}&lt;{@link Object}&gt;
+     */
+    @SuppressWarnings("unchecked")
+    static <Data, Payload> Publisher<Object> flatten(Executor executor, Data data,
+                                                     Function<Data, Publisher<Payload>> unpack) {
+        return ((Publisher) Publisher.just(data, executor)).concatWith(unpack.apply(data));
+    }
+
+    /* Visible for testing */
+    static final class SplicingSubscriber<Data, MetaData, Payload> implements org.reactivestreams.Subscriber<Object> {
+
+        private static final AtomicReferenceFieldUpdater<SplicingSubscriber, Object>
+                maybePayloadSubUpdater = AtomicReferenceFieldUpdater.newUpdater(SplicingSubscriber.class,
+                Object.class, "maybePayloadSub");
+
+        /* Visible for testing */
+        static final Throwable CANCELED = unknownStackTrace(
+                new CancellationException("Canceled prematurely from Data"),
+                SplicingSubscriber.class, "cancelData(..)");
+        private static final Object COMPLETED = new Object();
+
+        /**
+         * A field that assumes various types and states depending on the state of the operator.
+         * <p>
+         * One of <ul>
+         *     <li>{@code null} – initial pending state before the {@link Single} is completed</li>
+         *     <li>{@link org.reactivestreams.Subscriber}&lt;{@code Payload}&gt; - when subscribed to the payload</li>
+         *     <li>{@link #CANCELED} - when the {@link Single} is canceled prematurely</li>
+         *     <li>{@link #COMPLETED} - when the stream completed prematurely (empty) payload</li>
+         *     <li>{@link Throwable} - the error that occurred in the stream</li>
+         * </ul>
+         */
+        @Nullable
+        @SuppressWarnings({"unused", "unchecked"})
+        private volatile Object maybePayloadSub;
+
+        /**
+         * Once a {@link #maybePayloadSub} is set to a {@link org.reactivestreams.Subscriber} we cache a copy in a
+         * non-volatile field to allow caching in register and avoid instanceof and casting on the hot path.
+         */
+        @Nullable
+        private org.reactivestreams.Subscriber<Payload> payloadSubscriber;
+
+        /**
+         * Indicates whether the meta-data has been observed.
+         */
+        private boolean metaSeenInOnNext;
+
+        /**
+         * Stores the upstream {@link Subscription} wrapped in a {@link ConcurrentSubscription} here until the
+         * {@link Single} completes and no longer expect concurrency, so we can use the {@link #rawSubscription}.
+         */
+        @Nullable
+        private Subscription concurrentSubscription;
+
+        /**
+         * The {@link Subscription} before wrapping to pass it to the downstream {@link org.reactivestreams.Subscriber}.
+         * Doesn't need to be {@code volatile}, as it should be visible wrt JMM according to
+         * <a href="https://github.com/reactive-streams/reactive-streams-jvm/blob/v1.0.2/README.md#2.11>RS spec 2.11</a>
+         */
+        @Nullable
+        private Subscription rawSubscription;
+
+        private final SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> parent;
+        private final Subscriber<? super Data> dataSubscriber;
+
+        /**
+         * This operator subscribes to one stream and forks into a {@link Single} and {@link Publisher}, so
+         * the only risk for concurrently accessing the upstream {@link Subscription} is when the {@link Single}
+         * gets canceled after the contained {@link Publisher} is subscribed. To avoid this problem we guard it with
+         * a CAS on {@link Single} termination.
+         *
+         * @param parent reference to the parent class holding immutable state
+         * @param dataSubscriber {@link Subscriber} to the {@code Data}
+         */
+        private SplicingSubscriber(SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> parent,
+                                   Subscriber<? super Data> dataSubscriber) {
+            this.parent = parent;
+            this.dataSubscriber = dataSubscriber;
+        }
+
+        /**
+         * This cancels the {@link Subscription} of the upstream {@link Publisher}&lt;{@link Object}&gt; unless this
+         * {@link Single} has already terminated.
+         * <p>
+         * Guarded by the CAS to avoid concurrency with the {@link Subscription} on the contained {@link
+         * Publisher}&lt;{@code Payload}&gt;
+         */
+        private void cancelData() {
+            if (maybePayloadSubUpdater.compareAndSet(this, null, CANCELED)) {
+                concurrentSubscription.cancel();
+            }
+        }
+
+        @Override
+        public void onSubscribe(Subscription inStreamSubscription) {
+            rawSubscription = inStreamSubscription;
+            // Prevent concurrency between request() and cancel()
+            concurrentSubscription = ConcurrentSubscription.wrap(inStreamSubscription);
+            // onSubscribe() before request() to avoid synchronous onNext() execution
+            dataSubscriber.onSubscribe(this::cancelData);
+            // get the first element a MetaData that we consume to complete the Single<Data>
+            concurrentSubscription.request(1);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onNext(Object obj) {
+            if (metaSeenInOnNext) {
+                Payload payload = (Payload) obj;
+                if (payloadSubscriber != null) {
+                    payloadSubscriber.onNext(payload);
+                } else {
+                    Object subscriber = maybePayloadSub;
+                    if (subscriber instanceof org.reactivestreams.Subscriber) {
+                        payloadSubscriber = ((org.reactivestreams.Subscriber<Payload>) subscriber);
+                        payloadSubscriber.onNext(payload);
+                    }
+                }
+            } else {
+                MetaData meta = (MetaData) obj;
+                metaSeenInOnNext = true;
+                Publisher<Payload> payloadPublisher = newPayloadPublisher();
+                Data data = parent.packer.apply(meta, payloadPublisher);
+                dataSubscriber.onSuccess(data);
+            }
+        }
+
+        @Nonnull
+        private Publisher<Payload> newPayloadPublisher() {
+            return new Publisher<Payload>(parent.executor) {
+                @Override
+                protected void handleSubscribe(org.reactivestreams.Subscriber<? super Payload> newSubscriber) {
+                    if (maybePayloadSubUpdater.compareAndSet(SplicingSubscriber.this, null, newSubscriber)) {
+                        // TODO risk of a race here with terminal events, will be addressed in follow-up PR
+                        newSubscriber.onSubscribe(rawSubscription);
+                    } else {
+                        Object maybeSubscriber = SplicingSubscriber.this.maybePayloadSub;
+                        newSubscriber.onSubscribe(EMPTY_SUBSCRIPTION);
+                        if (maybeSubscriber == COMPLETED) { // Prematurely completed (header + empty payload)
+                            newSubscriber.onComplete();
+                        } else if (maybeSubscriber instanceof Throwable) { // Premature error or cancel
+                            newSubscriber.onError((Throwable) maybeSubscriber);
+                        } else {
+                            newSubscriber.onError(new IllegalStateException("Duplicate Subscribers are not allowed. " +
+                                    "Existing: " + maybeSubscriber + ", new: " + newSubscriber));
+                        }
+                    }
+                }
+            };
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onError(Throwable t) {
+            if (!maybePayloadSubUpdater.compareAndSet(this, null, t)) {
+                Object maybeSubscriber = maybePayloadSub;
+                if (maybeSubscriber == CANCELED) {
+                    dataSubscriber.onError(t);
+                } else {
+                    ((org.reactivestreams.Subscriber<Payload>) maybeSubscriber).onError(t);
+                }
+            } else {
+                dataSubscriber.onError(t);
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onComplete() {
+            final Object maybeSubscriber = maybePayloadSubUpdater.getAndSet(this, COMPLETED);
+            if (maybeSubscriber instanceof org.reactivestreams.Subscriber) {
+                ((org.reactivestreams.Subscriber<Payload>) maybeSubscriber).onComplete();
+            } else if (!metaSeenInOnNext) {
+                dataSubscriber.onError(new IllegalStateException("Empty stream"));
+            }
+        }
+    }
+}
---
+++ src/test/java/io/servicetalk/http/netty/SpliceFlatStreamToMetaSingleTest.java
@@ -0,0 +1,248 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.concurrent.api.Executor;
+import io.servicetalk.concurrent.api.MockedSingleListenerRule;
+import io.servicetalk.concurrent.api.MockedSubscriberRule;
+import io.servicetalk.concurrent.api.Publisher;
+import io.servicetalk.concurrent.api.PublisherRule;
+import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static io.servicetalk.concurrent.api.DeliberateException.DELIBERATE_EXCEPTION;
+import static io.servicetalk.concurrent.api.Executors.immediate;
+import static io.servicetalk.concurrent.api.Publisher.from;
+import static io.servicetalk.concurrent.internal.Await.awaitIndefinitely;
+import static io.servicetalk.http.netty.SpliceFlatStreamToMetaSingle.flatten;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class SpliceFlatStreamToMetaSingleTest {
+
+    public static final Executor EXECUTOR = immediate();
+    private final MetaData metaData = new MetaData("foo");
+    private final Payload one = new Payload();
+    private final Payload two = new Payload();
+    private final LastPayload last = new LastPayload();
+    private final Data data = new Data(metaData, from(immediate(), one, two, last));
+
+    @Rule
+    public final ServiceTalkTestTimeout timeout = new ServiceTalkTestTimeout();
+
+    @Rule
+    public final PublisherRule<Object> upstream = new PublisherRule<>();
+
+    @Rule
+    public final MockedSubscriberRule<Payload> payloadSubscriber = new MockedSubscriberRule<>();
+
+    @Rule
+    public final MockedSubscriberRule<Payload> dupePayloadSubscriber = new MockedSubscriberRule<>();
+
+    @Rule
+    public final MockedSingleListenerRule<Data> dataSubscriber = new MockedSingleListenerRule<>();
+
+    @Test
+    public void flattenShouldBreakDownGroupWithEmbeddedPayloadIntoFlatHeterogeneousPublisherOfObject()
+            throws ExecutionException, InterruptedException {
+        List<Object> flattened = awaitIndefinitely(flatten(immediate(), data, Data::getPayload));
+        assertThat(flattened, contains(data, one, two, last));
+    }
+
+    @Test
+    public void streamWithHeaderAndPayloadShouldProduceDataWithEmbeddedPayload() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        payloadSubscriber.subscribe(data.getPayload());
+        payloadSubscriber.request(2);
+        upstream.sendItems(one, last);
+        upstream.complete();
+        payloadSubscriber.verifySuccess(one, last);
+    }
+
+    @Test
+    public void streamWithHeaderAndEmptyPayloadShouldCompleteOnPublisherOnSubscribe()
+            throws ExecutionException, InterruptedException {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        upstream.complete();
+        payloadSubscriber.subscribe(data.getPayload());
+        assertThat(awaitIndefinitely(data.getPayload()), empty());
+    }
+
+    @Test
+    public void emptyStreamShouldCompleteDataWithError() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.complete();
+        dataSubscriber.verifyFailure(IllegalStateException.class);
+    }
+
+    // We want to provide visibility to the user when data is available rather than dropping it.
+    @Test
+    public void cancelDataAfterDataCompleteShouldFailPublisherOnSubscribe() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        dataSubscriber.cancel();
+        payloadSubscriber.subscribe(data.getPayload());
+        payloadSubscriber.verifyFailure(SpliceFlatStreamToMetaSingle.SplicingSubscriber.CANCELED);
+    }
+
+    @Test
+    public void cancelDataBeforeDataCompleteShouldDeliverError() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        dataSubscriber.cancel();
+        upstream.fail(true, DELIBERATE_EXCEPTION);
+        dataSubscriber.verifyFailure(DELIBERATE_EXCEPTION);
+    }
+
+    @Test
+    public void streamErrorAfterPublisherSubscribeShouldDeliverError() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        payloadSubscriber.subscribe(data.getPayload());
+        payloadSubscriber.request(1);
+        upstream.sendItems(one);
+        upstream.fail(false, DELIBERATE_EXCEPTION);
+        payloadSubscriber.verifyItems(one);
+        payloadSubscriber.verifyFailure(DELIBERATE_EXCEPTION);
+    }
+
+    @Test
+    public void streamCompleteAfterPublisherSubscribeShouldDeliverComplete() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        payloadSubscriber.subscribe(data.getPayload());
+        payloadSubscriber.request(3);
+        upstream.sendItems(one, two, last);
+        upstream.complete();
+        payloadSubscriber.verifySuccess(one, two, last);
+    }
+
+    @Test
+    public void streamCompleteBeforePublisherSubscribeShouldDeliverCompleteOnSubscribe() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        upstream.complete();
+        payloadSubscriber.subscribe(data.getPayload());
+        payloadSubscriber.verifySuccess();
+    }
+
+    @Test
+    public void streamErrorBeforePublisherSubscribeShouldDeliverErrorOnSubscribe() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        upstream.fail(false, DELIBERATE_EXCEPTION);
+        payloadSubscriber.subscribe(data.getPayload());
+        payloadSubscriber.verifyFailure(DELIBERATE_EXCEPTION);
+    }
+
+    @Test
+    public void publisherSubscribeTwiceShouldFailSecondSubscriber() {
+        Publisher<Object> stream = upstream.getPublisher();
+        SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
+                EXECUTOR, stream, Data::new);
+        dataSubscriber.listen(op);
+        upstream.sendItems(metaData);
+        Data data = dataSubscriber.verifySuccessAndReturn(Data.class);
+        assertThat(data.getMeta(), equalTo(data.getMeta()));
+        payloadSubscriber.subscribe(data.getPayload());
+        payloadSubscriber.request(3);
+        upstream.sendItems(one, two, last);
+        dupePayloadSubscriber.subscribe(data.getPayload());
+        dupePayloadSubscriber.verifyFailure(IllegalStateException.class);
+        upstream.complete();
+        payloadSubscriber.verifySuccess(one, two, last);
+    }
+
+    private static class MetaData {
+        private final String meta;
+
+        MetaData(String meta) {
+            this.meta = meta;
+        }
+
+        String getMeta() {
+            return meta;
+        }
+    }
+
+    private static final class Data extends MetaData {
+        private final Publisher<Payload> payload;
+
+        Data(MetaData metaData, Publisher<Payload> payload) {
+            super(metaData.getMeta());
+            this.payload = payload;
+        }
+
+        Publisher<Payload> getPayload() {
+            return payload;
+        }
+    }
+
+    private static class Payload {
+    }
+
+    private static final class LastPayload extends Payload {
+    }
+}
