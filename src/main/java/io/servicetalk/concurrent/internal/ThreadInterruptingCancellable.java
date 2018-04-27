3db0da8f12582e5918502a78d6478b8a7f39e1c9
Scott Mitchell <scott_mitchell@apple.com>
2018-04-27 16:13:22 -0700

    ThreadInterruptingCancellable
    Motivation:
    When doing blocking operations and converting from an asynchronous interface a Subscriber may want to interrupt the current thread in order to cancel work. We currently don't have a utility to accomplish this.
    
    Modifications:
    - Introduce ThreadInterruptingCancellable which is capable of interrupting a thread on cancel
    
    Result:
    Blocking operations can be interrupted by asynchronous consumers.

---
+++ src/main/java/io/servicetalk/concurrent/internal/ThreadInterruptingCancellable.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.concurrent.internal;
+
+import io.servicetalk.concurrent.Cancellable;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A {@link Cancellable} that will {@link Thread#interrupt() interrupt a thread}.
+ * <p>
+ * It is important that {@link #setDone()} is called after the associated blocking operation completes to avoid
+ * "spurious" thread interrupts.
+ */
+public final class ThreadInterruptingCancellable implements Cancellable {
+    private static final AtomicIntegerFieldUpdater<ThreadInterruptingCancellable> statusUpdater =
+            AtomicIntegerFieldUpdater.newUpdater(ThreadInterruptingCancellable.class, "status");
+
+    @SuppressWarnings("unused")
+    private volatile int status;
+    private final Thread threadToInterrupt;
+
+    /**
+     * Create a new instance.
+     * @param threadToInterrupt The thread {@link Thread#interrupt() interrupt} in {@link #cancel()}.
+     */
+    public ThreadInterruptingCancellable(Thread threadToInterrupt) {
+        this.threadToInterrupt = requireNonNull(threadToInterrupt);
+    }
+
+    @Override
+    public void cancel() {
+        if (statusUpdater.compareAndSet(this, 0, 1)) {
+            threadToInterrupt.interrupt();
+        }
+    }
+
+    /**
+     * Indicates the operation associated with this {@link Cancellable} is done and future calls to {@link #cancel()}
+     * should be NOOPs.
+     */
+    public void setDone() {
+        status = 1;
+    }
+}
