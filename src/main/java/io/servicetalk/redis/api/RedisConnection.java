4d0566770fd4b5a9cb5069cd81fe61bbc0f304ab
Scott Mitchell <scott_mitchell@apple.com>
2018-05-02 15:39:59 -0700

    ExecutionContext#getAllocator() rename to getBufferAllocator()
    Motivation:
    Use more fully specified method name to reduce the chances of collisions for classes that implement multiple interface.

--- src/main/java/io/servicetalk/redis/api/RedisConnection.java
+++ src/main/java/io/servicetalk/redis/api/RedisConnection.java
@@ -34,7 +34,7 @@ public abstract class RedisConnection extends RedisRequester {
 
     @Override
     public BufferAllocator getBufferAllocator() {
-        return getConnectionContext().getAllocator();
+        return getConnectionContext().getBufferAllocator();
     }
 
     /**
