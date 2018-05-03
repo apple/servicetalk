d95106782f9ef20a2a6c2878f2b35c247a437f1b
Nitesh Kant <nitesh_kant@apple.com>
2018-05-02 17:02:08 -0700

    Initial commit.

---
+++ src/main/java/io/servicetalk/examples/http/helloworld/HelloWorldBlockingServer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.examples.http.helloworld;
+
+import io.servicetalk.http.api.HttpServerStarter;
+import io.servicetalk.http.netty.NettyHttpServerStarter;
+import io.servicetalk.transport.api.IoExecutor;
+import io.servicetalk.transport.api.ServerContext;
+import io.servicetalk.transport.netty.NettyIoExecutors;
+
+import java.net.InetSocketAddress;
+
+import static io.servicetalk.concurrent.api.Executors.newCachedThreadExecutor;
+import static io.servicetalk.concurrent.internal.Await.awaitIndefinitely;
+
+/**
+ * A hello world server starter using a {@link HelloWorldBlockingService}.
+ */
+public final class HelloWorldBlockingServer {
+
+    private HelloWorldBlockingServer() {
+        // No instances.
+    }
+
+    /**
+     * Starts this server.
+     *
+     * @param args Program arguments, none supported yet.
+     * @throws Exception If the server could not be started.
+     */
+    public static void main(String[] args) throws Exception {
+        // Shared IoExecutor for the application.
+        IoExecutor ioExecutor = NettyIoExecutors.createExecutor();
+
+        HttpServerStarter starter = new NettyHttpServerStarter(ioExecutor);
+        ServerContext serverContext = awaitIndefinitely(starter.start(new InetSocketAddress(8080), newCachedThreadExecutor(), new HelloWorldBlockingService().asAsynchronousService()));
+        assert serverContext != null;
+        awaitIndefinitely(serverContext.onClose());
+    }
+}
---
+++ src/main/java/io/servicetalk/examples/http/helloworld/HelloWorldBlockingService.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.examples.http.helloworld;
+
+import io.servicetalk.http.api.BlockingHttpRequest;
+import io.servicetalk.http.api.BlockingHttpResponse;
+import io.servicetalk.http.api.BlockingHttpService;
+import io.servicetalk.http.api.HttpPayloadChunk;
+import io.servicetalk.transport.api.ConnectionContext;
+
+import static io.servicetalk.http.api.BlockingHttpResponses.newResponse;
+import static io.servicetalk.http.api.HttpPayloadChunks.newPayloadChunk;
+import static io.servicetalk.http.api.HttpResponseStatuses.OK;
+
+/**
+ * Business logic for the Hello World application.
+ * This is a blocking service, for asynchronous use {@link HelloWorldService}.
+ */
+final class HelloWorldBlockingService extends BlockingHttpService<HttpPayloadChunk, HttpPayloadChunk> {
+
+    @Override
+    public BlockingHttpResponse<HttpPayloadChunk> handle(final ConnectionContext ctx, final BlockingHttpRequest<HttpPayloadChunk> request) {
+        return newResponse(OK, newPayloadChunk(ctx.getAllocator().fromAscii("Hello World!")), ctx.getExecutor());
+    }
+}
