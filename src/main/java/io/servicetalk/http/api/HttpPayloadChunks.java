1001382e4234d000ffad985c5eb83a46aa7b8aae
Nitesh Kant <nitesh_kant@apple.com>
2018-05-03 11:11:46 -0700

    Http payload aggregation
    * Http payload aggregation
    
    __Motivation__
    
    We always provide HTTP payload as a stream (`Publisher` or `Iterable`) but many people want to interact with an aggregated payload.
    
    __Modification__
    
    Provide static utilities to aggregate an `Iterable` or `Publisher` of `HttpPayloadChunk`s into a single `LastHttpPayloadChunk`.
    
    __Result__
    
    Now one can also use an aggregated payload!

--- src/main/java/io/servicetalk/http/api/HttpPayloadChunks.java
+++ src/main/java/io/servicetalk/http/api/HttpPayloadChunks.java
@@ -16,6 +16,12 @@
 package io.servicetalk.http.api;
 
 import io.servicetalk.buffer.Buffer;
+import io.servicetalk.buffer.BufferAllocator;
+import io.servicetalk.buffer.CompositeBuffer;
+import io.servicetalk.concurrent.api.Publisher;
+import io.servicetalk.concurrent.api.Single;
+
+import javax.annotation.Nullable;
 
 /**
  * Factory methods for creating {@link HttpPayloadChunk}s.
@@ -45,5 +51,161 @@ public final class HttpPayloadChunks {
      */
     public static LastHttpPayloadChunk newLastPayloadChunk(final Buffer content, final HttpHeaders trailers) {
         return new DefaultLastHttpPayloadChunk(content, trailers);
+    }
+
+    /**
+     * Aggregates a {@link Publisher} of {@link HttpPayloadChunk} into a {@link Single} of {@link LastHttpPayloadChunk}.
+     *
+     * @param content {@link Publisher} to be aggregated.
+     * @param allocator {@link BufferAllocator} to create a {@link Buffer} for content accumulation.
+     * @return A {@link Single} which contains the aggregated HTTP payload and optional trailers.
+     */
+    public static Single<LastHttpPayloadChunk> aggregateChunks(final Publisher<HttpPayloadChunk> content,
+                                                               final BufferAllocator allocator) {
+        return content.reduce(() -> newAggregatingChunk(allocator), (aggregatingChunk, chunk) -> {
+            aggregatingChunk.addChunk(chunk);
+            return aggregatingChunk;
+        }).map(aggregatingChunk -> aggregatingChunk);
+    }
+
+    /**
+     * Aggregates an {@link Iterable} of {@link HttpPayloadChunk} into a {@link LastHttpPayloadChunk}.
+     *
+     * @param content {@link Iterable} to be aggregated.
+     * @param allocator {@link BufferAllocator} to create a {@link Buffer} for content accumulation.
+     * @return Aggregated {@link LastHttpPayloadChunk} which contains the aggregated HTTP payload and optional trailers.
+     */
+    public static LastHttpPayloadChunk aggregateChunks(final Iterable<HttpPayloadChunk> content,
+                                                       final BufferAllocator allocator) {
+        AggregatingChunk aggregatingChunk = newAggregatingChunk(allocator);
+        for (HttpPayloadChunk chunk : content) {
+            aggregatingChunk.addChunk(chunk);
+        }
+        return aggregatingChunk;
+    }
+
+    /**
+     * Creates a new instance of {@link AggregatingChunk} to be used to aggregate an HTTP payload {@link Publisher}.
+     *
+     * @param allocator {@link BufferAllocator} to create a {@link Buffer} for content accumulation.
+     *
+     * @return A new instance of {@link AggregatingChunk}.
+     */
+    public static AggregatingChunk newAggregatingChunk(final BufferAllocator allocator) {
+        return newAggregatingChunk(Integer.MAX_VALUE, allocator);
+    }
+
+    /**
+     * Creates a new instance of {@link AggregatingChunk} to be used to aggregate an HTTP payload {@link Publisher}.
+     *
+     * @param allocator {@link BufferAllocator} to create a {@link Buffer} for content accumulation.
+     * @param trailerFactory {@link HttpHeadersFactory} to create a trailer for the returned {@link AggregatingChunk}.
+     * This will be used to create a trailer, only if {@link AggregatingChunk#getTrailers()} is called before
+     * aggregation is completed.
+     *
+     * @return A new instance of {@link AggregatingChunk}.
+     */
+    public static AggregatingChunk newAggregatingChunk(final BufferAllocator allocator,
+                                                       final HttpHeadersFactory trailerFactory) {
+        return newAggregatingChunk(Integer.MAX_VALUE, trailerFactory, allocator);
+    }
+
+    /**
+     * Creates a new instance of {@link AggregatingChunk} to be used to aggregate an HTTP payload {@link Publisher}.
+     *
+     * @param maxChunksCount Maximum expected {@link HttpPayloadChunk}s.
+     * @param allocator {@link BufferAllocator} to create a {@link Buffer} for content accumulation.
+     *
+     * @return A new instance of {@link AggregatingChunk}.
+     */
+    public static AggregatingChunk newAggregatingChunk(final int maxChunksCount, final BufferAllocator allocator) {
+        return newAggregatingChunk(maxChunksCount, DefaultHttpHeadersFactory.INSTANCE, allocator);
+    }
+
+    /**
+     * Creates a new instance of {@link AggregatingChunk} to be used to aggregate an HTTP payload {@link Publisher}.
+     *
+     * @param maxChunksCount Maximum expected {@link HttpPayloadChunk}s.
+     * @param trailerFactory {@link HttpHeadersFactory} to create a trailer for the returned {@link AggregatingChunk}.
+     * This will be used to create a trailer, only if {@link AggregatingChunk#getTrailers()} is called before
+     * aggregation is completed.
+     * @param allocator {@link BufferAllocator} to create a {@link Buffer} for content accumulation.
+     *
+     * @return A new instance of {@link AggregatingChunk}.
+     */
+    public static AggregatingChunk newAggregatingChunk(final int maxChunksCount,
+                                                       final HttpHeadersFactory trailerFactory,
+                                                       final BufferAllocator allocator) {
+        return new AggregatingChunkImpl(allocator, maxChunksCount, trailerFactory);
+    }
+
+    /**
+     * A special {@link LastHttpPayloadChunk} that allows to incrementally aggregate multiple {@link HttpPayloadChunk}.
+     *
+     * <h2>Incremental aggregation</h2>
+     * This interface expects to incrementally aggregate {@link HttpPayloadChunk}s from a stream.
+     * Data from {@link LastHttpPayloadChunk}, viz., {@link #getContent()} and {@link #getTrailers()} MAY be different
+     * before the aggregation is completed.
+     */
+    public interface AggregatingChunk extends LastHttpPayloadChunk {
+
+        /**
+         * Adds an {@link HttpPayloadChunk} to this {@link AggregatingChunk}.
+         * If this {@link HttpPayloadChunk} is a {@link LastHttpPayloadChunk} then the trailers will be available via
+         * {@link #getTrailers()} after this method returns. Otherwise, the payload as returned by
+         * {@link HttpPayloadChunk#getContent()} is added to this {@link AggregatingChunk}.
+         *
+         * @param chunk {@link HttpPayloadChunk} to add.
+         */
+        void addChunk(HttpPayloadChunk chunk);
+    }
+
+    private static class AggregatingChunkImpl implements AggregatingChunk {
+
+        private final HttpHeadersFactory trailerFactory;
+        private final CompositeBuffer buffer;
+        @Nullable
+        private HttpHeaders trailers;
+
+        AggregatingChunkImpl(final BufferAllocator allocator, final int maxChunksCount,
+                             HttpHeadersFactory trailerFactory) {
+            this.trailerFactory = trailerFactory;
+            buffer = allocator.newCompositeBuffer(maxChunksCount);
+        }
+
+        @Override
+        public void addChunk(final HttpPayloadChunk chunk) {
+            if (chunk instanceof LastHttpPayloadChunk) {
+                if (trailers == null) {
+                    trailers = ((LastHttpPayloadChunk) chunk).getTrailers();
+                } else {
+                    trailers.add(((LastHttpPayloadChunk) chunk).getTrailers());
+                }
+            }
+            buffer.addBuffer(chunk.getContent(), true);
+        }
+
+        @Override
+        public HttpHeaders getTrailers() {
+            if (trailers == null) {
+                trailers = trailerFactory.newEmptyTrailers();
+            }
+            return trailers;
+        }
+
+        @Override
+        public LastHttpPayloadChunk duplicate() {
+            return newLastPayloadChunk(buffer.duplicate(), getTrailers());
+        }
+
+        @Override
+        public LastHttpPayloadChunk replace(final Buffer content) {
+            return newLastPayloadChunk(content, getTrailers());
+        }
+
+        @Override
+        public Buffer getContent() {
+            return buffer;
+        }
     }
 }
---
+++ src/test/java/io/servicetalk/http/api/ChunkAggregatorIterableTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.api;
+
+import io.servicetalk.buffer.Buffer;
+import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static io.servicetalk.buffer.netty.BufferAllocators.DEFAULT_ALLOCATOR;
+import static io.servicetalk.concurrent.api.DeliberateException.DELIBERATE_EXCEPTION;
+import static io.servicetalk.http.api.HttpPayloadChunks.newLastPayloadChunk;
+import static io.servicetalk.http.api.HttpPayloadChunks.newPayloadChunk;
+import static java.nio.charset.Charset.defaultCharset;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.rules.ExpectedException.none;
+
+public class ChunkAggregatorIterableTest {
+
+    @Rule
+    public final Timeout timeout = new ServiceTalkTestTimeout();
+    @Rule
+    public final ExpectedException expected = none();
+
+    @Test
+    public void emptyStream() throws Exception {
+        LastHttpPayloadChunk lastChunk = aggregate(emptyList());
+        verifyAggregatedChunk(lastChunk);
+    }
+
+    @Test
+    public void errorStream() throws Exception {
+        expected.expect(sameInstance(DELIBERATE_EXCEPTION));
+        addErrorAndAggregate();
+    }
+
+    @Test
+    public void noLastHttpChunk() throws Exception {
+        final Buffer data = DEFAULT_ALLOCATOR.fromAscii("Hello");
+        HttpPayloadChunk chunk = newPayloadChunk(data);
+        LastHttpPayloadChunk lastChunk = aggregate(chunk);
+
+        verifyAggregatedChunk(lastChunk, data);
+    }
+
+    @Test
+    public void onlyLastHttpChunkNoContent() throws Exception {
+        HttpHeaders trailers = DefaultHttpHeadersFactory.INSTANCE.newEmptyTrailers();
+        trailers.add("foo", "bar");
+        LastHttpPayloadChunk chunk = newLastPayloadChunk(DEFAULT_ALLOCATOR.newBuffer(0), trailers);
+        LastHttpPayloadChunk aggregated = aggregate(chunk);
+
+        verifyAggregatedChunk(aggregated, trailers);
+    }
+
+    @Test
+    public void onlyLastHttpChunk() throws Exception {
+        final Buffer data = DEFAULT_ALLOCATOR.fromAscii("Hello");
+        HttpHeaders trailers = DefaultHttpHeadersFactory.INSTANCE.newEmptyTrailers();
+        trailers.add("foo", "bar");
+        LastHttpPayloadChunk chunk = newLastPayloadChunk(data, trailers);
+        LastHttpPayloadChunk aggregated = aggregate(chunk);
+
+        verifyAggregatedChunk(aggregated, data, trailers);
+    }
+
+    @Test
+    public void chunkAndLastChunkNoContent() throws Exception {
+        final Buffer data = DEFAULT_ALLOCATOR.fromAscii("Hello");
+        HttpHeaders trailers = DefaultHttpHeadersFactory.INSTANCE.newEmptyTrailers();
+        HttpPayloadChunk chunk = newPayloadChunk(data);
+        LastHttpPayloadChunk lastChunk = newLastPayloadChunk(DEFAULT_ALLOCATOR.newBuffer(0), trailers);
+        LastHttpPayloadChunk aggregated = aggregate(chunk, lastChunk);
+
+        verifyAggregatedChunk(aggregated, data, trailers);
+    }
+
+    @Test
+    public void chunkAndLastChunkContent() throws Exception {
+        final Buffer data = DEFAULT_ALLOCATOR.fromAscii("Hello");
+        HttpHeaders trailers = DefaultHttpHeadersFactory.INSTANCE.newEmptyTrailers();
+        HttpPayloadChunk chunk = newPayloadChunk(data.slice(0, 2));
+        LastHttpPayloadChunk lastChunk = newLastPayloadChunk(data.slice(2, 3), trailers);
+        LastHttpPayloadChunk aggregated = aggregate(chunk, lastChunk);
+
+        verifyAggregatedChunk(aggregated, data, trailers);
+    }
+
+    @Test
+    public void chunksAndThenError() throws Exception {
+        final Buffer data = DEFAULT_ALLOCATOR.fromAscii("Hello");
+        HttpPayloadChunk chunk = newPayloadChunk(data);
+        expected.expect(sameInstance(DELIBERATE_EXCEPTION));
+        addErrorAndAggregate(chunk);
+    }
+
+    private LastHttpPayloadChunk aggregate(HttpPayloadChunk... chunks) throws Exception {
+        return aggregate(asList(chunks));
+    }
+
+    private void addErrorAndAggregate(HttpPayloadChunk... chunks) throws Exception {
+        Iterable<HttpPayloadChunk> iterable = asList(chunks);
+        aggregate(() -> new Iterator<HttpPayloadChunk>() {
+
+            private final Iterator<HttpPayloadChunk> chunkIterator = iterable.iterator();
+            private boolean errorThrown;
+
+            @Override
+            public boolean hasNext() {
+                return chunkIterator.hasNext() || !errorThrown;
+            }
+
+            @Override
+            public HttpPayloadChunk next() {
+                if (chunkIterator.hasNext()) {
+                    return chunkIterator.next();
+                }
+                if (errorThrown) {
+                    throw new NoSuchElementException();
+                }
+                errorThrown = true;
+                throw DELIBERATE_EXCEPTION;
+            }
+        });
+    }
+
+    private LastHttpPayloadChunk aggregate(Iterable<HttpPayloadChunk> chunks) throws Exception {
+        return HttpPayloadChunks.aggregateChunks(chunks, DEFAULT_ALLOCATOR);
+    }
+
+    private void verifyAggregatedChunk(final LastHttpPayloadChunk chunk) {
+        verifyAggregatedChunk(chunk, DEFAULT_ALLOCATOR.newBuffer(0));
+    }
+
+    private void verifyAggregatedChunk(final LastHttpPayloadChunk chunk, HttpHeaders expectedTrailers) {
+        verifyAggregatedChunk(chunk, DEFAULT_ALLOCATOR.newBuffer(0), expectedTrailers);
+    }
+
+    private void verifyAggregatedChunk(final LastHttpPayloadChunk chunk, Buffer expectedData) {
+        verifyAggregatedChunk(chunk, expectedData, DefaultHttpHeadersFactory.INSTANCE.newEmptyTrailers());
+    }
+
+    private void verifyAggregatedChunk(final LastHttpPayloadChunk chunk, Buffer expectedData,
+                                       HttpHeaders expectedTrailers) {
+        assertThat("Unexpected trailers.", chunk.getTrailers(), equalTo(expectedTrailers));
+        assertThat("Unexpected content.", chunk.getContent(), is(notNullValue()));
+        assertThat("Unexpected content readable bytes.", chunk.getContent().getReadableBytes(),
+                is(expectedData.getReadableBytes()));
+        assertThat("Unexpected content data.", chunk.getContent().toString(defaultCharset()),
+                is(expectedData.toString(defaultCharset())));
+    }
+}
