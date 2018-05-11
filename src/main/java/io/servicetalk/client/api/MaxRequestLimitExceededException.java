522af9de5e8480e186b4b576c33ade9ef7d02b63
Scott Mitchell <scott_mitchell@apple.com>
2018-05-11 14:02:41 -0700

    MaxRequestLimitExceededException simplification
    Motivation:
    MaxRequestLimitExceededException currently requires the max concurrent requests but this state may not be available at the time the exception is generated, and may also change by the time the condition occurred and creation of the exception which maybe misleading.
    
    Modificaitons:
    - MaxRequestLimitExceededException should just take a String message
    - MaxRequestLimitExceededException should be retryable to indicate nothing was written and retry may be appropriate
    
    Result:
    Simpler MaxRequestLimitExceededException.

--- src/main/java/io/servicetalk/client/api/MaxRequestLimitExceededException.java
+++ src/main/java/io/servicetalk/client/api/MaxRequestLimitExceededException.java
@@ -18,40 +18,15 @@ package io.servicetalk.client.api;
 /**
  * Exception raised when more concurrent requests have been issued on a connection than is allowed.
  */
-public final class MaxRequestLimitExceededException extends RuntimeException {
-    private static final long serialVersionUID = 3442285141858696983L;
-
-    private final int limit;
+public final class MaxRequestLimitExceededException extends RetryableException {
+    private static final long serialVersionUID = 9114515334374632438L;
 
     /**
      * New instance.
      *
-     * @param connection The connection object.
-     * @param limit Saturation limit for the connection.
-     * @param cause {@link Throwable} cause for this exception.
+     * @param message The exception message.
      */
-    public MaxRequestLimitExceededException(Object connection, int limit, Throwable cause) {
-        super("Connection " + connection + " exceeded max requests limit: " + limit, cause);
-        this.limit = limit;
-    }
-
-    /**
-     * New instance.
-     *
-     * @param connection The connection object.
-     * @param limit Saturation limit for the connection.
-     */
-    public MaxRequestLimitExceededException(Object connection, int limit) {
-        super("Connection " + connection + " exceeded max requests limit: " + limit);
-        this.limit = limit;
-    }
-
-    /**
-     * The configured limit which was breached.
-     *
-     * @return max request limit.
-     */
-    public int getLimit() {
-        return limit;
+    public MaxRequestLimitExceededException(String message) {
+        super(message);
     }
 }
