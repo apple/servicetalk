ce077436aac99b7a6345c2b97c63f71a7e2a5bf8
Idel Pivnitskiy <ipivnitskiy@apple.com>
2018-06-05 10:50:27 -0700

    Add an example for `AddressParsingHttpRequester`
    Motivation:
    
    Provide an example for new API.
    
    Modification:
    
    - Add `AggregatingPayloadAddressParsingRequester`;
    
    Result:
    
    Example for `AddressParsingHttpRequester`.

--- src/main/java/io/servicetalk/examples/http/aggregation/AggregatingPayloadAddressParsingRequester.java
+++ src/main/java/io/servicetalk/examples/http/aggregation/AggregatingPayloadAddressParsingRequester.java
@@ -16,38 +16,27 @@
 package io.servicetalk.examples.http.aggregation;
 
 import io.servicetalk.buffer.api.CompositeBuffer;
-import io.servicetalk.client.api.ServiceDiscoverer;
 import io.servicetalk.concurrent.api.AsyncCloseables;
 import io.servicetalk.concurrent.api.CompositeCloseable;
-import io.servicetalk.dns.discovery.netty.DefaultDnsServiceDiscovererBuilder;
-import io.servicetalk.http.api.AggregatedHttpClient;
-import io.servicetalk.http.api.AggregatedHttpRequest;
-import io.servicetalk.http.api.HttpPayloadChunk;
-import io.servicetalk.http.netty.DefaultHttpClientBuilder;
+import io.servicetalk.http.all.netty.AddressParsingHttpRequesterBuilder;
+import io.servicetalk.http.api.AggregatedHttpRequester;
 import io.servicetalk.transport.api.DefaultExecutionContext;
-import io.servicetalk.transport.api.DefaultHostAndPort;
 import io.servicetalk.transport.api.ExecutionContext;
-import io.servicetalk.transport.api.HostAndPort;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetSocketAddress;
 import java.util.concurrent.CountDownLatch;
 
 import static io.servicetalk.buffer.netty.BufferAllocators.DEFAULT_ALLOCATOR;
 import static io.servicetalk.concurrent.api.Executors.newCachedThreadExecutor;
 import static io.servicetalk.http.api.AggregatedHttpRequests.newRequest;
-import static io.servicetalk.http.api.HttpHeaderNames.TRANSFER_ENCODING;
-import static io.servicetalk.http.api.HttpHeaderValues.CHUNKED;
 import static io.servicetalk.http.api.HttpRequestMethods.GET;
-import static io.servicetalk.http.utils.HttpHostHeaderFilter.newHostHeaderFilter;
-import static io.servicetalk.loadbalancer.RoundRobinLoadBalancer.newRoundRobinFactory;
 import static io.servicetalk.transport.netty.NettyIoExecutors.createIoExecutor;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
-public final class AggregatingPayloadClient {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AggregatingPayloadClient.class);
+public final class AggregatingPayloadAddressParsingRequester {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AggregatingPayloadAddressParsingRequester.class);
 
     public static void main(String[] args) throws Exception {
         // Collection of all resources in this test that can be closed together at the end.
@@ -56,21 +45,13 @@ public final class AggregatingPayloadClient {
             ExecutionContext executionContext =
                     new DefaultExecutionContext(DEFAULT_ALLOCATOR, createIoExecutor(), newCachedThreadExecutor());
 
-            // In this example we will use DNS as our Service Discovery system.
-            ServiceDiscoverer<HostAndPort, InetSocketAddress> dnsDiscoverer =
-                    new DefaultDnsServiceDiscovererBuilder(executionContext).build();
-
-            // Create a ClientBuilder and use round robin load balancing.
-            DefaultHttpClientBuilder<InetSocketAddress> clientBuilder =
-                    new DefaultHttpClientBuilder<>(newRoundRobinFactory());
-
-            // Build the client, and register for DNS discovery events.
-            HostAndPort address = new DefaultHostAndPort("localhost", 8080);
-            AggregatedHttpClient client = clientBuilder.appendClientFilterFactory(c -> newHostHeaderFilter(address, c))
-                    .buildAggregated(executionContext, dnsDiscoverer.discover(address));
+            // Build the requester.
+            // This builder sets appropriate defaults for Service Discovery and load balancing.
+            final AggregatedHttpRequester requester = new AddressParsingHttpRequesterBuilder()
+                    .buildAggregated(executionContext);
 
             // Register resources to be cleaned up at the end.
-            resources.concat(client, dnsDiscoverer, executionContext.getExecutor(), executionContext.getIoExecutor());
+            resources.concat(requester, executionContext.getExecutor(), executionContext.getIoExecutor());
 
             // This example is demonstrating asynchronous execution, but needs to prevent the main thread from exiting
             // before the response has been processed. This isn't typical usage for a streaming API but is useful for
@@ -79,19 +60,18 @@ public final class AggregatingPayloadClient {
 
             // Create a big buffer so that we can leverage aggregation on the response which is the request payload
             // echoed back.
-            CompositeBuffer payload = executionContext.getBufferAllocator().newCompositeBuffer(10);
+            final CompositeBuffer payload = executionContext.getBufferAllocator().newCompositeBuffer(10);
             for (int i = 0; i < 10; i++) {
                 payload.addBuffer(executionContext.getBufferAllocator().fromAscii(i + " hello\n"));
             }
-            AggregatedHttpRequest<HttpPayloadChunk> request = newRequest(GET, "/sayHello", payload);
-            // This is required at the moment since HttpClient does not add a transfer-encoding header.
-            request.getHeaders().add(TRANSFER_ENCODING, CHUNKED);
-            client.request(request)
+
+            requester.request(newRequest(GET, "http://localhost:8080/sayHello", payload))
                     .doAfterError(cause -> LOGGER.error("request failed!", cause))
                     .doAfterFinally(responseProcessedLatch::countDown)
                     .subscribe(response -> {
-                        LOGGER.info("got response \n{}", response.toString((name, value) -> value));
-                        LOGGER.info("Response content: \n{}", response.getPayloadBody().getContent().toString(US_ASCII));
+                        LOGGER.info("Got response \n{}", response.toString((name, value) -> value));
+                        LOGGER.info("Response content: \n{}",
+                                response.getPayloadBody().getContent().toString(US_ASCII));
                     });
 
             // Don't exit the main thread until after the response is completely processed.
