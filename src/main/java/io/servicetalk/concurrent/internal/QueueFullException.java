52f4c4bc2f79c26385b38ff52ea9b7dd0673d05e
Nitesh Kant <nitesh_kant@apple.com>
2018-08-02 15:45:42 -0700

    Move `SignalOffloader` to `concurrent-internal`
    * Move `SignalOffloader` to `concurrent-internal`
    
    __Motivation__
    
    In order to override `SignalOffloader` for specific `Executor` (eg: IoExecutor), we need it to be public. It isn't mature enough to be a public API, so we move it to `concurrent-internal`
    
    __Modification__
    
    Refactored code to move `SignalOffloader` to `concurrent-internal`.
    
    __Result__
    
    `Executor` implementations can not define custom `SignalOffloader`s

---
+++ src/main/java/io/servicetalk/concurrent/internal/QueueFullException.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.concurrent.internal;
+
+/**
+ * Exception indicating a bounded queue is full.
+ */
+public final class QueueFullException extends RuntimeException {
+    private static final long serialVersionUID = 7206036429195573500L;
+
+    /**
+     * New instance.
+     *
+     * @param queueIdentifier Identifier for the queue that is full.
+     * @param capacity Capacity for queue.
+     */
+    public QueueFullException(String queueIdentifier, int capacity) {
+        super("Queue " + queueIdentifier + " exceeded maximum capacity " + capacity);
+    }
+}
