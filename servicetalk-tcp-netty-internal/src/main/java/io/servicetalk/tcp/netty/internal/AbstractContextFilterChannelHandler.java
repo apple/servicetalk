1591190bf1f2e7311e2c24fd9c21a1df7b0ea14c
Derek Lewis <derek_a_lewis@apple.com>
2018-08-15 09:24:26 -0700

    Ensure exceptions from `subscribe()` are caught and close the connection.
    Motivation:
    
    If the call to `subscribe()` throws an exception, this should be treated
    as a failure of the context filter, and close the connection.
    
    Modifications:
    
    - move the `subscribe()` call into the existing catch block.
    
    Results:
    
    Exceptions from `subscribe()` close the connection.

--- servicetalk-tcp-netty-internal/src/main/java/io/servicetalk/tcp/netty/internal/AbstractContextFilterChannelHandler.java
+++ servicetalk-tcp-netty-internal/src/main/java/io/servicetalk/tcp/netty/internal/AbstractContextFilterChannelHandler.java
@@ -64,50 +64,46 @@ abstract class AbstractContextFilterChannelHandler extends ChannelDuplexHandler 
     abstract void onContextFilterSuccessful(ChannelHandlerContext ctx);
 
     private void runContextFilter(final ChannelHandlerContext ctx) {
-        Single<Boolean> filterResult;
         try {
-            filterResult = contextFilter.filter(context);
+            contextFilter.filter(context).subscribe(new Single.Subscriber<Boolean>() {
+                @Override
+                public void onSubscribe(final Cancellable cancellable) {
+                    assert sequentialCancellable != null;
+
+                    sequentialCancellable.setNextCancellable(cancellable);
+                }
+
+                @Override
+                public void onSuccess(@Nullable final Boolean result) {
+                    if (result != null && result) {
+                        // handleSuccess makes multiple calls that will queue things on the event loop already, so we
+                        // offload the whole method to the event loop. This also ensures handleSuccess is only run from
+                        // the event loop, which means state doesn't need to be handled concurrently.
+                        final EventLoop eventLoop = ctx.channel().eventLoop();
+                        if (eventLoop.inEventLoop()) {
+                            handleSuccess(ctx);
+                        } else {
+                            eventLoop.execute(() -> handleSuccess(ctx));
+                        }
+                    } else {
+                        // Getting the remote-address may involve volatile reads and potentially a syscall, so guard it.
+                        if (LOGGER.isDebugEnabled()) {
+                            LOGGER.debug("Rejected connection from {}", context.getRemoteAddress());
+                        }
+                        ctx.close();
+                    }
+                }
+
+                @Override
+                public void onError(final Throwable t) {
+                    LOGGER.warn("Error from context filter {} for context {}.", contextFilter, context, t);
+                    ctx.close();
+                }
+            });
         } catch (Throwable t) {
             ctx.close();
             LOGGER.warn("Exception from context filter {} for context {}.", contextFilter, context, t);
-            return;
         }
-        filterResult.subscribe(new Single.Subscriber<Boolean>() {
-
-            @Override
-            public void onSubscribe(final Cancellable cancellable) {
-                assert sequentialCancellable != null;
-
-                sequentialCancellable.setNextCancellable(cancellable);
-            }
-
-            @Override
-            public void onSuccess(@Nullable final Boolean result) {
-                if (result != null && result) {
-                    // handleSuccess makes multiple calls that will queue things on the event loop already, so we
-                    // offload the whole method to the event loop. This also ensures handleSuccess is only run from
-                    // the event loop, which means state doesn't need to be handled concurrently.
-                    final EventLoop eventLoop = ctx.channel().eventLoop();
-                    if (eventLoop.inEventLoop()) {
-                        handleSuccess(ctx);
-                    } else {
-                        eventLoop.execute(() -> handleSuccess(ctx));
-                    }
-                } else {
-                    // Getting the remote-address may involve volatile reads and potentially a syscall, so guard it.
-                    if (LOGGER.isDebugEnabled()) {
-                        LOGGER.debug("Rejected connection from {}", context.getRemoteAddress());
-                    }
-                    ctx.close();
-                }
-            }
-
-            @Override
-            public void onError(final Throwable t) {
-                LOGGER.warn("Error from context filter {} for context {}.", contextFilter, context, t);
-                ctx.close();
-            }
-        });
     }
 
     @Override
