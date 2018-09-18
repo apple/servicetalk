a16b0763d4ac6adaa558f944b74778d891ca3258
Nitesh Kant <nitesh_kant@apple.com>
2018-09-17 22:40:16 -0700

    Update hello-world examples (#35)
    * Update hello-world examples
    
    __Motivation__
    
    We should update examples to reflect the new APIs.
    
    __Modification__
    
    Hello-world examples are modified to do the following:
    
    - Remove `aggregated` package and flatten it to be directly under the parent package.
    - Add `*UrlClient` for all variants.
    - Use try-with-resources for client to remove the `AsyncCloseable` complexity from the examples.
    - Remove `CountDownLatch` for async client examples.
    - Use `ServerContext#awaitShutdown()` to remove the await on `onClose()` which is cryptic.
    
    __Result__
    
    More smaller examples.

---
+++ servicetalk-examples/src/main/java/io/servicetalk/examples/http/helloworld/blocking/streaming/BlockingHelloWorldStreamingUrlClient.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.examples.http.helloworld.blocking.streaming;
+
+import io.servicetalk.buffer.api.Buffer;
+import io.servicetalk.concurrent.BlockingIterator;
+import io.servicetalk.http.api.BlockingStreamingHttpClient;
+import io.servicetalk.http.api.BlockingStreamingHttpResponse;
+import io.servicetalk.http.netty.HttpClients;
+
+public final class BlockingHelloWorldStreamingUrlClient {
+
+    public static void main(String[] args) throws Exception {
+        try (BlockingStreamingHttpClient client = HttpClients.forMultiAddressUrl().buildBlockingStreaming()) {
+            BlockingStreamingHttpResponse response = client.request(client.get("http://localhost:8080/sayHello"));
+            System.out.println(response);
+            try (BlockingIterator<Buffer> payload = response.getPayloadBody().iterator()) {
+                while (payload.hasNext()) {
+                    System.out.println(payload.next());
+                }
+            }
+        }
+    }
+}
