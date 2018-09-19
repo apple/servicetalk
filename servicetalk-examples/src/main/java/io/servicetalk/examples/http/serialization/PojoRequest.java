bee10fd4c878f32663a96e2b42ca5346f67a979b
Nitesh Kant <nitesh_kant@apple.com>
2018-09-18 17:09:10 -0700

    Add serialization examples (#36)
    * Add serialization examples
    
    __Motivation__
    
    Add examples to demonstrate new serialization APIs.
    
    __Modification__
    
    Added examples for all programming paradigm variants.
    
    __Result__
    
    Examples now demonstrates serialization capabilities.

---
+++ servicetalk-examples/src/main/java/io/servicetalk/examples/http/serialization/PojoRequest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2018 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.examples.http.serialization;
+
+/**
+ * A request object to request {@link MyPojo} from the server.
+ */
+public final class PojoRequest {
+
+    private String id;
+
+    public PojoRequest() {
+    }
+
+    public PojoRequest(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
