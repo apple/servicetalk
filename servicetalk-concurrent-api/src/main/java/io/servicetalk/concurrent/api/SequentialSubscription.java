85ea8dfa13e3309a86c4e5a5f777ec8743bea16a
Scott Mitchell <scott_mitchell@apple.com>
2018-11-30 17:52:02 -0800

    review comments

--- servicetalk-concurrent-api/src/main/java/io/servicetalk/concurrent/api/SequentialSubscription.java
+++ servicetalk-concurrent-api/src/main/java/io/servicetalk/concurrent/api/SequentialSubscription.java
@@ -23,13 +23,10 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 import java.util.concurrent.atomic.AtomicLongFieldUpdater;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 import static io.servicetalk.concurrent.internal.EmptySubscription.EMPTY_SUBSCRIPTION;
-import static io.servicetalk.concurrent.internal.SubscriberUtils.calculateSourceRequested;
 import static io.servicetalk.concurrent.internal.SubscriberUtils.isRequestNValid;
 import static java.util.Objects.requireNonNull;
-import static java.util.concurrent.atomic.AtomicReferenceFieldUpdater.newUpdater;
 
 /**
  * A {@link Subscription} that delegates all {@link Subscription} calls to a <strong>current</strong>
@@ -49,8 +46,6 @@ final class SequentialSubscription implements Subscription, Cancellable {
             AtomicLongFieldUpdater.newUpdater(SequentialSubscription.class, "requested");
     private static final AtomicLongFieldUpdater<SequentialSubscription> sourceRequestedUpdater =
             AtomicLongFieldUpdater.newUpdater(SequentialSubscription.class, "sourceRequested");
-    private static final AtomicReferenceFieldUpdater<SequentialSubscription, Subscription> subscriptionUpdater =
-            newUpdater(SequentialSubscription.class, Subscription.class, "subscription");
 
     private long sourceEmitted;
     @SuppressWarnings("unused")
@@ -80,7 +75,7 @@ final class SequentialSubscription implements Subscription, Cancellable {
     public void request(long n) {
         if (isRequestNValid(n)) {
             requestedUpdater.accumulateAndGet(this, n, FlowControlUtil::addWithOverflowProtectionIfNotNegative);
-            calculateSourceRequested(subscriptionUpdater, requestedUpdater, sourceRequestedUpdater, this);
+            requestFromSubscription();
         } else {
             // With invalid input we don't attempt to enforce concurrency and rely upon the subscription
             // to enforce the specification rules [1].
@@ -94,6 +89,11 @@ final class SequentialSubscription implements Subscription, Cancellable {
         // Invalidate requested here so that it can be seen by switchTo later.
         requested = -1;
 
+        // We may concurrently interact with the Subscription here if another thread is in switchTo. However switchTo
+        // takes care to wrap the Subscription in ConcurrentSubscription while concurrent invocation is possible.
+        // Note that the order of instructions is important between invalidating requested and reading the subscription
+        // volatile variable! requested is set first so that the switchTo thread will observe that cancel has been
+        // called and call cancel on any new (or concurrently switched) Subscriptions.
         subscription.cancel();
     }
 
@@ -116,14 +116,16 @@ final class SequentialSubscription implements Subscription, Cancellable {
         subscription = ConcurrentSubscription.wrap(next);
 
         // If requested is non-negative, then subscription will be cancelled if the outer Subscription is
-        // cancelled. However if it is negative that means we have already been cancelled and we should for a cancel on
-        // the new Subscription.
+        // cancelled. However if it is negative that means we have already been cancelled and we should cancel the new
+        // Subscription.
         if (requested >= 0) {
-            // After the Subscription has been swapped, reset the sourceRequested value because the next Subscription
-            // has not had any data requested.
+            // We previously invalidated the sourceRequested to prevent other threads from interacting with the
+            // Subscription before we had a chance to reset it, which may result in double requesting. Now that we have
+            // swapped the subscription we can reset the sourceRequested value to sourceEmitted (the amount we have
+            // observed) because the new Subscription hasn't delivered any data yet.
             sourceRequested = sourceEmitted;
 
-            calculateSourceRequested(subscriptionUpdater, requestedUpdater, sourceRequestedUpdater, this);
+            requestFromSubscription();
         } else {
             subscription.cancel();
         }
@@ -141,5 +143,25 @@ final class SequentialSubscription implements Subscription, Cancellable {
         ++sourceEmitted;
         // There is no limit to how much we request from the current Subscription, so no need to check if we need to
         // request any more here.
+    }
+
+    private void requestFromSubscription() {
+        for (;;) {
+            // We have to read the Subscription before sourceRequested because sourceRequested is set to a negative
+            // value before swapping the subscription. So the subscription will be usable if sourceRequested is valid.
+            final Subscription subscription = this.subscription;
+            final long sourceRequested = this.sourceRequested;
+            final long requested = this.requested;
+            if (requested == sourceRequested || sourceRequested < 0 || requested < 0) {
+                break;
+            }
+
+            // sourceRequested ...[delta]... requested
+            final long delta = requested - sourceRequested;
+            if (sourceRequestedUpdater.compareAndSet(this, sourceRequested, sourceRequested + delta)) {
+                subscription.request(delta);
+                break;
+            }
+        }
     }
 }
