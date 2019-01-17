6f4fcac8be7d2440b5ae258465adc120cf8e73e6
Idel Pivnitskiy <ipivnitskiy@apple.com>
2019-01-17 11:25:45 -0800

    Add missed final modifier

--- servicetalk-client-api/src/main/java/io/servicetalk/client/api/RetryableConnectException.java
+++ servicetalk-client-api/src/main/java/io/servicetalk/client/api/RetryableConnectException.java
@@ -36,7 +36,7 @@ public class RetryableConnectException extends ConnectException implements Retry
      * Create a new instance.
      * @param cause the original cause.
      */
-    public RetryableConnectException(ConnectException cause) {
+    public RetryableConnectException(final ConnectException cause) {
         super(cause.getMessage());
         initCause(cause);
     }
