d1373191647b8326d56932eb387740ae41e0779d
Idel Pivnitskiy <ipivnitskiy@apple.com>
2019-01-22 12:16:59 -0800

    Remove ConnectionLimitExceededException

--- servicetalk-client-api/src/main/java/io/servicetalk/client/api/LimitingActiveConnectionFactoryFilter.java
+++ servicetalk-client-api/src/main/java/io/servicetalk/client/api/LimitingActiveConnectionFactoryFilter.java
@@ -146,16 +146,7 @@ public final class LimitingActiveConnectionFactoryFilter<ResolvedAddress, C exte
          * @return {@link Throwable} representing a connection attempt was refused.
          */
         default Throwable newConnectionRefusedException(ResolvedAddress target) {
-            return new ConnectionLimitExceededException(target.toString());
-        }
-    }
-
-    // visible for tests
-    static final class ConnectionLimitExceededException extends ConnectException {
-        private static final long serialVersionUID = -750567284938958897L;
-
-        private ConnectionLimitExceededException(final String host) {
-            super("No more connections allowed for the host: " + host);
+            return new ConnectException("No more connections allowed for the host: " + target);
         }
     }
 
