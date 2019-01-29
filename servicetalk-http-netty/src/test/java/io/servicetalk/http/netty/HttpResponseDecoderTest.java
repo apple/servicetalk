e333f931bc92c8c428120aebb9e188e1c9e81558
Scott Mitchell <scott_mitchell@apple.com>
2019-01-29 08:28:08 -0800

    Add unit test for HttpResponseDecoder for empty line trailer terminator
    Motivation:
    There was a bug recently found in Netty where the decoder would discard a
    trailer if there was an empty line that terminates the trailers was parsed in a
    separate buffer.
    
    Modifications:
    - Add a unit test to HttpResponseDecoderTest to verify this scenario is handled
    correctly.
    
    Result:
    Better test coverage for HttpResponseDecoder, and confirmation we are not
    subject to the same bug.

--- servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/HttpResponseDecoderTest.java
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/HttpResponseDecoderTest.java
@@ -36,6 +36,9 @@ import static io.netty.buffer.Unpooled.wrappedBuffer;
 import static io.servicetalk.buffer.netty.BufferAllocators.DEFAULT_ALLOCATOR;
 import static io.servicetalk.http.api.HttpHeaderNames.CONNECTION;
 import static io.servicetalk.http.api.HttpHeaderNames.CONTENT_LENGTH;
+import static io.servicetalk.http.api.HttpHeaderNames.TRAILER;
+import static io.servicetalk.http.api.HttpHeaderNames.TRANSFER_ENCODING;
+import static io.servicetalk.http.api.HttpHeaderValues.CHUNKED;
 import static io.servicetalk.http.api.HttpHeaderValues.KEEP_ALIVE;
 import static io.servicetalk.http.api.HttpProtocolVersions.HTTP_1_1;
 import static io.servicetalk.http.api.HttpResponseStatuses.OK;
@@ -135,6 +138,33 @@ public class HttpResponseDecoderTest {
         assertTrue(channel.writeInbound(wrappedBuffer(afterContentBytes)));
         validateHttpResponse(channel, -content.length, true);
         channel.finishAndReleaseAll();
+    }
+
+    @Test
+    public void chunkedWithTrailersEmptyLineSeparateBuffer() {
+        EmbeddedChannel channel = newEmbeddedChannel();
+        byte[] beforeContentBytes = ("HTTP/1.1 200 OK" + "\r\n" +
+                "Connection: keep-alive" + "\r\n" +
+                "Transfer-Encoding: chunked" + "\r\n" +
+                "Server: unit-test" + "\r\n" +
+                "Trailer: My-Trailer" + "\r\n").getBytes(US_ASCII);
+        assertFalse(channel.writeInbound(wrappedBuffer(beforeContentBytes)));
+        assertTrue(channel.writeInbound(wrappedBuffer("\r\n".getBytes(US_ASCII))));
+        assertTrue(channel.writeInbound(wrappedBuffer("0\r\n".getBytes(US_ASCII))));
+        assertTrue(channel.writeInbound(wrappedBuffer("My-Trailer: 42\r\n".getBytes(US_ASCII))));
+        assertTrue(channel.writeInbound(wrappedBuffer("\r\n".getBytes(US_ASCII))));
+
+        HttpResponseMetaData response = channel.readInbound();
+        assertEquals(OK, response.status());
+        assertEquals(HTTP_1_1, response.version());
+        HttpHeaders headers = response.headers();
+        assertStandardHeaders(headers);
+        assertSingleHeaderValue(headers, TRANSFER_ENCODING, CHUNKED);
+        assertSingleHeaderValue(headers, TRAILER, "My-Trailer");
+
+        HttpHeaders trailers = channel.readInbound();
+        assertSingleHeaderValue(trailers, "My-Trailer", "42");
+        assertFalse(channel.finishAndReleaseAll());
     }
 
     @Test
