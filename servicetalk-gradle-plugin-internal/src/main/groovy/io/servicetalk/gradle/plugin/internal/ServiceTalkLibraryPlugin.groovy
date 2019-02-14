60e3519c80e9a0a87ed2d395ce111a1969b45786
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2019-02-13 19:14:25 -0800

    Add docker/ folder

--- servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/ServiceTalkLibraryPlugin.groovy
+++ servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/ServiceTalkLibraryPlugin.groovy
@@ -237,7 +237,7 @@ class ServiceTalkLibraryPlugin extends ServiceTalkCorePlugin {
         // The classpath field must be non-null, but could be empty because it's not required for this task:
         classpath = project.files([])
         source = fileTree(".") {
-          includes = ["*.gradle", "*.properties", "gradle/**"]
+          includes = ["*.gradle", "*.properties", "gradle/**", "docker/**"]
           excludes = ["gradle/wrapper/**"]
         }
       }
