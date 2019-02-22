2c55283afbb11650e42f66f3d50ba2baa4744928
Scott Mitchell <scott_mitchell@apple.com>
2019-02-22 14:16:13 -0800

    AsyncContextInMemoryScopeManager#active() shouldn't return the Scope after it is closed (#329)
    Modifications:
    OpenTracing APIs have a relationship between Scope#close() and
    ScopeManager#active() such that when close() is called active() should no longer
    return that Scope. AsyncContextInMemoryScopeManager currently doesn't do this in
    order to keep lifetime of a Scope isolated to a single filter (e.g. tracing
    filter) but still accessible during the lifetime of the request to other filters
    (e.g. logging/MDC) without tight coupling. However folks that use the
    OpenTracing APIs directly will see incorrect behavior.
    
    Modifications:
    - AsyncContextInMemoryScopeManager#active() should obey OpenTracing API
      restrictions.
    - AsyncContextInMemoryScopeManager should add an alternative method that allows
      us to avoid tight coordination across filter boundaries and shared lifetime
    management. This method is OK with returning Scope objects where close() has
    been called.
    
    Result:
    AsyncContextInMemoryScopeManager more closely follows OpenTracing API
    constraints.

--- servicetalk-opentracing-asynccontext/src/main/java/io/servicetalk/opentracing/asynccontext/AsyncContextInMemoryScopeManager.java
+++ servicetalk-opentracing-asynccontext/src/main/java/io/servicetalk/opentracing/asynccontext/AsyncContextInMemoryScopeManager.java
@@ -24,13 +24,14 @@ import io.servicetalk.opentracing.inmemory.api.InMemorySpan;
 import javax.annotation.Nullable;
 
 import static io.servicetalk.concurrent.api.AsyncContextMap.Key.newKey;
+import static java.util.Objects.requireNonNull;
 
 /**
  * A {@link InMemoryScopeManager} that uses {@link AsyncContext} as the backing storage.
  */
 public final class AsyncContextInMemoryScopeManager implements InMemoryScopeManager {
-    private static final AsyncContextMap.Key<InMemoryScope> SCOPE_KEY = newKey("opentracing");
-    public static final InMemoryScopeManager SCOPE_MANAGER = new AsyncContextInMemoryScopeManager();
+    private static final AsyncContextMap.Key<AsyncContextInMemoryScope> SCOPE_KEY = newKey("opentracing");
+    public static final AsyncContextInMemoryScopeManager SCOPE_MANAGER = new AsyncContextInMemoryScopeManager();
 
     private AsyncContextInMemoryScopeManager() {
         // singleton
@@ -38,29 +39,7 @@ public final class AsyncContextInMemoryScopeManager implements InMemoryScopeMana
 
     @Override
     public InMemoryScope activate(final InMemorySpan span, final boolean finishSpanOnClose) {
-        InMemoryScope scope = new InMemoryScope() {
-            @Override
-            public void close() {
-                if (finishSpanOnClose) {
-                    span.finish();
-                }
-
-                // TODO(scott): the default thread local implementation does something similar,
-                // but since AsyncContext has other infrastructure to save/restore it isn't clear this
-                // is required by the API.
-                // AsyncContext.put(key, oldScope);
-            }
-
-            @Override
-            public InMemorySpan span() {
-                return span;
-            }
-
-            @Override
-            public String toString() {
-                return span.toString();
-            }
-        };
+        AsyncContextInMemoryScope scope = new AsyncContextInMemoryScope(span, finishSpanOnClose);
         AsyncContext.put(SCOPE_KEY, scope);
         return scope;
     }
@@ -68,6 +47,56 @@ public final class AsyncContextInMemoryScopeManager implements InMemoryScopeMana
     @Nullable
     @Override
     public InMemoryScope active() {
+        AsyncContextInMemoryScope scope = AsyncContext.get(SCOPE_KEY);
+        return scope == null || scope.closed ? null : scope;
+    }
+
+    /**
+     * Get the currently {@link #active()} {@link InMemoryScope} or maybe also the current {@link InMemoryScope} for
+     * which {@link InMemoryScope#close()} was previously called.
+     * @return the currently {@link #active()} {@link InMemoryScope} or maybe also the current {@link InMemoryScope} for
+     * which {@link InMemoryScope#close()} was previously called.
+     */
+    @Nullable
+    public InMemoryScope currentScope() {
         return AsyncContext.get(SCOPE_KEY);
+    }
+
+    private static final class AsyncContextInMemoryScope implements InMemoryScope {
+        private final InMemorySpan span;
+        private final boolean finishSpanOnClose;
+        private boolean closed;
+
+        AsyncContextInMemoryScope(InMemorySpan span, boolean finishSpanOnClose) {
+            this.span = requireNonNull(span);
+            this.finishSpanOnClose = finishSpanOnClose;
+        }
+
+        @Override
+        public void close() {
+            if (closed) {
+                return;
+            }
+            closed = true;
+            if (finishSpanOnClose) {
+                span.finish();
+            }
+
+            // The lifetime of a Scope in ServiceTalk is typically owned by a single filter. However this information
+            // is used across filter boundaries for logging, MDC, etc... If calling close() on the Scope also removed
+            // the scope from AsyncContext this would either make it invisible from other filters or introduce a
+            // before/after order dependency which is fragile. Instead we use the closed variable to ensure that
+            // AsyncContextInMemoryScopeManager#active() doesn't return a scope after it has been closed.
+        }
+
+        @Override
+        public InMemorySpan span() {
+            return span;
+        }
+
+        @Override
+        public String toString() {
+            return span.toString();
+        }
     }
 }
---
+++ servicetalk-opentracing-asynccontext/src/test/java/io/servicetalk/opentracing/asynccontext/AsyncContextInMemoryScopeManagerTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.opentracing.asynccontext;
+
+import io.servicetalk.opentracing.inmemory.api.InMemoryScope;
+import io.servicetalk.opentracing.inmemory.api.InMemorySpan;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static io.servicetalk.opentracing.asynccontext.AsyncContextInMemoryScopeManager.SCOPE_MANAGER;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class AsyncContextInMemoryScopeManagerTest {
+    @Mock
+    private InMemorySpan mockSpan;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
+    @Test
+    public void closedScopeNotReturnedByActive() {
+        InMemoryScope scope = SCOPE_MANAGER.activate(mockSpan, true);
+        assertSame(scope, SCOPE_MANAGER.active());
+        assertSame(mockSpan, scope.span());
+        scope.close();
+        assertNull(SCOPE_MANAGER.active());
+    }
+
+    @Test
+    public void closedScopeReturnedByActiveOrClosed() {
+        InMemoryScope scope = SCOPE_MANAGER.activate(mockSpan, true);
+        assertSame(scope, SCOPE_MANAGER.active());
+        assertSame(mockSpan, scope.span());
+        scope.close();
+        assertSame(scope, SCOPE_MANAGER.currentScope());
+    }
+}
