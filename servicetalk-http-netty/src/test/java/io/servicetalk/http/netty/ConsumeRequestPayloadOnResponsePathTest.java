f8224d98dc207cda015ac0b8504d997f2a8ddf5f
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2019-03-04 13:44:31 -0800

    Test users may consume request payload body on response path (#362)
    Motivation:
    
    Verify that users may consume request payload body on response path and
    ST doesn't drain it before that.
    
    Modifications:
    
    Add `ConsumeRequestPayloadOnResponsePathTest` which tests different
    use cases:
      - (ConsumeReqPayload -> SendResponseMeta) -> SendResponsePayload -> SendTrailers;
      - SendResponseMeta -> (ConsumeReqPayload -> SendResponsePayload -> SendTrailers);
      - SendResponseMeta -> (SendResponsePayload -> ConsumeReqPayload)  -> SendTrailers;
      - (SendResponseMeta -> ConsumeReqPayload) -> SendResponsePayload -> SendTrailers;
      - SendResponseMeta -> (SendResponsePayload -> ConsumeReqPayload) -> SendTrailers;
      - SendResponseMeta -> (SendResponsePayload -> SendTrailers -> ConsumeReqPayload);
      - (SendResponseMeta || ConsumeReqPayload) -> SendResponsePayload -> SendTrailers;
      - (ConsumeReqPayload || SendResponseMeta) -> SendResponsePayload -> SendTrailers;
      - SendResponseMeta -> (ConsumeReqPayload || SendResponsePayload) -> SendTrailers;
      - SendResponseMeta -> (ConsumeReqPayload || SendResponsePayload -> SendTrailers);
    
    Result:
    
    Making sure users may consume request payload body on response path.

---
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ConsumeRequestPayloadOnResponsePathTest.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.buffer.api.CompositeBuffer;
+import io.servicetalk.buffer.netty.BufferAllocators;
+import io.servicetalk.concurrent.api.Completable;
+import io.servicetalk.concurrent.api.Single;
+import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
+import io.servicetalk.http.api.BlockingHttpClient;
+import io.servicetalk.http.api.HttpResponse;
+import io.servicetalk.http.api.HttpServiceContext;
+import io.servicetalk.http.api.StreamingHttpRequest;
+import io.servicetalk.http.api.StreamingHttpResponse;
+import io.servicetalk.http.api.StreamingHttpResponseFactory;
+import io.servicetalk.http.api.StreamingHttpServiceFilter;
+import io.servicetalk.transport.api.ServerContext;
+import io.servicetalk.transport.netty.internal.AddressUtils;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+
+import static io.servicetalk.concurrent.api.Publisher.from;
+import static io.servicetalk.concurrent.api.Single.success;
+import static io.servicetalk.http.api.HttpHeaderNames.TRAILER;
+import static io.servicetalk.http.api.HttpResponseStatuses.OK;
+import static io.servicetalk.http.api.HttpSerializationProviders.textSerializer;
+import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class ConsumeRequestPayloadOnResponsePathTest {
+
+    private static final String EXPECTED_REQUEST_PAYLOAD = "ExpectedRequestPayload";
+    private static final String X_TOTAL_LENGTH = "X-Total-Length";
+
+    @Rule
+    public final ServiceTalkTestTimeout timeout = new ServiceTalkTestTimeout();
+
+    private final CountDownLatch waitServer = new CountDownLatch(1);
+    private final AtomicReference<Throwable> errorRef = new AtomicReference<>();
+    private final CompositeBuffer receivedPayload = BufferAllocators.DEFAULT_ALLOCATOR.newCompositeBuffer();
+
+    @Test
+    public void testConsumeRequestPayloadBeforeResponseMetaDataSent() throws Exception {
+        test((responseSingle, request) -> consumePayloadBody(request).concatWith(responseSingle));
+    }
+
+    @Test
+    public void testConsumeRequestPayloadBeforeResponsePayloadSent() throws Exception {
+        test((responseSingle, request) -> responseSingle.flatMap(response -> success(
+                response.transformRawPayloadBody(payloadBody -> consumePayloadBody(request).concatWith(payloadBody)))));
+    }
+
+    @Test
+    public void testConsumeRequestPayloadBeforeTrailersSent() throws Exception {
+        test((responseSingle, request) -> responseSingle.flatMap(response -> success(
+                response.transformPayloadBody(payloadBody -> payloadBody.concatWith(consumePayloadBody(request))))));
+    }
+
+    @Test
+    public void testConsumeRequestPayloadAfterResponseMetaDataSent() throws Exception {
+        test((responseSingle, request) -> responseSingle.concatWith(consumePayloadBody(request)));
+    }
+
+    @Test
+    public void testConsumeRequestPayloadAfterResponsePayloadSent() throws Exception {
+        test((responseSingle, request) -> responseSingle.flatMap(response -> success(
+                response.transformPayloadBody(payloadBody -> payloadBody.concatWith(consumePayloadBody(request))))));
+    }
+
+    @Test
+    public void testConsumeRequestPayloadAfterTrailersSent() throws Exception {
+        test((responseSingle, request) -> responseSingle.flatMap(response -> success(
+                response.transformRawPayloadBody(payloadBody -> payloadBody.concatWith(consumePayloadBody(request))))));
+    }
+
+    @Test
+    public void testSendResponseMetaDataAndConsumeRequestPayload() throws Exception {
+        // TODO: replace flatMap when Single.merge(Completable) is available
+        test((responseSingle, request) -> responseSingle.flatMap(response ->
+                consumePayloadBody(request).concatWith(success(response))));
+    }
+
+    @Test
+    public void testConsumeRequestPayloadAndSendResponseMetaData() throws Exception {
+        test((responseSingle, request) -> consumePayloadBody(request)
+                // TODO: remove toPublisher() when Completable.merge(Single) is available
+                .merge(responseSingle.toPublisher()).toSingleOrError());
+    }
+
+    @Test
+    public void testConsumeRequestPayloadAndResponsePayloadSent() throws Exception {
+        test((responseSingle, request) -> responseSingle.flatMap(response -> success(
+                response.transformPayloadBody(payloadBody -> consumePayloadBody(request).merge(payloadBody)))));
+    }
+
+    // TODO: add testResponsePayloadSentAndConsumeRequestPayload when Publisher.merge(Completable) is available
+
+    @Test
+    public void testConsumeRequestPayloadAndTrailersSent() throws Exception {
+        test((responseSingle, request) -> responseSingle.flatMap(response -> success(
+                response.transformRawPayloadBody(payloadBody -> consumePayloadBody(request).merge(payloadBody)))));
+    }
+    // TODO: add testTrailersSentAndConsumeRequestPayload when Publisher.merge(Completable) is available
+
+    private Completable consumePayloadBody(final StreamingHttpRequest request) {
+        return request.payloadBody().doBeforeNext(receivedPayload::addBuffer)
+                .ignoreElements()
+                .doBeforeError(errorRef::set)
+                .doAfterFinally(waitServer::countDown);
+    }
+
+    private void test(final BiFunction<Single<StreamingHttpResponse>, StreamingHttpRequest,
+            Single<StreamingHttpResponse>> consumeRequestPayload) throws Exception {
+
+        try (ServerContext serverContext = HttpServers.forAddress(localAddress(0))
+                .appendServiceFilter(service -> new StreamingHttpServiceFilter(service) {
+                    @Override
+                    public Single<StreamingHttpResponse> handle(final HttpServiceContext ctx,
+                                                                final StreamingHttpRequest request,
+                                                                final StreamingHttpResponseFactory responseFactory) {
+                        return consumeRequestPayload.apply(delegate().handle(ctx, request, responseFactory), request);
+                    }
+                })
+                .listenStreamingAndAwait((ctx, request, responseFactory) -> {
+                    final StreamingHttpResponse response = responseFactory.ok()
+                            .addHeader(TRAILER, X_TOTAL_LENGTH)
+                            .payloadBody(from("Response\n", "Payload\n", "Body\n"), textSerializer())
+                            .transform(AtomicInteger::new, (chunk, total) -> {
+                                total.addAndGet(chunk.readableBytes());
+                                return chunk;
+                            }, (total, trailers) -> trailers.add(X_TOTAL_LENGTH, String.valueOf(total.get())));
+
+                    return success(response);
+                })) {
+
+            HttpResponse response;
+            try (BlockingHttpClient client = HttpClients.forSingleAddress(AddressUtils.serverHostAndPort(serverContext))
+                    .buildBlocking()) {
+                response = client.request(client.post("/").payloadBody(EXPECTED_REQUEST_PAYLOAD, textSerializer()));
+            }
+
+            waitServer.await();
+            assertThat(response.status(), is(OK));
+            assertThat("Request payload body might be consumed by someone else", errorRef.get(), is(nullValue()));
+            assertThat(receivedPayload.toString(UTF_8), is(EXPECTED_REQUEST_PAYLOAD));
+            assertThat(response.headers().contains(TRAILER, X_TOTAL_LENGTH), is(true));
+            assertThat(response.trailers().contains(X_TOTAL_LENGTH), is(true));
+            assertThat(response.trailers().get(X_TOTAL_LENGTH).toString(),
+                    is(Integer.toString(response.payloadBody().readableBytes())));
+        }
+    }
+}
