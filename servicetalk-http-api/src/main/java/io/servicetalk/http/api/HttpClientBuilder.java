30e3bb0ad3db722cd8e61b8b4ebb96fa21a03f51
Derek Lewis <derek_a_lewis@apple.com>
2019-03-08 15:40:50 -0800

    Extract common methods from http client and connection builders.
    Motivation:
    
    We whould like to share a common interface between client and connection
    builders, for things that are common across them.
    
    Modifications:
    
    - Create base class with common methods.
    - Move Javadoc to base class.
    
    Results:
    
    Both sets of builders inherit from a common interface with the shared
    settings.

--- servicetalk-http-api/src/main/java/io/servicetalk/http/api/HttpClientBuilder.java
+++ servicetalk-http-api/src/main/java/io/servicetalk/http/api/HttpClientBuilder.java
@@ -40,168 +40,56 @@ import static java.util.Objects.requireNonNull;
  * @param <R> the type of address after resolution (resolved address)
  * @param <SDE> the type of {@link ServiceDiscovererEvent}
  */
-abstract class HttpClientBuilder<U, R, SDE extends ServiceDiscovererEvent<R>> {
+abstract class HttpClientBuilder<U, R, SDE extends ServiceDiscovererEvent<R>> extends BaseHttpBuilder<R> {
     /**
      * An {@link HttpExecutionStrategy} to use when there is none specified on the {@link HttpClientBuilder}.
      */
     static final HttpExecutionStrategy DEFAULT_BUILDER_STRATEGY = defaultStrategy();
 
-    /**
-     * Sets the {@link HttpExecutionStrategy} for all clients created from this {@link HttpClientBuilder}.
-     *
-     * @param strategy {@link HttpExecutionStrategy} to use.
-     * @return {@code this}.
-     */
-    public abstract HttpClientBuilder<U, R, SDE> executionStrategy(HttpExecutionStrategy strategy);
-
-    /**
-     * Sets the {@link IoExecutor} for all clients created from this {@link HttpClientBuilder}.
-     *
-     * @param ioExecutor {@link IoExecutor} to use.
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> ioExecutor(IoExecutor ioExecutor);
 
-    /**
-     * Sets the {@link BufferAllocator} for all clients created from this {@link HttpClientBuilder}.
-     *
-     * @param allocator {@link BufferAllocator} to use.
-     * @return {@code this}.
-     */
+    @Override
+    public abstract HttpClientBuilder<U, R, SDE> executionStrategy(HttpExecutionStrategy strategy);
+
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> bufferAllocator(BufferAllocator allocator);
 
-    /**
-     * Add a {@link SocketOption} for all connections created by this client.
-     *
-     * @param option the option to apply.
-     * @param value the value.
-     * @param <T> the type of the value.
-     * @return {@code this}.
-     */
+    @Override
     public abstract <T> HttpClientBuilder<U, R, SDE> socketOption(SocketOption<T> option, T value);
 
-    /**
-     * Enable wire-logging for connections created by this builder. All wire events will be logged at trace level.
-     *
-     * @param loggerName The name of the logger to log wire events.
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> enableWireLogging(String loggerName);
 
-    /**
-     * Disable previously configured wire-logging for connections created by this builder.
-     * If wire-logging has not been configured before, this method has no effect.
-     *
-     * @return {@code this}.
-     * @see #enableWireLogging(String)
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> disableWireLogging();
 
-    /**
-     * Set the {@link HttpHeadersFactory} to be used for creating {@link HttpHeaders} when decoding responses.
-     *
-     * @param headersFactory the {@link HttpHeadersFactory} to use.
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> headersFactory(HttpHeadersFactory headersFactory);
 
-    /**
-     * Set the maximum size of the initial HTTP line for created {@link StreamingHttpClient}.
-     *
-     * @param maxInitialLineLength The {@link StreamingHttpClient} will throw TooLongFrameException if the initial HTTP
-     * line exceeds this length.
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> maxInitialLineLength(int maxInitialLineLength);
 
-    /**
-     * Set the maximum total size of HTTP headers, which could be send be created {@link StreamingHttpClient}.
-     *
-     * @param maxHeaderSize The {@link StreamingHttpClient} will throw TooLongFrameException if the total size of all
-     * HTTP headers exceeds this length.
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> maxHeaderSize(int maxHeaderSize);
 
-    /**
-     * Set the value used to calculate an exponential moving average of the encoded size of the initial line and the
-     * headers for a guess for future buffer allocations.
-     *
-     * @param headersEncodedSizeEstimate An estimated size of encoded initial line and headers.
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> headersEncodedSizeEstimate(int headersEncodedSizeEstimate);
 
-    /**
-     * Set the value used to calculate an exponential moving average of the encoded size of the trailers for a guess for
-     * future buffer allocations.
-     *
-     * @param trailersEncodedSizeEstimate An estimated size of encoded trailers.
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> trailersEncodedSizeEstimate(int trailersEncodedSizeEstimate);
 
-    /**
-     * Set the maximum number of pipelined HTTP requests to queue up, anything above this will be rejected,
-     * 1 means pipelining is disabled and requests and responses are processed sequentially.
-     * <p>
-     * Request pipelining requires HTTP 1.1.
-     *
-     * @param maxPipelinedRequests number of pipelined requests to queue up
-     * @return {@code this}.
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> maxPipelinedRequests(int maxPipelinedRequests);
 
-    /**
-     * Append the filter to the chain of filters used to decorate the {@link StreamingHttpConnection} created by this
-     * builder.
-     * <p>
-     * Filtering allows you to wrap a {@link StreamingHttpConnection} and modify behavior during request/response
-     * processing
-     * Some potential candidates for filtering include logging, metrics, and decorating responses.
-     * <p>
-     * The order of execution of these filters are in order of append. If 3 filters are added as follows:
-     * <pre>
-     *     builder.append(filter1).append(filter2).append(filter3)
-     * </pre>
-     * making a request to a connection wrapped by this filter chain the order of invocation of these filters will be:
-     * <pre>
-     *     filter1 =&gt; filter2 =&gt; filter3 =&gt; connection
-     * </pre>
-     * @param factory {@link HttpConnectionFilterFactory} to decorate a {@link StreamingHttpConnection} for the purpose
-     * of filtering.
-     * @return {@code this}
-     */
+    @Override
     public abstract HttpClientBuilder<U, R, SDE> appendConnectionFilter(HttpConnectionFilterFactory factory);
 
-    /**
-     * Append the filter to the chain of filters used to decorate the {@link StreamingHttpConnection} created by this
-     * builder, for every request that passes the provided {@link Predicate}.
-     * <p>
-     * Filtering allows you to wrap a {@link StreamingHttpConnection} and modify behavior during request/response
-     * processing
-     * Some potential candidates for filtering include logging, metrics, and decorating responses.
-     * <p>
-     * The order of execution of these filters are in order of append. If 3 filters are added as follows:
-     * <pre>
-     *     builder.append(filter1).append(filter2).append(filter3)
-     * </pre>
-     * making a request to a connection wrapped by this filter chain the order of invocation of these filters will be:
-     * <pre>
-     *     filter1 =&gt; filter2 =&gt; filter3 =&gt; connection
-     * </pre>
-     * @param predicate the {@link Predicate} to test if the filter must be applied.
-     * @param factory {@link HttpConnectionFilterFactory} to decorate a {@link StreamingHttpConnection} for the purpose
-     * of filtering.
-     * @return {@code this}
-     */
+    @Override
     public HttpClientBuilder<U, R, SDE> appendConnectionFilter(Predicate<StreamingHttpRequest> predicate,
-                                                                HttpConnectionFilterFactory factory) {
-        requireNonNull(predicate);
-        requireNonNull(factory);
-
-        return appendConnectionFilter(connection ->
-                new ConditionalHttpConnectionFilter(predicate, factory.create(connection), connection));
+                                                               HttpConnectionFilterFactory factory) {
+        super.appendConnectionFilter(predicate, factory);
+        return this;
     }
 
     /**
