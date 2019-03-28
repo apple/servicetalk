07d77053c53b9d2bd68619137d8f152d483357c8
Derek Lewis <derek_a_lewis@apple.com>
2019-03-28 10:46:08 -0700

    Fixing static import quirk introduced by PR #470

--- servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/SpliceFlatStreamToMetaSingleTest.java
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/SpliceFlatStreamToMetaSingleTest.java
@@ -256,7 +256,7 @@ public class SpliceFlatStreamToMetaSingleTest {
         // We use Publisher.just() here to make sure the Publisher invokes onError when onNext throws.
         // TestPublisher used in other cases, does not show that behavior. Instead it throws from sendItems() which is
         // less obvious failure message than what we get with dataSubscriber.verifyFailure(DELIBERATE_EXCEPTION);
-        Publisher<Object> stream = Publisher.from(metaData);
+        Publisher<Object> stream = from(metaData);
         SpliceFlatStreamToMetaSingle<Data, MetaData, Payload> op = new SpliceFlatStreamToMetaSingle<>(
                 stream, (metaData, payload) -> {
                     throw DELIBERATE_EXCEPTION;
