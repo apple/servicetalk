67b9f465f07e690f7bc7b49da0175ffbeead524b
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2019-05-02 10:01:50 -0700

    Throw ReadOnlyBufferException from read-only Buffer implementations (#530)
    Motivation:
    
    1. `ReadOnlyByteBuffer` throws `UnsupportedOperationException` instead
    of `ReadOnlyBufferException`, which is inconsistent with read-only
    implementations of `java.nio.ByteBuffer` and `io.netty.buffer.ByteBuf`.
    2. Some methods of `ReadOnlyByteBuffer` and `ReadOnlyBuffer` do not
    throw `ReadOnlyByteBuffer` when they should.
    
    Modifications:
    
    - Throw `ReadOnlyBufferException` instead of
    `UnsupportedOperationException` in `ReadOnlyByteBuffer`;
    - Throw `ReadOnlyBufferException` from `ReadOnlyByteBuffer.hasArray()`,
    `ReadOnlyByteBuffer.array()` and `ReadOnlyByteBuffer.arrayOffset()`;
    - Throw `ReadOnlyBufferException` from
    `ReadOnlyBuffer.writeUtf8(CharSequence, int)`;
    - Throw `ReadOnlyBufferException` from `ensureWritable(int)`;
    - Make `ensureWritable(int, boolean)` consistent between
    `ReadOnlyByteBuffer` and `ReadOnlyBuffer`;
    - Add `@throws ReadOnlyBufferException` to `Buffer` javadoc where
    applicable;
    
    Result:
    
    ServiceTalk implementation of read-only `Buffer`s correctly throw
    `ReadOnlyBufferException` when necessary.

--- servicetalk-buffer-netty/src/main/java/io/servicetalk/buffer/netty/ReadOnlyBuffer.java
+++ servicetalk-buffer-netty/src/main/java/io/servicetalk/buffer/netty/ReadOnlyBuffer.java
@@ -76,8 +76,13 @@ final class ReadOnlyBuffer extends WrappedBuffer {
     }
 
     @Override
-    public Buffer writerIndex(int writerIndex) {
+    public Buffer ensureWritable(int minWritableBytes) {
         throw new ReadOnlyBufferException();
+    }
+
+    @Override
+    public int ensureWritable(int minWritableBytes, boolean force) {
+        return 1;
     }
 
     @Override
@@ -312,6 +317,11 @@ final class ReadOnlyBuffer extends WrappedBuffer {
 
     @Override
     public Buffer writeUtf8(CharSequence seq) {
+        throw new ReadOnlyBufferException();
+    }
+
+    @Override
+    public Buffer writeUtf8(CharSequence seq, int ensureWritable) {
         throw new ReadOnlyBufferException();
     }
 
