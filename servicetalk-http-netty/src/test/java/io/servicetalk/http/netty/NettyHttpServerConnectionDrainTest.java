4fdaadebe3f6d38f7a0a18949a084e9b06254a86
Jo Voordeckers <jvoordeckers@apple.com>
2019-05-10 11:45:09 -0700

    Address comments

--- servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/NettyHttpServerConnectionDrainTest.java
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/NettyHttpServerConnectionDrainTest.java
@@ -15,15 +15,14 @@
  */
 package io.servicetalk.http.netty;
 
-import io.servicetalk.concurrent.api.Single;
+import io.servicetalk.concurrent.api.Completable;
 import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
 import io.servicetalk.http.api.BlockingHttpClient;
-import io.servicetalk.http.api.HttpExecutionStrategy;
 import io.servicetalk.http.api.HttpResponse;
-import io.servicetalk.http.api.StreamingHttpClientFilter;
-import io.servicetalk.http.api.StreamingHttpRequest;
-import io.servicetalk.http.api.StreamingHttpRequester;
-import io.servicetalk.http.api.StreamingHttpResponse;
+import io.servicetalk.http.api.HttpServerBuilder;
+import io.servicetalk.http.api.StreamingHttpClient;
+import io.servicetalk.http.api.StreamingHttpService;
+import io.servicetalk.transport.api.ExecutionContext;
 import io.servicetalk.transport.api.ServerContext;
 import io.servicetalk.transport.netty.internal.AddressUtils;
 
@@ -32,27 +31,26 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
+import java.net.SocketAddress;
 import java.net.StandardSocketOptions;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
+import javax.annotation.Nullable;
 
 import static io.servicetalk.concurrent.api.Publisher.from;
 import static io.servicetalk.concurrent.api.Single.succeeded;
+import static io.servicetalk.concurrent.internal.FutureUtils.awaitTermination;
 import static io.servicetalk.http.api.HttpSerializationProviders.textDeserializer;
 import static io.servicetalk.http.api.HttpSerializationProviders.textSerializer;
 import static io.servicetalk.transport.netty.internal.AddressUtils.serverHostAndPort;
-import static java.lang.Boolean.parseBoolean;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.fail;
 
 public class NettyHttpServerConnectionDrainTest {
-
-    @Rule
-    public final Timeout timeout = new ServiceTalkTestTimeout();
-
     private static final String LARGE_TEXT;
 
     static {
@@ -65,99 +63,137 @@ public class NettyHttpServerConnectionDrainTest {
         LARGE_TEXT = sb.toString();
     }
 
+    @Rule
+    public final Timeout timeout = new ServiceTalkTestTimeout();
+
     @Test
     public void requestIsAutoDrainedWhenUserFailsToConsume() throws Exception {
-        try (ServerContext serverContext = HttpServers.forAddress(AddressUtils.localAddress(0))
-                // Ensures small TCP buffer such that our large request payload will fill up to make sure we can't
-                // complete the request without at least reading more data
-                .socketOption(StandardSocketOptions.SO_RCVBUF, 64)
-                .enableDrainingRequestPayloadBody() // This is enabled by default
-                .listenStreamingAndAwait((ctx, request, responseFactory) -> {
-                    // User fails to consume payload, expect auto-draining to take care of it
-                    return succeeded(responseFactory.ok().payloadBody(from("OK"), textSerializer()));
-                });
-
-             BlockingHttpClient client = HttpClients.forSingleAddress(serverHostAndPort(serverContext))
-                     .buildBlocking()) {
-
-            HttpResponse response = client.request(client.post("/").payloadBody(LARGE_TEXT, textSerializer()));
-            assertThat(response.payloadBody(textDeserializer()), equalTo("OK"));
+        BlockingHttpClient client = null;
+        try (ServerContext serverContext = server(true, respondOkWithoutReadingRequest())) {
+            client = HttpClients.forSingleAddress(serverHostAndPort(serverContext))
+                    .buildBlocking();
+            postLargePayloadAndAssertResponseOk(client);
+        } finally {
+            closeClient(client);
         }
     }
 
     @Test
     public void requestIsDrainedByUserWithDrainingDisabled() throws Exception {
-        try (ServerContext serverContext = HttpServers.forAddress(AddressUtils.localAddress(0))
-                // Ensures small TCP buffer such that our large request payload will fill up to make sure we can't
-                // complete the request without at least reading more data
-                .socketOption(StandardSocketOptions.SO_RCVBUF, 64)
-                // No auto-draining of payload, user needs to drain
-                .disableDrainingRequestPayloadBody()
-                .listenStreamingAndAwait((ctx, request, responseFactory) ->
-                        request.payloadBody().ignoreElements() // User consumes payload (ignoring)
-                                .concat(succeeded(responseFactory.ok().payloadBody(from("OK"), textSerializer()))));
-
+        try (ServerContext serverContext = server(false, (ctx, request, responseFactory) ->
+                request.payloadBody().ignoreElements() // User consumes payload (ignoring)
+                        .concat(succeeded(responseFactory.ok().payloadBody(from("OK"), textSerializer()))));
              BlockingHttpClient client = HttpClients.forSingleAddress(serverHostAndPort(serverContext))
                      .buildBlocking()) {
 
-            HttpResponse response = client.request(client.post("/").payloadBody(LARGE_TEXT, textSerializer()));
-            assertThat(response.payloadBody(textDeserializer()), equalTo("OK"));
+            postLargePayloadAndAssertResponseOk(client);
         }
     }
 
     @Test
     public void requestIsConsumedByUserWithDrainingEnabled() throws Exception {
         AtomicReference<String> resultRef = new AtomicReference<>();
-        try (ServerContext serverContext = HttpServers.forAddress(AddressUtils.localAddress(0))
-                // Ensures small TCP buffer such that our large request payload will fill up to make sure we can't
-                // complete the request without at least reading more data
-                .socketOption(StandardSocketOptions.SO_RCVBUF, 64)
-                .enableDrainingRequestPayloadBody() // This is enabled by default, should not cause issues for the user
-                .listenStreamingAndAwait((ctx, request, responseFactory) ->
-                        request.payloadBody()
-                                // User consumes payload and stores for assert ensuring auto-draining doesn't break it
-                                .collect(() -> new StringBuilder(LARGE_TEXT.length()),
-                                        (sb, b) -> sb.append(b.toString(CharsetUtil.US_ASCII)))
-                                .map(StringBuilder::toString)
-                                .whenOnSuccess(resultRef::set)
-                                .toCompletable()
-                                .concat(succeeded(responseFactory.ok().payloadBody(from("OK"), textSerializer()))));
+        try (ServerContext serverContext = server(true, (ctx, request, responseFactory) ->
+                request.payloadBody()
+                        // User consumes payload and stores for assert ensuring auto-draining doesn't break it
+                        .collect(() -> new StringBuilder(LARGE_TEXT.length()),
+                                (sb, b) -> sb.append(b.toString(CharsetUtil.US_ASCII)))
+                        .map(StringBuilder::toString)
+                        .whenOnSuccess(resultRef::set)
+                        .toCompletable()
+                        .concat(succeeded(responseFactory.ok().payloadBody(from("OK"), textSerializer()))));
 
              BlockingHttpClient client = HttpClients.forSingleAddress(serverHostAndPort(serverContext))
                      .buildBlocking()) {
 
-            HttpResponse response = client.request(client.post("/").payloadBody(LARGE_TEXT, textSerializer()));
-            assertThat(response.payloadBody(textDeserializer()), equalTo("OK"));
+            postLargePayloadAndAssertResponseOk(client);
             assertThat(resultRef.get(), equalTo(LARGE_TEXT));
         }
     }
 
     @Test(expected = TimeoutException.class)
     public void requestTimesOutWithoutAutoDrainingOrUserConsuming() throws Exception {
-        try (ServerContext serverContext = HttpServers.forAddress(AddressUtils.localAddress(0))
-                // Ensures small TCP buffer such that our large request payload will fill up to make sure we can't
-                // complete the request without at least reading more data
-                .socketOption(StandardSocketOptions.SO_RCVBUF, 64)
-                // With draining disabled the request hangs
-                .disableDrainingRequestPayloadBody()
-                .listenStreamingAndAwait((ctx, request, responseFactory) ->
-                        succeeded(responseFactory.ok().payloadBody(from("OK"), textSerializer())));
+        final CountDownLatch latch = new CountDownLatch(1);
+        StreamingHttpClient client = null;
+        try (ServerContext serverContext = server(false, respondOkWithoutReadingRequest(latch::countDown))) {
 
-             BlockingHttpClient client = HttpClients.forSingleAddress(serverHostAndPort(serverContext))
-                     .appendClientFilter((client1, lbEvents) -> new StreamingHttpClientFilter(client1) {
-                         @Override
-                         protected Single<StreamingHttpResponse> request(final StreamingHttpRequester delegate,
-                                                                         final HttpExecutionStrategy strategy,
-                                                                         final StreamingHttpRequest request) {
-                             // Without draining the request is expected to hang, don't wait too long unless on CI
-                             int timeoutSeconds = ServiceTalkTestTimeout.CI ? 15 : 1;
-                             return delegate.request(strategy, request).idleTimeout(timeoutSeconds, SECONDS);
-                         }
-                     })
-                     .buildBlocking()) {
+            client = HttpClients.forSingleAddress(serverHostAndPort(serverContext)).buildStreaming();
 
-            client.request(client.post("/").payloadBody(LARGE_TEXT, textSerializer()));
+            client.request(client.post("/").payloadBody(from(LARGE_TEXT), textSerializer()))
+                    // Subscribe to send the request, don't care about the response since graceful close of the server
+                    // will hang until the request is consumed, thus we expect the timeout to hit
+                    .ignoreElement().subscribe();
+
+            latch.await(); // Wait till the request is received before initiating graceful close of the server
+        } finally {
+            closeClient(client);
         }
         fail("Request should not complete normally");
+    }
+
+    private void closeClient(@Nullable final AutoCloseable client) throws Exception {
+        if (client != null) {
+            client.close();
+        }
+    }
+
+    private void postLargePayloadAndAssertResponseOk(final BlockingHttpClient client) throws Exception {
+        HttpResponse response = client.request(client.post("/").payloadBody(LARGE_TEXT, textSerializer()));
+        assertThat(response.payloadBody(textDeserializer()), equalTo("OK"));
+    }
+
+    private StreamingHttpService respondOkWithoutReadingRequest(Runnable onRequest) {
+        return (ctx, request, responseFactory) -> {
+            onRequest.run();
+            return succeeded(responseFactory.ok().payloadBody(from("OK"), textSerializer()));
+        };
+    }
+
+    private StreamingHttpService respondOkWithoutReadingRequest() {
+        return respondOkWithoutReadingRequest(() -> { });
+    }
+
+    private ServerContext server(boolean autoDrain, StreamingHttpService handler) throws Exception {
+        HttpServerBuilder httpServerBuilder = HttpServers.forAddress(AddressUtils.localAddress(0))
+                // Ensures small TCP buffer such that our large request payload will fill up to make sure we can't
+                // complete the request without at least reading more data
+                .socketOption(StandardSocketOptions.SO_RCVBUF, 64);
+
+        if (autoDrain) {
+            httpServerBuilder = httpServerBuilder.enableDrainingRequestPayloadBody();
+        } else {
+            httpServerBuilder = httpServerBuilder.disableDrainingRequestPayloadBody();
+        }
+        ServerContext serverContext = httpServerBuilder
+                .listenStreamingAndAwait(handler);
+        return new ServerContext() {
+            @Override
+            public SocketAddress listenAddress() {
+                return serverContext.listenAddress();
+            }
+
+            @Override
+            public ExecutionContext executionContext() {
+                return serverContext.executionContext();
+            }
+
+            @Override
+            public Completable onClose() {
+                return serverContext.onClose();
+            }
+
+            @Override
+            public Completable closeAsync() {
+                return serverContext.closeAsync();
+            }
+
+            @Override
+            public void close() {
+                // Without draining the request is expected to hang, don't wait too long unless on CI
+                int timeoutSeconds = ServiceTalkTestTimeout.CI ? 15 : 1;
+                awaitTermination(serverContext.closeAsyncGracefully()
+                        .idleTimeout(timeoutSeconds, SECONDS).toFuture());
+            }
+        };
     }
 }
