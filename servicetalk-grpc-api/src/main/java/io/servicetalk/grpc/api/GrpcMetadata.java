1210a01bd6d6fe1d6348ef7f9bcf5df0a5e92826
Nitesh Kant <nitesh_kant@apple.com>
2019-07-18 16:17:09 -0700

    Grpc user facing APIs (#702)
    __Motivation__
    
    As the first step towards implementing gRPC we need the following:
    
    - Builder APIs to create gRPC based client/server
    - Helper classes required for generated stubs.
    
    __Modification__
    
    Added the following APIs:
    
    - Grpc client/server builders.
    - Client and service interfaces for blocking and async programming models.
    - Client call factory to be used by generated client stubs.
    - `GrpcRoutes` to add routes from the generated service factory code.
    - Service factory to be used by generated service stubs.
    - Client/Server filter factory.
    
    __Result__
    
    gRPC user facing APIs and helper classes to be used by stubs

---
+++ servicetalk-grpc-api/src/main/java/io/servicetalk/grpc/api/GrpcMetadata.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.grpc.api;
+
+import java.time.Duration;
+import javax.annotation.Nullable;
+
+/**
+ * Metadata for a <a href="https://www.grpc.io">gRPC</a> call.
+ */
+public interface GrpcMetadata {
+
+    /**
+     * Returns the path for the associated <a href="https://www.grpc.io">gRPC</a> method.
+     *
+     * @return The path for the associated <a href="https://www.grpc.io">gRPC</a> method.
+     */
+    String path();
+
+    /**
+     * Returns {@code true} if the associated message is to be sent compressed.
+     *
+     * @return {@code true} if the associated message is to be sent compressed.
+     */
+    boolean isCompressed();
+
+    /**
+     * The associated message will be sent compressed if {@code compressed} is {@code true}.
+     *
+     * @param compressed {@code true} if the associated message is to be sent compressed.
+     */
+    void compressed(boolean compressed);
+
+    /**
+     * Returns a <a href="https://grpc.io/blog/deadlines/">gRPC deadline</a>, if set for the associated RPC method.
+     *
+     * @return {@link Duration} representing the <a href="https://grpc.io/blog/deadlines/">gRPC deadline</a> or
+     * {@code null} if none exists.
+     */
+    @Nullable
+    Duration deadline();
+}
---
+++ servicetalk-grpc-api/src/main/java/io/servicetalk/grpc/api/GrpcStatusCode.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.grpc.api;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nullable;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * Standard gRPC status codes. Copied from {@code io.grpc.Status}.
+ */
+public enum GrpcStatusCode {
+    /** Successful. */
+    OK(0),
+    /** Cancelled (typically by caller). */
+    CANCELLED(1),
+    /** Unknown error. */
+    UNKNOWN(2),
+    /** Client specified an invalid argument. */
+    INVALID_ARGUMENT(3),
+    /** Deadline expired. */
+    DEADLINE_EXCEEDED(4),
+    /** Some requested entity not found. */
+    NOT_FOUND(5),
+    /** Some entity that we attempted to create already exists. */
+    ALREADY_EXISTS(6),
+    /** Permission denied for a particular clinet. Different from UNAUTHENTICATED. */
+    PERMISSION_DENIED(7),
+    /** Resource exhausted. */
+    RESOURCE_EXHAUSTED(8),
+    /** The action cannot be executed on the current system state. Client should not retry.. */
+    FAILED_PRECONDITION(9),
+    /** Aborted, typically due to a concurrency issue (think CAS). Client may retry the whole sequence.. */
+    ABORTED(10),
+    /** Used for range errors. */
+    OUT_OF_RANGE(11),
+    /** Unimplemented action. */
+    UNIMPLEMENTED(12),
+    /** Internal invariant violated. */
+    INTERNAL(13),
+    /** Service unavailable, similar to 503, client may retry. */
+    UNAVAILABLE(14),
+    /** Data corruption. */
+    DATA_LOSS(15),
+    /** Cannot authenticate the client. */
+    UNAUTHENTICATED(16);
+
+    private static final Map<Integer, GrpcStatusCode> INT_TO_STATUS_CODE_MAP;
+    static {
+        INT_TO_STATUS_CODE_MAP = new HashMap<>(GrpcStatusCode.values().length);
+        for (GrpcStatusCode code : GrpcStatusCode.values()) {
+            GrpcStatusCode replaced = INT_TO_STATUS_CODE_MAP.put(code.value(), code);
+            if (replaced != null) {
+                throw new IllegalStateException(String.format("GrpcStatusCode value %d used by both %s and %s",
+                        code.value(), replaced, code));
+            }
+        }
+    }
+
+    private final int value;
+
+    GrpcStatusCode(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Obtains the status code given a code value.
+     * @param codeValue code value.
+     * @return status code associated with the code value, or {@link #UNKNOWN}.
+     */
+    public static GrpcStatusCode fromCodeValue(@Nullable CharSequence codeValue) {
+        if (codeValue == null) {
+            return UNKNOWN;
+        }
+        try {
+            return fromCodeValue(parseInt(codeValue.toString()));
+        } catch (NumberFormatException e) {
+            return UNKNOWN;
+        }
+    }
+
+    /**
+     * Obtains the status code given an integer code value.
+     * @param codeValue integer code value.
+     * @return status code associated with the code value, or {@link #UNKNOWN}.
+     */
+    public static GrpcStatusCode fromCodeValue(int codeValue) {
+        return INT_TO_STATUS_CODE_MAP.getOrDefault(codeValue, UNKNOWN);
+    }
+
+    /**
+     * Returns the integer code value.
+     * @return the integer code value.
+     */
+    public int value() {
+        return value;
+    }
+
+    /**
+     * Returns a standard {@link GrpcStatus} with this status code.
+     * @return a standard {@link GrpcStatus} with this status code.
+     */
+    public GrpcStatus status() {
+        return GrpcStatus.CACHED_INSTANCES.get(value);
+    }
+}
