e2b60ddacadc0d1aa42c68663c58d85733e0227a
David Dossot <ddossot@apple.com>
2019-07-29 08:24:34 -0700

    Add core insfrastructure for gRPC protoc and Gradle plugins (#716)
    __Motivation__
    
    We need to generate custom client and server code for gRPC and for this need a custom protoc plugin.
    Configuring protoc custom plugins in Gradle can be a bit tedious so we want to provide a custom plugin to help.
    
    __Modifications__
    
    - Add a protoc plugin skeleton
    - Add a basic Gradle plugin that configures protoc with our plugin
    - Add docs for this Gradle plugin
    - Use this plugin in the gRPC example but for now do not yet use its outcome
    
    __Results__
    
    gRPC codegen is afoot.

---
+++ servicetalk-grpc-gradle-plugin/src/main/groovy/io/servicetalk/grpc/gradle/plugin/ServiceTalkGrpcPlugin.groovy
@@ -0,0 +1,158 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.grpc.gradle.plugin
+
+import org.gradle.api.GradleException
+import org.gradle.api.InvalidUserCodeException
+import org.gradle.api.InvalidUserDataException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.plugins.ide.eclipse.EclipsePlugin
+import org.gradle.plugins.ide.idea.IdeaPlugin
+import org.gradle.plugins.ide.idea.model.IdeaModel
+import org.gradle.tooling.model.idea.IdeaModule
+import org.gradle.util.GradleVersion
+
+class ServiceTalkGrpcPlugin implements Plugin<Project> {
+  void apply(Project project) {
+    if (GradleVersion.current().baseVersion < GradleVersion.version("4.10")) {
+      throw new GradleException("This plugin requires Gradle 4.10 or higher.")
+    }
+
+    if (project.plugins.hasPlugin("com.google.protobuf")) {
+      throw new InvalidUserCodeException("Plugin `servicetalk-grpc` needs to be applied " +
+          "*before* plugin `com.google.protobuf`. (The latter is applied automatically by the former.)")
+    }
+
+    ServiceTalkGrpcExtension extension = project.extensions.create("serviceTalkGrpc", ServiceTalkGrpcExtension)
+    extension.conventionMapping.generatedCodeDir = { project.file("$project.buildDir/generated/source/proto") }
+
+    project.afterEvaluate {
+      Properties pluginProperties = new Properties()
+      pluginProperties.load(getClass().getResourceAsStream("/META-INF/gradle-plugins/servicetalk-grpc.properties"))
+      def serviceTalkVersion = pluginProperties."implementation-version"
+      if (serviceTalkVersion == null) {
+        throw new IllegalStateException("Failed to retrieve ServiceTalk version from plugin meta.")
+      }
+
+      def protobufVersion = extension.protobufVersion
+      if (protobufVersion == null) {
+        throw new InvalidUserDataException("Please set `serviceTalkGrpc.protobufVersion`.")
+      }
+
+      def serviceTalkProtocPluginPath = extension.serviceTalkProtocPluginPath
+
+      project.configure(project) {
+        Task ideaTask = extension.generateIdeConfiguration ? project.tasks.findByName("ideaModule") : null
+        Task eclipseTask = extension.generateIdeConfiguration ? project.tasks.findByName("eclipse") : null
+
+        protobuf {
+          protoc {
+            artifact = "com.google.protobuf:protoc:$protobufVersion"
+          }
+
+          plugins {
+            servicetalk_grpc {
+              if (serviceTalkProtocPluginPath != null) {
+                path = file(serviceTalkProtocPluginPath)
+              } else {
+                artifact = "io.servicetalk:servicetalk-grpc-protoc:$serviceTalkVersion"
+              }
+            }
+          }
+
+          generateProtoTasks {
+            all().each { task ->
+              task.plugins {
+                servicetalk_grpc {
+                  outputSubDir = "java"
+                }
+              }
+
+              if (ideaTask != null) {
+                ideaTask.dependsOn(task)
+              }
+              if (eclipseTask != null) {
+                eclipseTask.dependsOn(task)
+              }
+            }
+          }
+
+          generatedFilesBaseDir = project.file(extension.generatedCodeDir).absolutePath
+        }
+
+        clean {
+          delete protobuf.generatedFilesBaseDir
+        }
+
+        if (extension.generateIdeConfiguration) {
+          def generatedMainDir = project.file("$extension.generatedCodeDir/main/java")
+          def generatedTestDir = project.file("$extension.generatedCodeDir/test/java")
+
+          project.plugins.withType(IdeaPlugin) {
+            project.idea.module {
+              sourceDirs += [generatedMainDir]
+              testSourceDirs += [generatedTestDir]
+              generatedSourceDirs += [generatedMainDir, generatedTestDir]
+            }
+          }
+
+          project.plugins.withType(EclipsePlugin) {
+            def addOptionalAttributesNode = { node ->
+              def attributesNode = new Node(node, 'attributes')
+              def attributeNode = new Node(attributesNode, 'attribute', [name: 'optional', value: 'true'])
+              node.append(attributeNode)
+              return node
+            }
+
+            project.eclipse {
+              classpath {
+                file {
+                  withXml {
+                    def node = it.asNode()
+                    addOptionalAttributesNode(new Node(node, 'classpathentry', [kind: 'src', path: project.relativePath(generatedMainDir)]))
+                    addOptionalAttributesNode(new Node(node, 'classpathentry', [kind: 'src', path: project.relativePath(generatedTestDir)]))
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // Google's plugin processes its configuration in an afterEvaluate callback,
+    // hence we need to apply it *after* registering our own afterEvaluate callback
+    project.plugins.apply("com.google.protobuf")
+
+    project.afterEvaluate {
+      // Protobuf plugin forcefully adds to IDEA's model
+      if (!extension.generateIdeConfiguration) {
+        IdeaModel ideaModel = project.getExtensions().findByType(IdeaModel)
+
+        if (ideaModel != null) {
+          def generatedMainDir = project.file("$extension.generatedCodeDir/main/java")
+          ideaModel.module.sourceDirs.remove(generatedMainDir)
+
+          def generatedTestDir = project.file("$extension.generatedCodeDir/test/java")
+          ideaModel.module.testSourceDirs.remove(generatedTestDir)
+        }
+      }
+    }
+  }
+}
