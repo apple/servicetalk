af826985961a6bbed2823782991b5d3aed4d2834
David Dossot <ddossot@apple.com>
2019-08-01 14:17:16 -0700

    Add gRPC service code generation (#719)
    __Motivation__
    
    We need to generate non-blocking and blocking variants of the gRPC services specified in proto.
    
    __Modifications__
    
    - Add code generation of service and service filtering classes.
    - Add ground work for generating client classes.
    
    __Results__
    
    - Users can generate gRPC services classes that run on ServiceTalk.
    - gRPC client codegen is afoot.

---
+++ servicetalk-grpc-protoc/src/main/java/io/servicetalk/grpc/protoc/Words.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.grpc.protoc;
+
+final class Words {
+    static final String append = "append";
+    static final String appendServiceFilter = append + "ServiceFilter";
+    static final String builder = "builder";
+    static final String closeable = "closeable";
+    static final String closeAsync = "closeAsync";
+    static final String closeAsyncGracefully = closeAsync + "Gracefully";
+    static final String ctx = "ctx";
+    static final String delegate = "delegate";
+    static final String existing = "existing";
+    static final String filterFactory = "filterFactory";
+    static final String path = "path";
+    static final String request = "request";
+    static final String routes = "routes";
+    static final String rpc = "rpc";
+    static final String serializationProvider = "serializationProvider";
+    static final String service = "service";
+    static final String strategy = "strategy";
+
+    static final String Blocking = "Blocking";
+    static final String Builder = "Builder";
+    static final String Metadata = "Metadata";
+    static final String Factory = "Factory";
+    static final String Rpc = "Rpc";
+
+    private Words() {
+        // no instance
+    }
+}
