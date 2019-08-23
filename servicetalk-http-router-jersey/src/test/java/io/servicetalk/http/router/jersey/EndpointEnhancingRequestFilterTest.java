7f248ed1d6e77a799af7df17b6068b0bf399e288
Cole Stanfield <chaos@apple.com>
2019-08-23 14:02:58 -0700

    EndpointEnhancingRequestFilter to skip non-st requests
    Motivation:
    
    When introducing the ST jersey router dependencies in an existing jersey project, the EndpointEnhancingRequestFilter can be added into the filter stack and if the request does not originate from ST then there won’t be a ConnectionContext which will cause an NPE.
    
    Modifications:
    
    - Adding a non-null check in the filter method before the enhance() is called.
    
    Result:
    
    Non-ST requests will not be enhanced or filtered with EndpointEnhancingRequestFilter.

---
+++ servicetalk-http-router-jersey/src/test/java/io/servicetalk/http/router/jersey/EndpointEnhancingRequestFilterTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.router.jersey;
+
+import io.servicetalk.transport.api.ConnectionContext;
+
+import org.glassfish.jersey.internal.util.collection.Ref;
+import org.glassfish.jersey.internal.util.collection.Refs;
+import org.glassfish.jersey.process.internal.RequestScope;
+import org.junit.Test;
+
+import javax.inject.Provider;
+import javax.ws.rs.container.ContainerRequestContext;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.internal.util.reflection.FieldSetter.setField;
+
+public class EndpointEnhancingRequestFilterTest {
+
+    @Test
+    public void shouldNotFilterNonStRequests() throws Exception {
+        EndpointEnhancingRequestFilter filter = new EndpointEnhancingRequestFilter();
+        Provider<Ref<ConnectionContext>> ctxRefProvider = () -> Refs.of(null);
+        Provider<RouteStrategiesConfig> routeStrategiesConfigProvider = () -> mock(RouteStrategiesConfig.class);
+        RequestScope requestScope = mock(RequestScope.class);
+        setField(filter, filter.getClass().getDeclaredField("ctxRefProvider"), ctxRefProvider);
+        setField(filter, filter.getClass().getDeclaredField("routeStrategiesConfigProvider"),
+                routeStrategiesConfigProvider);
+        setField(filter, filter.getClass().getDeclaredField("requestScope"), requestScope);
+
+        ContainerRequestContext context = mock(ContainerRequestContext.class);
+        filter.filter(mock(ContainerRequestContext.class));
+        verifyZeroInteractions(context);
+    }
+}
