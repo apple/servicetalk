a6e3c2c7f041943a63bee291f0daac7ea3802d55
David Dossot <ddossot@apple.com>
2019-09-10 15:43:37 -0700

    Inherit buildscript repositories in Gradle plugin builds (#770)
    __Motivation__
    
    ServiceTalk's build produces custom Gradle plugins and thus has regular (i.e. non-`buildscript`) dependencies on other plugins.
    Users and environments that provide Gradle repositories via `~/.gradle/init.gradle` typically do not have plugins repositories outside of the `buildscript` section.
    
    __Modifications__
    
    - Inherit buildscript repositories into the main repositories block.
    - Make this mechanism disableable in case it causes unforeseen issues.
    
    __Results__
    
    Users/environments with predefined Gradle repositories will have a better chance at working OOTB.

--- servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/ServiceTalkLibraryPlugin.groovy
+++ servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/ServiceTalkLibraryPlugin.groovy
@@ -19,17 +19,11 @@ import com.github.spotbugs.SpotBugsTask
 import org.gradle.api.Project
 import org.gradle.api.plugins.quality.Pmd
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.Exec
-import org.gradle.api.tasks.SourceSet
-import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.api.tasks.javadoc.Javadoc
 
 import static io.servicetalk.gradle.plugin.internal.ProjectUtils.addManifestAttributes
 import static io.servicetalk.gradle.plugin.internal.ProjectUtils.addQualityTask
 import static io.servicetalk.gradle.plugin.internal.ProjectUtils.createJavadocJarTask
 import static io.servicetalk.gradle.plugin.internal.ProjectUtils.createSourcesJarTask
-import static io.servicetalk.gradle.plugin.internal.ProjectUtils.getOrCreateJavadocTask
 import static io.servicetalk.gradle.plugin.internal.ProjectUtils.locateBuildLevelConfigFile
 import static io.servicetalk.gradle.plugin.internal.Versions.PMD_VERSION
 import static io.servicetalk.gradle.plugin.internal.Versions.SPOTBUGS_VERSION
