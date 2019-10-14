11a0bb23ac8dd4f30953bc61e6b608bad58660e3
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2019-10-14 15:38:21 -0700

    fix error

--- servicetalk-http-netty/src/main/java/io/servicetalk/http/netty/AlpnServerContext.java
+++ servicetalk-http-netty/src/main/java/io/servicetalk/http/netty/AlpnServerContext.java
@@ -62,7 +62,7 @@ final class AlpnServerContext {
                 serverConnection -> {
                     // Start processing requests on http/1.1 connection:
                     if (serverConnection instanceof NettyHttpServerConnection) {
-                        NettyHttpServer.startProcessing((NettyHttpServerConnection) serverConnection);
+                        NettyHttpServer.startProcessing((NettyHttpServerConnection) serverConnection, true);
                     }
                     // Nothing to do otherwise as h2 uses auto read on the parent channel
                 })
