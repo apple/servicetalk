03a7b0a48eb0d2192ddf2fdfd0ed09d409b5b6c9
Dmitrii Volykhin <volyihin@gmail.com>
2019-11-21 21:45:45 +0300

    RedirectingClientAndConnectionFilterTest should leverage AbstractHttp… (#867)
    __Motivation__
    
    `RedirectingClientAndConnectionFilterTest` could leverage the `AbstractHttpRequesterFilterTest` test tooling. Its approach is closely related to the one in `RedirectingClientAndConnectionFilterTest` and can reuse most of the code.
    
    __Modifications__
    
    - `RedirectingClientAndConnectionFilterTest` extends `AbstractHttpRequesterFilterTest`
    - Add `StrategyInfluencerAwareConversionsTest` for static access to package-private factory methods
    
    __Result__
    
    Resolves (#396): RedirectingClientAndConnectionFilterTest should leverage AbstractHttpRequesterFilterTest

---
+++ servicetalk-http-api/src/testFixtures/java/io/servicetalk/http/api/ConditionalFilterFactory.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2019 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.api;
+
+import java.util.function.Predicate;
+
+public final class ConditionalFilterFactory
+        implements StreamingHttpConnectionFilterFactory, StreamingHttpClientFilterFactory {
+    private final Predicate<StreamingHttpRequest> predicate;
+    private final FilterFactory predicateFactory;
+
+    public ConditionalFilterFactory(final Predicate<StreamingHttpRequest> predicate,
+                                    final FilterFactory predicateFactory) {
+        this.predicate = predicate;
+        this.predicateFactory = predicateFactory;
+    }
+
+    @Override
+    public StreamingHttpClientFilter create(final FilterableStreamingHttpClient client) {
+        return new ConditionalHttpClientFilter(predicate, predicateFactory.create(client), client);
+    }
+
+    @Override
+    public StreamingHttpConnectionFilter create(final FilterableStreamingHttpConnection connection) {
+        return new ConditionalHttpConnectionFilter(predicate, predicateFactory.create(connection), connection);
+    }
+
+    public FilterFactory append(FilterFactory append) {
+        StreamingHttpClientFilterFactory clientFactory = append((StreamingHttpClientFilterFactory) append);
+        StreamingHttpConnectionFilterFactory connectionFactory = append((StreamingHttpConnectionFilterFactory) append);
+        return new FilterFactory() {
+            @Override
+            public StreamingHttpClientFilter create(final FilterableStreamingHttpClient client) {
+                return clientFactory.create(client);
+            }
+
+            @Override
+            public StreamingHttpConnectionFilter create(final FilterableStreamingHttpConnection connection) {
+                return connectionFactory.create(connection);
+            }
+        };
+    }
+
+    public interface FilterFactory extends StreamingHttpClientFilterFactory, StreamingHttpConnectionFilterFactory {
+
+        static <FF extends StreamingHttpClientFilterFactory & StreamingHttpConnectionFilterFactory> FilterFactory from(
+                FF original) {
+            return new FilterFactory() {
+                @Override
+                public StreamingHttpClientFilter create(final FilterableStreamingHttpClient client) {
+                    return original.create(client);
+                }
+
+                @Override
+                public StreamingHttpConnectionFilter create(final FilterableStreamingHttpConnection connection) {
+                    return original.create(connection);
+                }
+            };
+        }
+    }
+}
