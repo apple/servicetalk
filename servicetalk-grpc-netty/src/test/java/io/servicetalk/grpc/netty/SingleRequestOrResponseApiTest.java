0a8a7b073a9dc34104bd706b6273e51042048fc3
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2020-02-25 16:28:26 -0800

    Test async and blocking client consistency

--- servicetalk-grpc-netty/src/test/java/io/servicetalk/grpc/netty/SingleRequestOrResponseApiTest.java
+++ servicetalk-grpc-netty/src/test/java/io/servicetalk/grpc/netty/SingleRequestOrResponseApiTest.java
@@ -19,6 +19,7 @@ import io.servicetalk.concurrent.BlockingIterable;
 import io.servicetalk.concurrent.api.Publisher;
 import io.servicetalk.concurrent.api.Single;
 import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
+import io.servicetalk.grpc.api.GrpcClientBuilder;
 import io.servicetalk.grpc.api.GrpcPayloadWriter;
 import io.servicetalk.grpc.api.GrpcServiceContext;
 import io.servicetalk.grpc.api.GrpcStatusException;
@@ -29,15 +30,16 @@ import io.servicetalk.grpc.netty.TesterProto.Tester.BlockingTesterClient;
 import io.servicetalk.grpc.netty.TesterProto.Tester.BlockingTesterService;
 import io.servicetalk.grpc.netty.TesterProto.Tester.ClientFactory;
 import io.servicetalk.grpc.netty.TesterProto.Tester.ServiceFactory;
+import io.servicetalk.grpc.netty.TesterProto.Tester.TesterClient;
 import io.servicetalk.grpc.netty.TesterProto.Tester.TesterService;
 import io.servicetalk.http.api.HttpExecutionStrategy;
 import io.servicetalk.http.api.StreamingHttpClientFilter;
 import io.servicetalk.http.api.StreamingHttpRequest;
 import io.servicetalk.http.api.StreamingHttpRequester;
 import io.servicetalk.http.api.StreamingHttpResponse;
+import io.servicetalk.transport.api.HostAndPort;
 import io.servicetalk.transport.api.ServerContext;
 
-import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
@@ -46,9 +48,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import java.net.InetSocketAddress;
 import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.IntStream;
 
+import static io.servicetalk.concurrent.api.Publisher.from;
 import static io.servicetalk.concurrent.api.Publisher.fromIterable;
 import static io.servicetalk.grpc.api.GrpcStatusCode.INVALID_ARGUMENT;
 import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
@@ -59,25 +64,31 @@ import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThrows;
+import static org.junit.Assume.assumeFalse;
 
 @RunWith(Parameterized.class)
-public class BlockingApiCorrectnessTest {
+public class SingleRequestOrResponseApiTest {
 
     @Rule
     public final Timeout timeout = new ServiceTalkTestTimeout();
 
+    private final boolean streamingServer;
+    private final boolean streamingClient;
     private final ServerContext serverContext;
-    private final BlockingTesterClient client;
+    private final GrpcClientBuilder<HostAndPort, InetSocketAddress> clientBuilder;
 
-    public BlockingApiCorrectnessTest(boolean streaming) throws Exception {
-        serverContext = GrpcServers.forAddress(localAddress(0)).listenAndAwait(streaming ?
+    public SingleRequestOrResponseApiTest(boolean streamingServer, boolean streamingClient) throws Exception {
+        this.streamingServer = streamingServer;
+        this.streamingClient = streamingClient;
+
+        serverContext = GrpcServers.forAddress(localAddress(0)).listenAndAwait(streamingServer ?
                 new ServiceFactory(new TesterServiceImpl()) :
                 new ServiceFactory(new BlockingTesterServiceImpl()));
 
-        client = GrpcClients.forAddress(serverHostAndPort(serverContext))
+        clientBuilder = GrpcClients.forAddress(serverHostAndPort(serverContext))
                 // HTTP filter that modifies path to workaround gRPC API constraints:
                 .appendHttpClientFilter(origin -> new StreamingHttpClientFilter(origin) {
                     @Override
@@ -89,60 +100,75 @@ public class BlockingApiCorrectnessTest {
                         request.requestTarget(BlockingTestResponseStreamRpc.PATH);
                         return super.request(delegate, strategy, request);
                     }
-                }).buildBlocking(new ClientFactory());
+                });
     }
 
-    @Parameters(name = "streaming={0}")
-    public static Object[] params() {
-        return new Object[]{true, false};
+    @Parameters(name = "streamingServer={0}, streamingServer={1}")
+    public static Object[][] params() {
+        return new Object[][]{{false, false}, {false, true}, {true, false}};
     }
 
     @After
     public void tearDown() throws Exception {
-        try {
-            client.close();
-        } finally {
-            serverContext.close();
-        }
+        serverContext.close();
     }
 
     @Test
-    public void serverBlockingResponseStreamingRouteFailsWithZeroRequestItems() {
-        serverBlockingResponseStreamingRouteFailsWithInvalidArgument(emptyList(),
+    public void serverResponseStreamingRouteFailsWithZeroRequestItems() throws Exception {
+        serverResponseStreamingRouteFailsWithInvalidArgument(emptyList(),
                 "Single request message was expected, but none was received");
     }
 
     @Test
-    public void serverBlockingResponseStreamingRouteFailsOnSecondRequestItem() {
-        serverBlockingResponseStreamingRouteFailsWithInvalidArgument(asList(newRequest(0), newRequest(0)),
+    public void serverResponseStreamingRouteFailsOnSecondRequestItem() throws Exception {
+        serverResponseStreamingRouteFailsWithInvalidArgument(asList(newRequest(0), newRequest(0)),
                 "More than one request message received");
     }
 
-    private void serverBlockingResponseStreamingRouteFailsWithInvalidArgument(Iterable<TestRequest> requestItems,
-                                                                              String expectedMsg) {
-        GrpcStatusException e = assertThrows(GrpcStatusException.class,
-                () -> client.testBiDiStream(requestItems).forEach(response -> { /* noop */ }));
-        assertThat(e.status().code(), is(INVALID_ARGUMENT));
-        assertThat(e.status().description(), equalTo(expectedMsg));
+    private void serverResponseStreamingRouteFailsWithInvalidArgument(Iterable<TestRequest> requestItems,
+                                                                      String expectedMsg) throws Exception {
+        assumeFalse(streamingClient);  // No need to run the test with different client-side, always use blocking client
+        try (BlockingTesterClient client = newBlockingClient()) {
+            GrpcStatusException e = assertThrows(GrpcStatusException.class,
+                    () -> client.testBiDiStream(requestItems).forEach(response -> { /* noop */ }));
+            assertThat(e.status().code(), is(INVALID_ARGUMENT));
+            assertThat(e.status().description(), equalTo(expectedMsg));
+        }
     }
 
     @Test
-    public void clientBlockingRequestStreamingCallFailsWithZeroResponseItems() {
-        clientBlockingRequestStreamingCallFailsOnInvalidResponse(0, NoSuchElementException.class,
-                is(nullValue()));
+    public void clientRequestStreamingCallFailsWithZeroResponseItems() throws Exception {
+        clientRequestStreamingCallFailsOnInvalidResponse(0, NoSuchElementException.class);
     }
 
     @Test
-    public void clientBlockingRequestStreamingCallFailsOnSecondResponseItem() {
-        clientBlockingRequestStreamingCallFailsOnInvalidResponse(2, IllegalArgumentException.class,
-                equalTo("More than one response message received"));
+    public void clientRequestStreamingCallFailsOnSecondResponseItem() throws Exception {
+        clientRequestStreamingCallFailsOnInvalidResponse(2, IllegalArgumentException.class);
     }
 
-    private <T extends Throwable> void clientBlockingRequestStreamingCallFailsOnInvalidResponse(
-            int numberOfResponses, Class<T> exceptionClass, Matcher<Object> exceptionMsgMatcher) {
-        T e = assertThrows(exceptionClass,
-                () -> client.testRequestStream(singletonList(newRequest(numberOfResponses))));
-        assertThat(e.getMessage(), exceptionMsgMatcher);
+    private <T extends Throwable> void clientRequestStreamingCallFailsOnInvalidResponse(
+            int numberOfResponses, Class<T> exceptionClass) throws Exception {
+        assumeFalse(streamingServer);  // No need to run the test with different server-side
+        if (streamingClient) {
+            try (TesterClient client = newClient()) {
+                ExecutionException e = assertThrows(ExecutionException.class,
+                        () -> client.testRequestStream(from(newRequest(numberOfResponses))).toFuture().get());
+                assertThat(e.getCause(), is(instanceOf(exceptionClass)));
+            }
+        } else {
+            try (BlockingTesterClient client = newBlockingClient()) {
+                assertThrows(exceptionClass,
+                        () -> client.testRequestStream(singletonList(newRequest(numberOfResponses))));
+            }
+        }
+    }
+
+    private BlockingTesterClient newBlockingClient() {
+        return clientBuilder.buildBlocking(new ClientFactory());
+    }
+
+    private TesterClient newClient() {
+        return clientBuilder.build(new ClientFactory());
     }
 
     private static TestRequest newRequest(int number) {
