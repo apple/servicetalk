19e9f3644a634783c840e13c1ad481d80634e75e
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2020-03-13 18:15:51 -0700

    JDK provider incorrectly configures default ciphers for TLSv1.3
    Motivation:
    
    We use the old approach of configuring secure protocols in netty via
    `SSLEngine#setEnabledProtocols(...)`. In this case, `SslContextBuilder`
    does not know if users configure TLSv1.3 or not and therefore, sets
    incorrect set of default ciphers which does not support TLSv1.3.
    As the result, users see:
    javax.net.ssl.SSLHandshakeException: No appropriate protocol
    (protocol is disabled or cipher suites are inappropriate).
    
    Modifications:
    
    - Use `SslContextBuilder#protocols(...)` instead of
    `SSLEngine#setEnabledProtocols(...)` to set secure protocols;
    - Delete `WrappingSslContext` that is not required anymore;
    
    Result:
    
    `SslContextBuilder` correctly sets default ciphers when TLSv1.3
    is present.

--- servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/Tls13Test.java
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/Tls13Test.java
@@ -15,17 +15,27 @@
  */
 package io.servicetalk.http.netty;
 
+import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
 import io.servicetalk.http.api.BlockingHttpClient;
+import io.servicetalk.http.api.BlockingHttpConnection;
 import io.servicetalk.http.api.HttpResponse;
+import io.servicetalk.http.api.HttpServerSecurityConfigurator;
+import io.servicetalk.http.api.SingleAddressHttpClientSecurityConfigurator;
 import io.servicetalk.test.resources.DefaultTestCerts;
+import io.servicetalk.transport.api.HostAndPort;
 import io.servicetalk.transport.api.SecurityConfigurator.SslProvider;
 import io.servicetalk.transport.api.ServerContext;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.net.InetSocketAddress;
 import java.util.Collection;
+import javax.annotation.Nullable;
+import javax.net.ssl.SSLSession;
 
 import static io.netty.util.internal.PlatformDependent.javaVersion;
 import static io.servicetalk.http.api.HttpHeaderNames.CONTENT_TYPE;
@@ -40,61 +50,88 @@ import static io.servicetalk.transport.netty.internal.AddressUtils.serverHostAnd
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(Parameterized.class)
 public class Tls13Test {
     private static final String TLS1_3 = "TLSv1.3";
     private static final String TLS1_3_REQUIRED_CIPHER = "TLS_AES_128_GCM_SHA256";
 
+    @Rule
+    public final Timeout timeout = new ServiceTalkTestTimeout();
+
     private final SslProvider serverSslProvider;
     private final SslProvider clientSslProvider;
+    @Nullable
+    private final String cipher;
 
-    public Tls13Test(SslProvider serverSslProvider, SslProvider clientSslProvider) {
+    public Tls13Test(SslProvider serverSslProvider, SslProvider clientSslProvider, @Nullable String cipher) {
         this.serverSslProvider = requireNonNull(serverSslProvider);
         this.clientSslProvider = requireNonNull(clientSslProvider);
+        this.cipher = cipher;
     }
 
-    @Parameterized.Parameters(name = "server={0} client={1}")
+    @Parameterized.Parameters(name = "server={0} client={1} cipher={2}")
     public static Collection<Object[]> sslProviders() {
         // TLSv1.3 is not currently supported in JDK8.
-        return javaVersion() < 11 ? singletonList(new Object[]{OPENSSL, OPENSSL}) :
-                asList(new Object[]{JDK, JDK},
-                        new Object[]{JDK, OPENSSL},
-                        new Object[]{OPENSSL, JDK},
-                        new Object[]{OPENSSL, OPENSSL}
+        return javaVersion() < 11 ?
+                asList(new Object[]{OPENSSL, OPENSSL, null},
+                        new Object[]{OPENSSL, OPENSSL, TLS1_3_REQUIRED_CIPHER}) :
+                asList(new Object[]{JDK, JDK, null},
+                        new Object[]{JDK, JDK, TLS1_3_REQUIRED_CIPHER},
+                        new Object[]{JDK, OPENSSL, null},
+                        new Object[]{JDK, OPENSSL, TLS1_3_REQUIRED_CIPHER},
+                        new Object[]{OPENSSL, JDK, null},
+                        new Object[]{OPENSSL, JDK, TLS1_3_REQUIRED_CIPHER},
+                        new Object[]{OPENSSL, OPENSSL, null},
+                        new Object[]{OPENSSL, OPENSSL, TLS1_3_REQUIRED_CIPHER}
         );
     }
 
     @Test
     public void requiredCipher() throws Exception {
-        try (ServerContext serverContext = HttpServers.forAddress(localAddress(0))
+        HttpServerSecurityConfigurator serverSecurityConfigurator = HttpServers.forAddress(localAddress(0))
                 .secure()
                 .protocols(TLS1_3)
-                .ciphers(singletonList(TLS1_3_REQUIRED_CIPHER))
-                .provider(serverSslProvider)
+                .provider(serverSslProvider);
+        if (cipher != null) {
+            serverSecurityConfigurator.ciphers(singletonList(cipher));
+        }
+        try (ServerContext serverContext = serverSecurityConfigurator
                 .commit(DefaultTestCerts::loadServerPem, DefaultTestCerts::loadServerKey)
-                .listenBlockingAndAwait((ctx, request, responseFactory) -> responseFactory.ok()
-                        .payloadBody(request.payloadBody(textDeserializer()), textSerializer()))) {
+                .listenBlockingAndAwait((ctx, request, responseFactory) -> {
+                    assertThat(request.payloadBody(textDeserializer()), equalTo("request-payload-body"));
+                    SSLSession sslSession = ctx.sslSession();
+                    assertThat(sslSession, is(notNullValue()));
+                    return responseFactory.ok().payloadBody(sslSession.getProtocol(), textSerializer());
+                })) {
 
-            try (BlockingHttpClient client = HttpClients.forSingleAddress(serverHostAndPort(serverContext))
+            SingleAddressHttpClientSecurityConfigurator<HostAndPort, InetSocketAddress> clientSecurityConfigurator =
+                    HttpClients.forSingleAddress(serverHostAndPort(serverContext))
                     .secure()
                     .protocols(TLS1_3)
-                    .ciphers(singletonList(TLS1_3_REQUIRED_CIPHER))
                     .disableHostnameVerification()
                     // required for generated test certificates
                     .trustManager(DefaultTestCerts::loadMutualAuthCaPem)
-                    .provider(clientSslProvider)
-                    .commit()
-                    .buildBlocking()) {
+                    .provider(clientSslProvider);
+            if (cipher != null) {
+                clientSecurityConfigurator.ciphers(singletonList(cipher));
+            }
+            try (BlockingHttpClient client = clientSecurityConfigurator.commit().buildBlocking();
+                 BlockingHttpConnection connection = client.reserveConnection(client.get("/"))) {
 
-                String requestPayload = "hello " + TLS1_3;
-                HttpResponse response = client.request(client.get("/").payloadBody(requestPayload, textSerializer()));
+                SSLSession sslSession = connection.connectionContext().sslSession();
+                assertThat(sslSession, is(notNullValue()));
+                assertThat(sslSession.getProtocol(), equalTo(TLS1_3));
+                HttpResponse response = client.request(client.post("/")
+                        .payloadBody("request-payload-body", textSerializer()));
 
                 assertThat(response.status(), is(OK));
                 assertThat(response.headers().get(CONTENT_TYPE), is(TEXT_PLAIN_UTF_8));
-                assertThat(response.payloadBody(textDeserializer()), is(requestPayload));
+                assertThat(response.payloadBody(textDeserializer()), equalTo(TLS1_3));
             }
         }
     }
