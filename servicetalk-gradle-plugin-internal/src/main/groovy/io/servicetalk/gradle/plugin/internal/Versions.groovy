154842a6702cc9684dc86fc3accce914289f0e9f
Scott Mitchell <scott_mitchell@apple.com>
2020-03-13 17:23:48 -0700

    Update gradlew to 6.2.2 (#966)
    Motivation:
    Update to the latest version of gradlew and update dependencies.
    
    Modifications:
    - Use spotbugs 4.0.2 plugin and 4.0.0 executable
    - Remove explicit guava compile dependency
    
    Result:
    Project now uses gradlew 6.2.2.

--- servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
+++ servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
@@ -22,6 +22,6 @@ import static org.gradle.api.JavaVersion.VERSION_1_8
 final class Versions {
   static final String CHECKSTYLE_VERSION = "8.28"
   static final String PMD_VERSION = "6.20.0"
-  static final String SPOTBUGS_VERSION = "3.1.12"
+  static final String SPOTBUGS_VERSION = "4.0.0"
   static final JavaVersion TARGET_VERSION = VERSION_1_8
 }
