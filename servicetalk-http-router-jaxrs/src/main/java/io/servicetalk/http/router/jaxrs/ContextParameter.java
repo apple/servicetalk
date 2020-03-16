52a712e1d19f55295942d384d9d4f8ebab5fae49
volyx <volyihin@gmail.com>
2020-03-17 00:59:11 +0300

    - Add parameters to methods

---
+++ servicetalk-http-router-jaxrs/src/main/java/io/servicetalk/http/router/jaxrs/ContextParameter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.router.jaxrs;
+
+import io.servicetalk.http.api.HttpServiceContext;
+import io.servicetalk.http.api.StreamingHttpRequest;
+import io.servicetalk.http.api.StreamingHttpResponseFactory;
+
+import javax.annotation.Nullable;
+
+public class ContextParameter extends Parameter {
+
+    public ContextParameter(Class<?> type) {
+        super(type.getSimpleName(), type);
+    }
+
+    @Nullable
+    @Override
+    public Object get(final HttpServiceContext ctx, final StreamingHttpRequest request,
+                      final StreamingHttpResponseFactory responseFactory) {
+        if (type == HttpServiceContext.class) {
+            return ctx;
+        }
+        if (type == StreamingHttpRequest.class) {
+            return request;
+        }
+
+        if (type == StreamingHttpResponseFactory.class) {
+            return responseFactory;
+        }
+
+        return ctx;
+    }
+}
