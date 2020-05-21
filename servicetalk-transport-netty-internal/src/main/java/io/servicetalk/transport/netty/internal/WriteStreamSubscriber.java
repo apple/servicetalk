1a7498da15a40f1f4777618f2f4ce06c34a60d1a
Nitesh Kant <nitesh_kant@apple.com>
2020-05-20 18:21:49 -0700

    `WriteStreamSubscriber` retains listeners for the lifetime of the source (#1059)
    * `WriteStreamSubscriber` retains listeners for the lifetime of the source
    
    __Motivation__
    
    `WriteStreamSubscriber` uses a single `Promise` for all writes done through it. As we add listeners to each write, the listeners get accumulated in the promise till the `WriteStreamSubscriber` terminates. For servers since we do a single write for the connection, these listeners are retained for the connection lifetime.
    
    __Modification__
    
    - Manage listeners in `AllWritesPromise` as opposed to delegating them to `DefaultWritePromise`.
    - Listeners are managed over the specific write boundaries.
    - Each time a write is complete, listeners added since last write are terminated and removed from the list.
    
    __Result__
    
    Listeners are retained by `WriteStreamSubscriber` for the duration of each write.

--- servicetalk-transport-netty-internal/src/main/java/io/servicetalk/transport/netty/internal/WriteStreamSubscriber.java
+++ servicetalk-transport-netty-internal/src/main/java/io/servicetalk/transport/netty/internal/WriteStreamSubscriber.java
@@ -26,12 +26,17 @@ import io.servicetalk.concurrent.internal.FlowControlUtils;
 import io.servicetalk.transport.netty.internal.DefaultNettyConnection.ChannelOutboundListener;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.DefaultChannelPromise;
 import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.concurrent.atomic.AtomicLongFieldUpdater;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import javax.annotation.Nullable;
@@ -67,6 +72,8 @@ import static java.util.Objects.requireNonNull;
  */
 final class WriteStreamSubscriber implements PublisherSource.Subscriber<Object>, ChannelOutboundListener, Cancellable {
     private static final Logger LOGGER = LoggerFactory.getLogger(WriteStreamSubscriber.class);
+    @SuppressWarnings("rawtypes")
+    private static final GenericFutureListener WRITE_BOUNDARY = future -> { };
     private static final byte SOURCE_TERMINATED = 1;
     private static final byte CHANNEL_CLOSED = 1 << 1;
     private static final byte CLOSE_OUTBOUND_ON_SUBSCRIBER_TERMINATION = 1 << 2;
@@ -259,9 +266,59 @@ final class WriteStreamSubscriber implements PublisherSource.Subscriber<Object>,
         private byte state;
         @Nullable
         private Throwable failureCause;
+        /**
+         * This deque contains all added listeners within {@link #WRITE_BOUNDARY write boundaries}.
+         * <pre>
+         *     {@link #WRITE_BOUNDARY}, listener1, listener2, {@link #WRITE_BOUNDARY}, listener3 ...
+         * </pre>
+         * We assume that no listener for a write is added after that write is completed (a.k.a late listeners).
+         */
+        private final Deque<GenericFutureListener<?>> listenersOnWriteBoundaries = new ArrayDeque<>();
 
         AllWritesPromise(final Channel channel) {
             super(channel);
+        }
+
+        @Override
+        public ChannelPromise addListener(final GenericFutureListener<? extends Future<? super Void>> listener) {
+            assert channel.eventLoop().inEventLoop();
+            if (hasFlag(SUBSCRIBER_TERMINATED)) {
+                return this;
+            }
+            listenersOnWriteBoundaries.addLast(listener);
+            return this;
+        }
+
+        @SafeVarargs
+        @Override
+        public final ChannelPromise addListeners(
+                final GenericFutureListener<? extends Future<? super Void>>... listeners) {
+            assert channel.eventLoop().inEventLoop();
+            if (hasFlag(SUBSCRIBER_TERMINATED)) {
+                return this;
+            }
+            for (GenericFutureListener<? extends Future<? super Void>> listener : listeners) {
+                listenersOnWriteBoundaries.addLast(listener);
+            }
+            return this;
+        }
+
+        @Override
+        public ChannelPromise removeListener(final GenericFutureListener<? extends Future<? super Void>> listener) {
+            assert channel.eventLoop().inEventLoop();
+            listenersOnWriteBoundaries.removeFirstOccurrence(listener);
+            return this;
+        }
+
+        @SafeVarargs
+        @Override
+        public final ChannelPromise removeListeners(
+                final GenericFutureListener<? extends Future<? super Void>>... listeners) {
+            assert channel.eventLoop().inEventLoop();
+            for (GenericFutureListener<? extends Future<? super Void>> listener : listeners) {
+                listenersOnWriteBoundaries.removeFirstOccurrence(listener);
+            }
+            return this;
         }
 
         boolean isWritable() {
@@ -275,6 +332,7 @@ final class WriteStreamSubscriber implements PublisherSource.Subscriber<Object>,
                 written = true;
             }
             activeWrites++;
+            listenersOnWriteBoundaries.addLast(WRITE_BOUNDARY);
             channel.write(msg, this);
         }
 
@@ -362,6 +420,8 @@ final class WriteStreamSubscriber implements PublisherSource.Subscriber<Object>,
                     return true;
                 }
                 return super.trySuccess(null);
+            } else {
+                notifyListenersTillNextWrite(failureCause);
             }
             return nettySharedPromiseTryStatus();
         }
@@ -398,6 +458,7 @@ final class WriteStreamSubscriber implements PublisherSource.Subscriber<Object>,
         }
 
         private void terminateSubscriber(@Nullable Throwable cause) {
+            notifyAllListeners(cause);
             if (cause == null) {
                 try {
                     subscriber.onComplete();
@@ -417,6 +478,26 @@ final class WriteStreamSubscriber implements PublisherSource.Subscriber<Object>,
                     // Close channel on error.
                     channel.close();
                 }
+            }
+        }
+
+        private void notifyAllListeners(@Nullable Throwable cause) {
+            final ChannelFuture future = cause == null ? channel.newSucceededFuture() : channel.newFailedFuture(cause);
+            GenericFutureListener<?> mayBeListener;
+            while ((mayBeListener = listenersOnWriteBoundaries.pollFirst()) != null) {
+                if (mayBeListener != WRITE_BOUNDARY) {
+                    notifyListener(eventLoop, future, mayBeListener);
+                }
+            }
+        }
+
+        private void notifyListenersTillNextWrite(@Nullable Throwable cause) {
+            Object shdBeWriteBoundary = listenersOnWriteBoundaries.pollFirst();
+            assert shdBeWriteBoundary == WRITE_BOUNDARY;
+
+            final ChannelFuture future = cause == null ? channel.newSucceededFuture() : channel.newFailedFuture(cause);
+            while (!listenersOnWriteBoundaries.isEmpty() && listenersOnWriteBoundaries.peekFirst() != WRITE_BOUNDARY) {
+                notifyListener(eventLoop, future, listenersOnWriteBoundaries.pollFirst());
             }
         }
 
---
+++ servicetalk-transport-netty-internal/src/test/java/io/servicetalk/transport/netty/internal/WriteStreamSubscriberFutureListenersTest.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.transport.netty.internal;
+
+import io.servicetalk.concurrent.api.TestCompletableSubscriber;
+import io.servicetalk.concurrent.api.TestSubscription;
+import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.EventLoop;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.util.ReferenceCountUtil;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.mockito.ArgumentCaptor;
+import org.mockito.stubbing.Answer;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static io.servicetalk.concurrent.internal.DeliberateException.DELIBERATE_EXCEPTION;
+import static io.servicetalk.transport.netty.internal.CloseHandler.UNSUPPORTED_PROTOCOL_CLOSE_HANDLER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+public class WriteStreamSubscriberFutureListenersTest {
+    @Rule
+    public final Timeout timeout = new ServiceTalkTestTimeout();
+
+    private final BlockingQueue<ChannelFutureListener> listeners;
+    private final EmbeddedChannel channel;
+    private final WriteStreamSubscriber subscriber;
+
+    public WriteStreamSubscriberFutureListenersTest() {
+        listeners = new LinkedBlockingQueue<>();
+        channel = new EmbeddedChannel(new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(final ChannelHandlerContext ctx, final Object msg, final ChannelPromise promise)
+                    throws Exception {
+                ChannelFutureListener listener = mock(ChannelFutureListener.class);
+                listeners.add(listener);
+                promise.addListener(listener);
+                super.write(ctx, msg, promise);
+            }
+        });
+        WriteDemandEstimator estimator = WriteDemandEstimators.newDefaultEstimator();
+        TestCompletableSubscriber completableSubscriber = new TestCompletableSubscriber();
+        subscriber = new WriteStreamSubscriber(channel, estimator, completableSubscriber,
+                UNSUPPORTED_PROTOCOL_CLOSE_HANDLER);
+        TestSubscription subscription = new TestSubscription();
+        subscriber.onSubscribe(subscription);
+        assertThat("No items requested.", subscription.requested(), greaterThan(0L));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        channel.close().sync().await();
+    }
+
+    @Test
+    public void singleWriteAndFlush() throws Exception {
+        ChannelFutureListener listener1 = doWrite();
+        channel.flush();
+        verifyListenerInvokedWithSuccess(listener1);
+    }
+
+    @Test
+    public void multipleWritesThenFlush() throws Exception {
+        ChannelFutureListener listener1 = doWrite();
+        ChannelFutureListener listener2 = doWrite();
+        channel.flush();
+
+        verifyListenerInvokedWithSuccess(listener1);
+        verifyListenerInvokedWithSuccess(listener2);
+    }
+
+    @Test
+    public void multipleWritesMultipleFlushes() throws Exception {
+        ChannelFutureListener listener1 = doWrite();
+        channel.flush();
+        verifyListenerInvokedWithSuccess(listener1);
+
+        ChannelFutureListener listener2 = doWrite();
+        channel.flush();
+        verifyListenerInvokedWithSuccess(listener2);
+    }
+
+    @Test
+    public void singleWriteThenSourceComplete() throws Exception {
+        ChannelFutureListener listener1 = doWrite();
+        subscriber.onComplete();
+        verifyZeroInteractions(listener1);
+
+        channel.flush();
+        verifyListenerInvokedWithSuccess(listener1);
+    }
+
+    @Test
+    public void singleWriteThenSourceFail() throws Exception {
+        ChannelFutureListener listener1 = doWrite();
+        subscriber.onError(DELIBERATE_EXCEPTION);
+        verifyZeroInteractions(listener1);
+
+        channel.flush();
+        verifyListenerInvokedWithFailure(listener1);
+    }
+
+    @Test
+    public void multipleWriteThenSourceComplete() throws Exception {
+        ChannelFutureListener listener1 = doWrite();
+        ChannelFutureListener listener2 = doWrite();
+        subscriber.onComplete();
+        verifyZeroInteractions(listener1);
+        verifyZeroInteractions(listener2);
+
+        channel.flush();
+        verifyListenerInvokedWithSuccess(listener1);
+        verifyListenerInvokedWithSuccess(listener2);
+    }
+
+    @Test
+    public void multipleWriteThenSourceFail() throws Exception {
+        ChannelFutureListener listener1 = doWrite();
+        ChannelFutureListener listener2 = doWrite();
+        subscriber.onError(DELIBERATE_EXCEPTION);
+        verifyZeroInteractions(listener1);
+        verifyZeroInteractions(listener2);
+
+        channel.flush();
+        verifyListenerInvokedWithFailure(listener1);
+        verifyListenerInvokedWithFailure(listener2);
+    }
+
+    @Test
+    public void synchronousCompleteWrite() throws Exception {
+        Channel mockChannel = mock(Channel.class);
+        EventLoop mockEventLoop = mock(EventLoop.class);
+        when(mockEventLoop.inEventLoop()).thenReturn(true);
+        when(mockChannel.eventLoop()).thenReturn(mockEventLoop);
+        when(mockChannel.newSucceededFuture()).thenReturn(channel.newSucceededFuture());
+        doAnswer((Answer<Void>) invocation -> {
+            ReferenceCountUtil.release(invocation.getArgument(0));
+            ChannelFutureListener listener = mock(ChannelFutureListener.class);
+            listeners.add(listener);
+            ChannelPromise promise = invocation.getArgument(1);
+            promise.addListener(listener);
+            promise.setSuccess();
+            return null;
+        }).when(mockChannel).write(any(), any());
+        WriteDemandEstimator estimator = WriteDemandEstimators.newDefaultEstimator();
+        TestCompletableSubscriber completableSubscriber = new TestCompletableSubscriber();
+        WriteStreamSubscriber subscriber = new WriteStreamSubscriber(mockChannel, estimator, completableSubscriber,
+                UNSUPPORTED_PROTOCOL_CLOSE_HANDLER);
+        subscriber.onNext(1);
+        verifyListenerInvokedWithSuccess(listeners.take());
+        subscriber.onNext(2);
+        verifyListenerInvokedWithSuccess(listeners.take());
+    }
+
+    private ChannelFutureListener doWrite() throws InterruptedException {
+        subscriber.onNext(1);
+        ChannelFutureListener listener = listeners.take();
+        verifyZeroInteractions(listener);
+        return listener;
+    }
+
+    private void verifyListenerInvokedWithSuccess(final ChannelFutureListener listener) throws Exception {
+        ArgumentCaptor<ChannelFuture> future = ArgumentCaptor.forClass(ChannelFuture.class);
+        verify(listener).operationComplete(future.capture());
+        assertThat("Unexpected future result.", future.getValue().isSuccess(), is(true));
+    }
+
+    private void verifyListenerInvokedWithFailure(final ChannelFutureListener listener) throws Exception {
+        ArgumentCaptor<ChannelFuture> future = ArgumentCaptor.forClass(ChannelFuture.class);
+        verify(listener).operationComplete(future.capture());
+        assertThat("Unexpected future result.", future.getValue().isSuccess(), is(false));
+        assertThat("Unexpected future result.", future.getValue().cause(), is(DELIBERATE_EXCEPTION));
+    }
+}
