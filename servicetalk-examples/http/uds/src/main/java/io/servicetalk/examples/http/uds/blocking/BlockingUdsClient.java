3fba2fe4c125d2c9a5396158891f296b64457f10
Scott Mitchell <scott_mitchell@apple.com>
2020-09-16 09:46:48 -0700

    Unix Domain Sockets HttpClientBuilder fixes (#1153)
    Motivation:
    UDS is supported by ServicetTalk by passing in a DomainSocketAddress
    address. However the HttpClientBuilder had some restrictions/assumptions
    about the address type which prevented its use.
    
    Modifications:
    - Relax assumptions/restrictions in the HttpClientBuilder to allow for
    DomainSocketAddress
    - Add unit test to demonstrate UDS
    - Add example for UDS
    
    Result:
    UDS client is now working and tested.

---
+++ servicetalk-examples/http/uds/src/main/java/io/servicetalk/examples/http/uds/blocking/BlockingUdsClient.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.examples.http.uds.blocking;
+
+import io.servicetalk.http.api.BlockingHttpClient;
+import io.servicetalk.http.api.HttpResponse;
+import io.servicetalk.http.netty.HttpClients;
+
+import static io.servicetalk.examples.http.uds.blocking.UdsUtils.udsAddress;
+import static io.servicetalk.http.api.HttpSerializationProviders.textDeserializer;
+
+/**
+ * <a href="http://man7.org/linux/man-pages/man7/unix.7.html">AF_UNIX socket</a> client example.
+ */
+public final class BlockingUdsClient {
+    public static void main(String[] args) throws Exception {
+        try (BlockingHttpClient client = HttpClients.forResolvedAddress(udsAddress()).buildBlocking()) {
+            HttpResponse response = client.request(client.get("/sayHello"));
+            System.out.println(response.toString((name, value) -> value));
+            System.out.println(response.payloadBody(textDeserializer()));
+        }
+    }
+}
