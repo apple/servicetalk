92a78e7ea3b39fbfabf59b341a3686502ff75610
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2020-09-29 12:03:22 -0700

    Improve ConnectionCloseHeaderHandlingTest

--- servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ConnectionCloseHeaderHandlingTest.java
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ConnectionCloseHeaderHandlingTest.java
@@ -17,6 +17,7 @@ package io.servicetalk.http.netty;
 
 import io.servicetalk.buffer.api.Buffer;
 import io.servicetalk.concurrent.BlockingIterator;
+import io.servicetalk.concurrent.api.Completable;
 import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
 import io.servicetalk.http.api.HttpPayloadWriter;
 import io.servicetalk.http.api.HttpServerBuilder;
@@ -25,12 +26,14 @@ import io.servicetalk.http.api.StreamingHttpClient;
 import io.servicetalk.http.api.StreamingHttpRequest;
 import io.servicetalk.http.api.StreamingHttpResponse;
 import io.servicetalk.test.resources.DefaultTestCerts;
+import io.servicetalk.transport.api.ConnectionContext;
+import io.servicetalk.transport.api.DelegatingConnectionAcceptor;
 import io.servicetalk.transport.api.HostAndPort;
-import io.servicetalk.transport.api.IoExecutor;
 import io.servicetalk.transport.api.ServerContext;
-import io.servicetalk.transport.netty.internal.IoThreadFactory;
+import io.servicetalk.transport.netty.internal.ExecutionContextRule;
 
 import org.junit.After;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
@@ -41,15 +44,19 @@ import org.junit.runners.Parameterized.Parameters;
 import java.nio.channels.ClosedChannelException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.annotation.Nullable;
 
 import static io.servicetalk.concurrent.api.AsyncCloseables.newCompositeCloseable;
+import static io.servicetalk.concurrent.api.Completable.completed;
 import static io.servicetalk.concurrent.api.Completable.never;
 import static io.servicetalk.concurrent.api.Publisher.from;
+import static io.servicetalk.http.api.HttpExecutionStrategies.defaultStrategy;
 import static io.servicetalk.http.api.HttpHeaderNames.CONNECTION;
 import static io.servicetalk.http.api.HttpHeaderNames.CONTENT_LENGTH;
 import static io.servicetalk.http.api.HttpHeaderValues.CLOSE;
@@ -60,35 +67,45 @@ import static io.servicetalk.http.api.HttpResponseStatus.OK;
 import static io.servicetalk.http.api.HttpSerializationProviders.textSerializer;
 import static io.servicetalk.http.api.Matchers.contentEqualTo;
 import static io.servicetalk.http.netty.HttpsProxyTest.safeClose;
-import static io.servicetalk.transport.netty.NettyIoExecutors.createIoExecutor;
 import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
 import static io.servicetalk.transport.netty.internal.AddressUtils.serverHostAndPort;
+import static io.servicetalk.transport.netty.internal.ExecutionContextRule.cached;
 import static io.servicetalk.utils.internal.PlatformDependent.throwException;
 import static java.lang.String.valueOf;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThrows;
 
 @RunWith(Enclosed.class)
 public class ConnectionCloseHeaderHandlingTest {
 
     private static final Collection<Boolean> TRUE_FALSE = asList(true, false);
+    private static final String SERVER_SHOULD_CLOSE = "serverShouldClose";
 
-    private abstract static class ConnectionSetup {
+    public abstract static class ConnectionSetup {
+
+        @ClassRule
+        public static final ExecutionContextRule SERVER_CTX = cached("server-io", "server-executor");
+        @ClassRule
+        public static final ExecutionContextRule CLIENT_CTX = cached("client-io", "client-executor");
 
         @Rule
         public final ServiceTalkTestTimeout timeout = new ServiceTalkTestTimeout();
 
         @Nullable
         private final ProxyTunnel proxyTunnel;
-        private final IoExecutor serverIoExecutor;
         private final ServerContext serverContext;
         private final StreamingHttpClient client;
         protected final ReservedStreamingHttpConnection connection;
 
-        protected final CountDownLatch connectionClosed = new CountDownLatch(1);
+        private final CountDownLatch clientConnectionClosed = new CountDownLatch(1);
+        private final CountDownLatch serverConnectionClosed = new CountDownLatch(1);
+
+        protected final BlockingQueue<StreamingHttpResponse> responses = new LinkedBlockingDeque<>();
+
         protected final CountDownLatch sendResponse = new CountDownLatch(1);
         protected final CountDownLatch responseReceived = new CountDownLatch(1);
         protected final CountDownLatch requestReceived = new CountDownLatch(1);
@@ -96,9 +113,17 @@ public class ConnectionCloseHeaderHandlingTest {
         protected final AtomicInteger requestPayloadSize = new AtomicInteger();
 
         protected ConnectionSetup(boolean viaProxy, boolean awaitRequestPayload) throws Exception {
-            serverIoExecutor = createIoExecutor(new IoThreadFactory("server-io-executor"));
             HttpServerBuilder serverBuilder = HttpServers.forAddress(localAddress(0))
-                    .ioExecutor(serverIoExecutor);
+                    .ioExecutor(SERVER_CTX.ioExecutor())
+                    .executionStrategy(defaultStrategy(SERVER_CTX.executor()))
+                    .enableWireLogging("servicetalk-tests-server-wire-logger")
+                    .appendConnectionAcceptorFilter(original -> new DelegatingConnectionAcceptor(original) {
+                        @Override
+                        public Completable accept(final ConnectionContext context) {
+                            context.onClose().whenFinally(serverConnectionClosed::countDown).subscribe();
+                            return completed();
+                        }
+                    });
 
             HostAndPort proxyAddress = null;
             if (viaProxy) {
@@ -115,8 +140,12 @@ public class ConnectionCloseHeaderHandlingTest {
                         requestReceived.countDown();
                         boolean noResponseContent = request.hasQueryParameter("noResponseContent", "true");
                         String content = noResponseContent ? "" : "server_content";
-                        response.addHeader(CONTENT_LENGTH, noResponseContent ? ZERO : valueOf(content.length()))
-                                .addHeader(CONNECTION, CLOSE);
+                        response.addHeader(CONTENT_LENGTH, noResponseContent ? ZERO : valueOf(content.length()));
+
+                        // Add the "connection: close" header only when requested:
+                        if (request.hasQueryParameter(SERVER_SHOULD_CLOSE)) {
+                            response.addHeader(CONNECTION, CLOSE);
+                        }
 
                         sendResponse.await();
                         try (HttpPayloadWriter<String> writer = response.sendMetaData(textSerializer())) {
@@ -148,15 +177,18 @@ public class ConnectionCloseHeaderHandlingTest {
                     .trustManager(DefaultTestCerts::loadMutualAuthCaPem)
                     .commit() :
                     HttpClients.forSingleAddress(serverAddress))
+                    .ioExecutor(CLIENT_CTX.ioExecutor())
+                    .executionStrategy(defaultStrategy(CLIENT_CTX.executor()))
+                    .enableWireLogging("servicetalk-tests-client-wire-logger")
                     .buildStreaming();
             connection = client.reserveConnection(client.get("/")).toFuture().get();
-            connection.onClose().whenFinally(connectionClosed::countDown).subscribe();
+            connection.onClose().whenFinally(clientConnectionClosed::countDown).subscribe();
         }
 
         @After
         public void tearDown() throws Exception {
             try {
-                newCompositeCloseable().appendAll(connection, client, serverContext, serverIoExecutor).close();
+                newCompositeCloseable().appendAll(connection, client, serverContext).close();
             } finally {
                 if (proxyTunnel != null) {
                     safeClose(proxyTunnel);
@@ -176,12 +208,16 @@ public class ConnectionCloseHeaderHandlingTest {
         }
 
         protected static void assertResponsePayloadBody(StreamingHttpResponse response) throws Exception {
+            CharSequence contentLengthHeader = response.headers().get(CONTENT_LENGTH);
+            assertThat(contentLengthHeader, is(notNullValue()));
             int actualContentLength = response.payloadBody().map(Buffer::readableBytes)
-                    .collect(AtomicInteger::new, (total, current) -> {
-                        total.addAndGet(current);
-                        return total;
-                    }).toFuture().get().get();
-            assertThat(response.headers().get(CONTENT_LENGTH), contentEqualTo(valueOf(actualContentLength)));
+                    .collect(() -> 0, Integer::sum).toFuture().get();
+            assertThat(valueOf(actualContentLength), contentEqualTo(contentLengthHeader));
+        }
+
+        protected void awaitConnectionClosed() throws Exception {
+            clientConnectionClosed.await();
+            serverConnectionClosed.await();
         }
     }
 
@@ -239,6 +275,8 @@ public class ConnectionCloseHeaderHandlingTest {
             }
             if (requestInitiatesClosure) {
                 request.addHeader(CONNECTION, CLOSE);
+            } else {
+                request.addQueryParameter(SERVER_SHOULD_CLOSE, "true");
             }
 
             sendResponse.countDown();
@@ -251,7 +289,7 @@ public class ConnectionCloseHeaderHandlingTest {
             requestPayloadReceived.await();
             assertThat(request.headers().get(CONTENT_LENGTH), contentEqualTo(valueOf(requestPayloadSize.get())));
 
-            connectionClosed.await();
+            awaitConnectionClosed();
             assertClosedChannelException("/second");
         }
     }
@@ -275,13 +313,13 @@ public class ConnectionCloseHeaderHandlingTest {
 
         @Test
         public void serverCloseTwoPipelinedRequestsSentBeforeFirstResponse() throws Exception {
-            AtomicReference<StreamingHttpResponse> firstResponse = new AtomicReference<>();
             AtomicReference<Throwable> secondRequestError = new AtomicReference<>();
             CountDownLatch secondResponseReceived = new CountDownLatch(1);
 
             connection.request(connection.get("/first")
+                    .addQueryParameter(SERVER_SHOULD_CLOSE, "true")
                     .addHeader(CONTENT_LENGTH, ZERO)).subscribe(first -> {
-                firstResponse.set(first);
+                responses.add(first);
                 responseReceived.countDown();
             });
             connection.request(connection.get("/second")
@@ -291,13 +329,12 @@ public class ConnectionCloseHeaderHandlingTest {
                     .subscribe(second -> { });
             requestReceived.await();
             sendResponse.countDown();
-            responseReceived.await();
 
-            StreamingHttpResponse response = firstResponse.get();
+            StreamingHttpResponse response = responses.take();
             assertResponse(response);
             assertResponsePayloadBody(response);
 
-            connectionClosed.await();
+            awaitConnectionClosed();
             secondResponseReceived.await();
             assertThat(secondRequestError.get(), instanceOf(ClosedChannelException.class));
             assertClosedChannelException("/third");
@@ -305,13 +342,13 @@ public class ConnectionCloseHeaderHandlingTest {
 
         @Test
         public void serverCloseSecondPipelinedRequestWriteAborted() throws Exception {
-            AtomicReference<StreamingHttpResponse> firstResponse = new AtomicReference<>();
             AtomicReference<Throwable> secondRequestError = new AtomicReference<>();
             CountDownLatch secondResponseReceived = new CountDownLatch(1);
 
             connection.request(connection.get("/first")
+                    .addQueryParameter(SERVER_SHOULD_CLOSE, "true")
                     .addHeader(CONTENT_LENGTH, ZERO)).subscribe(first -> {
-                firstResponse.set(first);
+                responses.add(first);
                 responseReceived.countDown();
             });
             String content = "request_content";
@@ -323,13 +360,12 @@ public class ConnectionCloseHeaderHandlingTest {
                     .subscribe(second -> { });
             requestReceived.await();
             sendResponse.countDown();
-            responseReceived.await();
 
-            StreamingHttpResponse response = firstResponse.get();
+            StreamingHttpResponse response = responses.take();
             assertResponse(response);
             assertResponsePayloadBody(response);
 
-            connectionClosed.await();
+            awaitConnectionClosed();
             secondResponseReceived.await();
             assertThat(secondRequestError.get(), instanceOf(ClosedChannelException.class));
             assertClosedChannelException("/third");
@@ -339,6 +375,7 @@ public class ConnectionCloseHeaderHandlingTest {
         public void serverCloseTwoPipelinedRequestsInSequence() throws Exception {
             sendResponse.countDown();
             StreamingHttpResponse response = connection.request(connection.get("/first")
+                    .addQueryParameter(SERVER_SHOULD_CLOSE, "true")
                     .addHeader(CONTENT_LENGTH, ZERO)).toFuture().get();
             assertResponse(response);
 
@@ -347,29 +384,53 @@ public class ConnectionCloseHeaderHandlingTest {
 
             responseReceived.countDown();
             assertResponsePayloadBody(response);
-            connectionClosed.await();
+            awaitConnectionClosed();
         }
 
         @Test
-        public void clientCloseTwoPipelinedRequestsSentBeforeFirstResponse() throws Exception {
-            AtomicReference<StreamingHttpResponse> firstResponse = new AtomicReference<>();
-
+        public void clientCloseTwoPipelinedRequestsSentFirstInitiatesClosure() throws Exception {
             connection.request(connection.get("/first")
                     .addHeader(CONTENT_LENGTH, ZERO)
                     // Request connection closure:
                     .addHeader(CONNECTION, CLOSE)).subscribe(first -> {
-                firstResponse.set(first);
+                responses.add(first);
                 responseReceived.countDown();
             });
             // Send another request before connection receives a response for the first request:
             assertClosedChannelException("/second");
             sendResponse.countDown();
-            responseReceived.await();
 
-            StreamingHttpResponse response = firstResponse.get();
+            StreamingHttpResponse response = responses.take();
             assertResponse(response);
             assertResponsePayloadBody(response);
-            connectionClosed.await();
+            awaitConnectionClosed();
+        }
+
+        @Test
+        public void clientCloseTwoPipelinedRequestsSentSecondInitiatesClosure() throws Exception {
+            connection.request(connection.get("/first")
+                    .addHeader(CONTENT_LENGTH, ZERO))
+                    .subscribe(responses::add);
+
+            connection.request(connection.get("/second")
+                    .addHeader(CONTENT_LENGTH, ZERO)
+                    // Request connection closure:
+                    .addHeader(CONNECTION, CLOSE))
+                    .subscribe(responses::add);
+
+            sendResponse.countDown();
+
+            StreamingHttpResponse firstResponse = responses.take();
+            responseReceived.countDown();
+            assertThat(firstResponse.status(), is(OK));
+            assertResponsePayloadBody(firstResponse);
+
+            StreamingHttpResponse secondResponse = responses.take();
+            assertResponse(secondResponse);
+            assertResponsePayloadBody(secondResponse);
+
+            awaitConnectionClosed();
+            assertClosedChannelException("/third");
         }
     }
 }
