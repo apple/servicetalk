3827e17d7c17dc1fa2562d951a4e378b25df0126
Scott Mitchell <scott_mitchell@apple.com>
2020-10-15 11:53:09 -0700

    Add grpc protoc options example
    Motivation:
    We recently added a new option to servicetalk-grpc-protoc, but don't
    have an example to demonstrate its use and validate correct behavior.

---
+++ servicetalk-examples/grpc/protoc-options/src/main/java/io/servicetalk/examples/grpc/protocoptions/BlockingProtocOptionsClient.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.examples.grpc.protocoptions;
+
+import io.servicetalk.grpc.netty.GrpcClients;
+
+import io.grpc.examples.helloworld.GreeterSt.BlockingGreeterClient;
+import io.grpc.examples.helloworld.GreeterSt.ClientFactory;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+
+public final class BlockingProtocOptionsClient {
+
+    public static void main(String[] args) throws Exception {
+        try (BlockingGreeterClient client = GrpcClients.forAddress("localhost", 8080)
+                .buildBlocking(new ClientFactory())) {
+            HelloReply reply = client.sayHello(HelloRequest.newBuilder().setName("Foo").build());
+            System.out.println(reply);
+        }
+    }
+}
---
+++ servicetalk-examples/grpc/protoc-options/src/main/java/io/servicetalk/examples/grpc/protocoptions/BlockingProtocOptionsServer.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.examples.grpc.protocoptions;
+
+import io.servicetalk.grpc.api.GrpcServiceContext;
+import io.servicetalk.grpc.netty.GrpcServers;
+
+import io.grpc.examples.helloworld.GreeterSt.BlockingGreeterService;
+import io.grpc.examples.helloworld.GreeterSt.ServiceFactory;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+
+public class BlockingProtocOptionsServer {
+
+    public static void main(String[] args) throws Exception {
+        GrpcServers.forPort(8080)
+                .listenAndAwait(new ServiceFactory(new MyGreeterService()))
+                .awaitShutdown();
+    }
+
+    private static final class MyGreeterService implements BlockingGreeterService {
+
+        @Override
+        public HelloReply sayHello(final GrpcServiceContext ctx, final HelloRequest request) {
+            return HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+        }
+    }
+}
