b98aa8d33195ad1d0b149c7f76495256331064ae
Thomas Kountis <thomas_kountis@apple.com>
2020-12-10 10:13:52 +0000

    Http compression integration tests; combatibility with Netty (#1239)
    * Http compression integration tests; combatibility with Netty

---
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ServiceTalkContentCodingTest.java
@@ -0,0 +1,298 @@
+/*
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.concurrent.api.Single;
+import io.servicetalk.encoding.api.ContentCodec;
+import io.servicetalk.http.api.BlockingHttpClient;
+import io.servicetalk.http.api.BlockingStreamingHttpClient;
+import io.servicetalk.http.api.ContentCodingHttpRequesterFilter;
+import io.servicetalk.http.api.ContentCodingHttpServiceFilter;
+import io.servicetalk.http.api.FilterableStreamingHttpClient;
+import io.servicetalk.http.api.HttpExecutionStrategy;
+import io.servicetalk.http.api.HttpServerBuilder;
+import io.servicetalk.http.api.HttpServiceContext;
+import io.servicetalk.http.api.StreamingHttpClient;
+import io.servicetalk.http.api.StreamingHttpClientFilter;
+import io.servicetalk.http.api.StreamingHttpClientFilterFactory;
+import io.servicetalk.http.api.StreamingHttpRequest;
+import io.servicetalk.http.api.StreamingHttpRequester;
+import io.servicetalk.http.api.StreamingHttpResponse;
+import io.servicetalk.http.api.StreamingHttpResponseFactory;
+import io.servicetalk.http.api.StreamingHttpService;
+import io.servicetalk.http.api.StreamingHttpServiceFilter;
+import io.servicetalk.http.api.StreamingHttpServiceFilterFactory;
+import io.servicetalk.transport.api.HostAndPort;
+import io.servicetalk.transport.api.ServerContext;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import static io.servicetalk.concurrent.api.Publisher.from;
+import static io.servicetalk.concurrent.api.Single.failed;
+import static io.servicetalk.concurrent.api.Single.succeeded;
+import static io.servicetalk.encoding.api.ContentCodings.identity;
+import static io.servicetalk.http.api.CharSequences.contentEquals;
+import static io.servicetalk.http.api.HeaderUtilsPassThrough.encodingFor;
+import static io.servicetalk.http.api.HttpHeaderNames.ACCEPT_ENCODING;
+import static io.servicetalk.http.api.HttpHeaderNames.CONTENT_ENCODING;
+import static io.servicetalk.http.api.HttpResponseStatus.INTERNAL_SERVER_ERROR;
+import static io.servicetalk.http.api.HttpResponseStatus.UNSUPPORTED_MEDIA_TYPE;
+import static io.servicetalk.http.api.HttpSerializationProviders.textDeserializer;
+import static io.servicetalk.http.api.HttpSerializationProviders.textSerializer;
+import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
+import static io.servicetalk.transport.netty.internal.AddressUtils.serverHostAndPort;
+import static java.util.Arrays.stream;
+import static java.util.Collections.disjoint;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class ServiceTalkContentCodingTest extends BaseContentCodingTest {
+
+    private static final BiFunction<Scenario, List<Throwable>, StreamingHttpServiceFilterFactory> REQ_FILTER =
+            (scenario, errors) -> new StreamingHttpServiceFilterFactory() {
+        @Override
+        public StreamingHttpServiceFilter create(final StreamingHttpService service) {
+            return new StreamingHttpServiceFilter(service) {
+                @Override
+
+                public Single<StreamingHttpResponse> handle(final HttpServiceContext ctx,
+                                                            final StreamingHttpRequest request,
+                                                            final StreamingHttpResponseFactory responseFactory) {
+                    final ContentCodec reqEncoding = scenario.requestEncoding;
+                    final List<ContentCodec> clientSupportedEncodings = scenario.clientSupported;
+
+                    try {
+
+                        String requestPayload = request.payloadBody(textDeserializer())
+                                .collect(StringBuilder::new, StringBuilder::append)
+                                .toFuture().get().toString();
+
+                        assertEquals(payloadAsString((byte) 'a'), requestPayload);
+
+                        final List<String> actualReqAcceptedEncodings = stream(request.headers()
+                                .get(ACCEPT_ENCODING, "NOT_PRESENT").toString().split(","))
+                                .map((String::trim)).collect(toList());
+
+                        final List<String> expectedReqAcceptedEncodings = clientSupportedEncodings.stream()
+                                .filter((enc) -> enc != identity())
+                                .map((ContentCodec::name))
+                                .map(CharSequence::toString)
+                                .collect(toList());
+
+                        if (reqEncoding != identity()) {
+                            assertTrue("Request encoding should be present in the request headers",
+                                    contentEquals(reqEncoding.name(),
+                                            request.headers().get(ACCEPT_ENCODING, "NOT_PRESENT")));
+                        }
+
+                        if (!expectedReqAcceptedEncodings.isEmpty() && !actualReqAcceptedEncodings.isEmpty()) {
+                            assertThat(actualReqAcceptedEncodings, equalTo(expectedReqAcceptedEncodings));
+                        }
+
+                        return super.handle(ctx, request, responseFactory);
+                    } catch (Throwable t) {
+                        errors.add(t);
+                        return failed(t);
+                    }
+                }
+            };
+        }
+    };
+
+    static final BiFunction<Scenario, List<Throwable>, StreamingHttpClientFilterFactory> RESP_FILTER =
+            (scenario, errors) -> new StreamingHttpClientFilterFactory() {
+        @Override
+        public StreamingHttpClientFilter create(final FilterableStreamingHttpClient client) {
+            return new StreamingHttpClientFilter(client) {
+                @Override
+                protected Single<StreamingHttpResponse> request(final StreamingHttpRequester delegate,
+                                                                final HttpExecutionStrategy strategy,
+                                                                final StreamingHttpRequest request) {
+                    return super.request(delegate, strategy, request).map(response -> {
+                        if (INTERNAL_SERVER_ERROR.equals(response.status())) {
+                            // Ignore any further validations
+                            return response;
+                        }
+
+                        List<ContentCodec> server = scenario.serverSupported;
+                        List<ContentCodec> client = scenario.clientSupported;
+
+                        ContentCodec expected = identity();
+                        for (ContentCodec codec : client) {
+                            if (server.contains(codec)) {
+                                expected = codec;
+                                break;
+                            }
+                        }
+
+                        try {
+                            assertEquals(expected, encodingFor(client, response.headers()
+                                    .get(CONTENT_ENCODING, identity().name())));
+                        } catch (Throwable t) {
+                            errors.add(t);
+                            throw t;
+                        }
+                        return response;
+                    });
+                }
+            };
+        }
+    };
+
+    private ServerContext serverContext;
+    private BlockingHttpClient client;
+    protected List<Throwable> errors = Collections.synchronizedList(new ArrayList<>());
+
+    public ServiceTalkContentCodingTest(final HttpProtocol protocol, final Codings serverCodings,
+                                        final Codings clientCodings, final Compression compression,
+                                        final boolean valid) {
+        super(protocol, serverCodings, clientCodings, compression, valid);
+    }
+
+    @Before
+    public void start() throws Exception {
+        serverContext = newServiceTalkServer(scenario, errors);
+        client = newServiceTalkClient(serverHostAndPort(serverContext), scenario, errors);
+    }
+
+    @After
+    public void finish() throws Exception {
+        client.close();
+        serverContext.close();
+    }
+
+    protected BlockingHttpClient client() {
+        return client;
+    }
+
+    @Override
+    public void testCompatibility() throws Throwable {
+        super.testCompatibility();
+        verifyNoErrors();
+    }
+
+    private void verifyNoErrors() throws Throwable {
+        if (!errors.isEmpty()) {
+            throw errors.get(0);
+        }
+    }
+
+    protected void assertSuccessful(final ContentCodec encoding) throws Throwable {
+        assertResponse(client().request(client()
+                .get("/")
+                .encoding(encoding)
+                .payloadBody(payloadAsString((byte) 'a'), textSerializer())).toStreamingResponse());
+
+        final BlockingStreamingHttpClient blockingStreamingHttpClient = client().asBlockingStreamingClient();
+        assertResponse(blockingStreamingHttpClient.request(blockingStreamingHttpClient
+                .get("/")
+                .encoding(encoding)
+                .payloadBody(singletonList(payloadAsString((byte) 'a')), textSerializer())).toStreamingResponse());
+
+        final StreamingHttpClient streamingHttpClient = client().asStreamingClient();
+        assertResponse(streamingHttpClient.request(streamingHttpClient
+                .get("/")
+                .encoding(encoding)
+                .payloadBody(from(payloadAsString((byte) 'a')), textSerializer())).toFuture().get());
+    }
+
+    private void assertResponse(final StreamingHttpResponse response) throws Throwable {
+        verifyNoErrors();
+
+        assertResponseHeaders(response.headers().get(CONTENT_ENCODING, "identity").toString());
+
+        String responsePayload = response.payloadBody(textDeserializer()).collect(StringBuilder::new,
+                StringBuilder::append).toFuture().get().toString();
+
+        assertEquals(payloadAsString((byte) 'b'), responsePayload);
+    }
+
+    protected void assertNotSupported(final ContentCodec encoding) throws Exception {
+        final BlockingStreamingHttpClient blockingStreamingHttpClient = client().asBlockingStreamingClient();
+        final StreamingHttpClient streamingHttpClient = client().asStreamingClient();
+
+        assertEquals(UNSUPPORTED_MEDIA_TYPE, client().request(client()
+                .get("/")
+                .encoding(encoding)
+                .payloadBody(payloadAsString((byte) 'a'), textSerializer())).status());
+
+        assertEquals(UNSUPPORTED_MEDIA_TYPE, blockingStreamingHttpClient.request(blockingStreamingHttpClient
+                .get("/")
+                .encoding(encoding)
+                .payloadBody(singletonList(payloadAsString((byte) 'a')), textSerializer())).status());
+
+        assertEquals(UNSUPPORTED_MEDIA_TYPE, streamingHttpClient.request(streamingHttpClient
+                .get("/")
+                .encoding(encoding)
+                .payloadBody(from(payloadAsString((byte) 'a')), textSerializer())).toFuture().get().status());
+    }
+
+    protected void assertResponseHeaders(final String contentEncodingValue) {
+        final List<ContentCodec> clientSupportedEncodings = scenario.clientSupported;
+        final List<ContentCodec> serverSupportedEncodings = scenario.serverSupported;
+
+        if (disjoint(serverSupportedEncodings, clientSupportedEncodings)) {
+            assertEquals(identity().name().toString(), contentEncodingValue);
+        } else {
+            assertNotNull("Response encoding not in the client supported list " +
+                    "[" + clientSupportedEncodings + "]", encodingFor(clientSupportedEncodings, contentEncodingValue));
+
+            assertNotNull("Response encoding not in the server supported list " +
+                    "[" + serverSupportedEncodings + "]", encodingFor(serverSupportedEncodings, contentEncodingValue));
+        }
+    }
+
+    static ServerContext newServiceTalkServer(final Scenario scenario, final List<Throwable> errors)
+            throws Exception {
+        HttpServerBuilder httpServerBuilder = HttpServers.forAddress(localAddress(0));
+
+        StreamingHttpService service = (ctx, request, responseFactory) -> succeeded(responseFactory.ok()
+                .payloadBody(from(payloadAsString((byte) 'b')), textSerializer()));
+
+        StreamingHttpServiceFilterFactory filterFactory = REQ_FILTER.apply(scenario, errors);
+
+        return httpServerBuilder
+                .protocols(scenario.protocol.config)
+                .appendServiceFilter(new ContentCodingHttpServiceFilter(scenario.serverSupported,
+                        scenario.serverSupported))
+                .appendServiceFilter(filterFactory)
+                .listenStreamingAndAwait(service);
+    }
+
+    static BlockingHttpClient newServiceTalkClient(final HostAndPort hostAndPort, final Scenario scenario,
+                                                   final List<Throwable> errors) {
+        return HttpClients
+                .forSingleAddress(hostAndPort)
+                .appendClientFilter(RESP_FILTER.apply(scenario, errors))
+                .appendClientFilter(new ContentCodingHttpRequesterFilter(scenario.clientSupported))
+                .protocols(scenario.protocol.config)
+                .buildBlocking();
+    }
+}
---
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ServiceTalkToNettyContentCodingCompatibilityTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.concurrent.api.DefaultThreadFactory;
+import io.servicetalk.http.api.BlockingHttpClient;
+import io.servicetalk.transport.api.HostAndPort;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpContentCompressor;
+import io.netty.handler.codec.http.HttpContentDecompressor;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpServerCodec;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.net.InetSocketAddress;
+
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpHeaderValues.TEXT_PLAIN;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.servicetalk.http.api.HttpProtocolVersion.HTTP_2_0;
+import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
+import static io.servicetalk.transport.netty.internal.BuilderUtils.serverChannel;
+import static io.servicetalk.transport.netty.internal.NettyIoExecutors.createEventLoopGroup;
+import static java.lang.Thread.NORM_PRIORITY;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
+
+@RunWith(Parameterized.class)
+public class ServiceTalkToNettyContentCodingCompatibilityTest extends ServiceTalkContentCodingTest {
+
+    private EventLoopGroup serverEventLoopGroup;
+    private Channel serverAcceptorChannel;
+    private BlockingHttpClient client;
+
+    public ServiceTalkToNettyContentCodingCompatibilityTest(final HttpProtocol protocol,
+                                                            final Codings serverCodings, final Codings clientCodings,
+                                                            final Compression compression,
+                                                            final boolean valid) {
+        super(protocol, serverCodings, clientCodings, compression, valid);
+    }
+
+    @Before
+    public void start() {
+        serverEventLoopGroup = createEventLoopGroup(2, new DefaultThreadFactory("server-io", true, NORM_PRIORITY));
+        serverAcceptorChannel = newNettyServer();
+        InetSocketAddress serverAddress = (InetSocketAddress) serverAcceptorChannel.localAddress();
+        client = newServiceTalkClient(HostAndPort.of(serverAddress), scenario, errors);
+    }
+
+    @After
+    public void finish() throws Exception {
+        serverAcceptorChannel.close().syncUninterruptibly();
+        serverEventLoopGroup.shutdownGracefully(0, 0, MILLISECONDS).syncUninterruptibly();
+        client.close();
+    }
+
+    private Channel newNettyServer() {
+        ServerBootstrap sb = new ServerBootstrap();
+        sb.group(serverEventLoopGroup);
+        sb.channel(serverChannel(serverEventLoopGroup, InetSocketAddress.class));
+
+        sb.childHandler(new ChannelInitializer<Channel>() {
+            @Override
+            protected void initChannel(final Channel ch) {
+                ChannelPipeline p = ch.pipeline();
+                p.addLast(new HttpServerCodec());
+                if (!scenario.serverSupported.isEmpty()) {
+                    p.addLast(new HttpContentDecompressor());
+                    p.addLast(new HttpContentCompressor());
+                }
+                p.addLast(EchoServerHandler.INSTANCE);
+            }
+        });
+        return sb.bind(localAddress(0)).syncUninterruptibly().channel();
+    }
+
+    @Override
+    public void testCompatibility() throws Throwable {
+        assumeFalse("Only testing H1 scenarios yet.", scenario.protocol.version.equals(HTTP_2_0));
+        assumeTrue("Only testing successful configurations; Netty doesn't have knowledge " +
+                "about unsupported compression types.", scenario.valid);
+
+        super.testCompatibility();
+    }
+
+    @Override
+    protected BlockingHttpClient client() {
+        return client;
+    }
+
+    @ChannelHandler.Sharable
+    static class EchoServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+        static final EchoServerHandler INSTANCE = new EchoServerHandler();
+
+        private static final byte[] CONTENT = payload((byte) 'b');
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) {
+            ctx.flush();
+        }
+
+        @Override
+        public void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
+            if (msg instanceof io.netty.handler.codec.http.HttpRequest) {
+                io.netty.handler.codec.http.HttpRequest req = (io.netty.handler.codec.http.HttpRequest) msg;
+                FullHttpResponse response = new DefaultFullHttpResponse(req.protocolVersion(),
+                        OK, wrappedBuffer(CONTENT));
+
+                response.headers()
+                        .set(CONTENT_TYPE, TEXT_PLAIN)
+                        .setInt(CONTENT_LENGTH, response.content().readableBytes());
+
+                ctx.write(response);
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+}
