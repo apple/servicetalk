ab9229daf05433a1d8187711ce6f0b1210c09767
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2021-01-08 14:00:59 -0800

    Revert changes for `ResponseCancelTest`

--- servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ResponseCancelTest.java
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ResponseCancelTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020-2021 Apple Inc. and the ServiceTalk project authors
+ * Copyright © 2020 Apple Inc. and the ServiceTalk project authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,9 +37,6 @@ import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 import java.net.InetSocketAddress;
 import java.nio.channels.ClosedChannelException;
@@ -53,13 +50,14 @@ import static io.servicetalk.concurrent.api.AsyncCloseables.newCompositeCloseabl
 import static io.servicetalk.concurrent.api.Processors.newSingleProcessor;
 import static io.servicetalk.concurrent.api.Single.defer;
 import static io.servicetalk.concurrent.api.SourceAdapters.fromSource;
+import static io.servicetalk.http.netty.HttpClients.forSingleAddress;
+import static io.servicetalk.http.netty.HttpServers.forAddress;
 import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
 import static io.servicetalk.transport.netty.internal.AddressUtils.serverHostAndPort;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
-@RunWith(Parameterized.class)
 public class ResponseCancelTest {
 
     private final BlockingQueue<Processor<HttpResponse, HttpResponse>> serverResponses;
@@ -72,19 +70,17 @@ public class ResponseCancelTest {
     @Rule
     public final Timeout timeout = new ServiceTalkTestTimeout();
 
-    public ResponseCancelTest(HttpProtocol protocol) throws Exception {
+    public ResponseCancelTest() throws Exception {
         serverResponses = new LinkedBlockingQueue<>();
         delayedClientCancels = new LinkedBlockingQueue<>();
         delayedClientTermination = new LinkedBlockingQueue<>();
-        ctx = HttpServers.forAddress(localAddress(0))
-                .protocols(protocol.config)
+        ctx = forAddress(localAddress(0))
                 .listenAndAwait((__, ___, factory) -> {
                     Processor<HttpResponse, HttpResponse> resp = newSingleProcessor();
                     serverResponses.add(resp);
                     return fromSource(resp);
                 });
-        client = HttpClients.forSingleAddress(serverHostAndPort(ctx))
-                .protocols(protocol.config)
+        client = forSingleAddress(serverHostAndPort(ctx))
                 .appendConnectionFilter(connection -> new StreamingHttpConnectionFilter(connection) {
                     @Override
                     public Single<StreamingHttpResponse> request(final HttpExecutionStrategy strategy,
@@ -110,11 +106,6 @@ public class ResponseCancelTest {
                 })
                 .appendConnectionFactoryFilter(original -> new CountingConnectionFactory(original, connectionCount))
                 .build();
-    }
-
-    @Parameters(name = "protocol={0}")
-    public static HttpProtocol[] data() {
-        return HttpProtocol.values();
     }
 
     @After
