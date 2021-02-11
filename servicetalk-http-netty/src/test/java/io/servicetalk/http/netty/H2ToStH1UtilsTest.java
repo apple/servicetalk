2c424ac8ee6d294dca1fe2722ce0d77821c4cbf3
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2021-02-11 11:41:21 -0800

    Test that H2ToStH1Utils.h1HeadersSplitCookieCrumbs works with any HttpHeadersFactory impl

---
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/H2ToStH1UtilsTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2021 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.http.api.DefaultHttpCookiePair;
+import io.servicetalk.http.api.DefaultHttpHeadersFactory;
+import io.servicetalk.http.api.HttpCookiePair;
+import io.servicetalk.http.api.HttpHeaders;
+import io.servicetalk.http.api.HttpHeadersFactory;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.servicetalk.http.api.HttpHeaderNames.ACCEPT_PATCH;
+import static io.servicetalk.http.api.HttpHeaderNames.COOKIE;
+import static io.servicetalk.http.api.HttpHeaderValues.TEXT_PLAIN;
+import static io.servicetalk.http.netty.H2ToStH1Utils.h1HeadersSplitCookieCrumbs;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+
+public class H2ToStH1UtilsTest {
+
+    @Test
+    public void testH1HeadersSplitCookieCrumbsForH1Headers() {
+        testH1HeadersSplitCookieCrumbs(DefaultHttpHeadersFactory.INSTANCE);
+    }
+
+    @Test
+    public void testH1HeadersSplitCookieCrumbsForH2Headers() {
+        testH1HeadersSplitCookieCrumbs(H2HeadersFactory.INSTANCE);
+    }
+
+    public void testH1HeadersSplitCookieCrumbs(HttpHeadersFactory headersFactory) {
+        HttpHeaders headers = headersFactory.newHeaders();
+        headers.add(COOKIE, "a=b; c=d; e=f");
+        headers.add(ACCEPT_PATCH, TEXT_PLAIN);  // Add another header which will be saved in the same entries[index]
+        h1HeadersSplitCookieCrumbs(headers);
+
+        List<HttpCookiePair> cookies = new ArrayList<>();
+        for (HttpCookiePair pair : headers.getCookies()) {
+            cookies.add(pair);
+        }
+        assertThat(cookies, hasSize(3));
+        assertThat(cookies, containsInAnyOrder(new DefaultHttpCookiePair("a", "b"),
+                new DefaultHttpCookiePair("c", "d"),
+                new DefaultHttpCookiePair("e", "f")));
+    }
+}
