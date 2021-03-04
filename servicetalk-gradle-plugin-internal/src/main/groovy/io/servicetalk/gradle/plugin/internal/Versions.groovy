3d8c7955faa26e7eb133921d067b5a8e95054433
Scott Mitchell <scott_mitchell@apple.com>
2021-03-03 19:14:09 -0800

    Update checkstyle 8.33 -> 8.41 (#1397)

--- servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
+++ servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
@@ -20,7 +20,7 @@ import org.gradle.api.JavaVersion
 import static org.gradle.api.JavaVersion.VERSION_1_8
 
 final class Versions {
-  static final String CHECKSTYLE_VERSION = "8.33"
+  static final String CHECKSTYLE_VERSION = "8.41"
   static final String PMD_VERSION = "6.24.0"
   static final String SPOTBUGS_VERSION = "4.2.2"
   static final JavaVersion TARGET_VERSION = VERSION_1_8
