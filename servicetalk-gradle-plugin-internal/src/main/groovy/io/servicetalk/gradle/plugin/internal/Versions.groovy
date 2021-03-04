fb885ae44f391e381c62b818a4b75d6a2a6a4f2c
Scott Mitchell <scott_mitchell@apple.com>
2021-03-03 18:47:53 -0800

    Update pmd 6.24.0 -> 6.32.0

--- servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
+++ servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
@@ -21,7 +21,7 @@ import static org.gradle.api.JavaVersion.VERSION_1_8
 
 final class Versions {
   static final String CHECKSTYLE_VERSION = "8.41"
-  static final String PMD_VERSION = "6.24.0"
+  static final String PMD_VERSION = "6.32.0"
   static final String SPOTBUGS_VERSION = "4.2.2"
   static final JavaVersion TARGET_VERSION = VERSION_1_8
 }
