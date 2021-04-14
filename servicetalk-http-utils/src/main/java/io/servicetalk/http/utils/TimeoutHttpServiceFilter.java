75a546f803d66a60ac2c45253b30b935c64594c6
Mike Duigou <bondolo@users.noreply.github.com>
2021-04-14 16:00:20 -0700

    Use only monotonic clock in deadline calculations (#1481)
    Motivation:
    The default clock used by `java.time.Instant` is not monotonic. This
    could result in deadlines not being correctly managed if the clock is
    adjusted during the course of timeout executing.
    Modifications:
    Replace use of `Instant` with `System.nanoTime()`
    Result:
    Timeout HTTP filters have monotonic behaviour.

--- servicetalk-http-utils/src/main/java/io/servicetalk/http/utils/TimeoutHttpServiceFilter.java
+++ servicetalk-http-utils/src/main/java/io/servicetalk/http/utils/TimeoutHttpServiceFilter.java
@@ -29,7 +29,6 @@ import io.servicetalk.http.api.StreamingHttpServiceFilter;
 import io.servicetalk.http.api.StreamingHttpServiceFilterFactory;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 import javax.annotation.Nullable;
@@ -166,10 +165,10 @@ public final class TimeoutHttpServiceFilter
                             response.timeout(timeout) : response.timeout(timeout, timeoutExecutor);
 
                     if (fullRequestResponse) {
-                        Instant deadline = Instant.now().plus(timeout);
+                        long deadline = System.nanoTime() + timeout.toNanos();
                         response = timeoutResponse.map(resp -> resp.transformMessageBody(body ->
                                 Publisher.defer(() -> {
-                                    Duration remaining = Duration.between(Instant.now(), deadline);
+                                    Duration remaining = Duration.ofNanos(deadline - System.nanoTime());
                                     return (Duration.ZERO.compareTo(remaining) <= 0 ?
                                             Publisher.failed(
                                                     new TimeoutException("timeout after " + timeout.toMillis() + "ms"))
