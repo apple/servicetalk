af6dd73f882bab823cd3c6196bd4a76e2f67a45c
Riley Park <riley.park@meino.net>
2021-06-01 20:48:55 -0700

    Migrate servicetalk-transport-netty-internal tests to JUnit 5 (#1568)
    Motivation:
    
    - JUnit 5 leverages features from Java 8 or later, such as lambda functions, making tests more powerful and easier to maintain.
    - JUnit 5 has added some very useful new features for describing, organizing, and executing tests. For instance, tests get better display names and can be organized hierarchically.
    - JUnit 5 is organized into multiple libraries, so only the features you need are imported into your project. With build systems such as Maven and Gradle, including the right libraries is easy.
    - JUnit 5 can use more than one extension at a time, which JUnit 4 could not (only one runner could be used at a time). This means you can easily combine the Spring extension with other extensions (such as your own custom extension).
    
    Modifications:
    
    - Unit tests have been migrated from JUnit 4 to JUnit 5
    
    Result:
    
    Module servicetalk-transport-netty-internal now runs tests using JUnit 5

--- servicetalk-transport-netty-internal/src/test/java/io/servicetalk/transport/netty/internal/NettyChannelPublisherRefCountTest.java
+++ servicetalk-transport-netty-internal/src/test/java/io/servicetalk/transport/netty/internal/NettyChannelPublisherRefCountTest.java
@@ -16,17 +16,14 @@
 package io.servicetalk.transport.netty.internal;
 
 import io.servicetalk.concurrent.api.Publisher;
-import io.servicetalk.concurrent.internal.ServiceTalkTestTimeout;
 import io.servicetalk.concurrent.test.internal.TestPublisherSubscriber;
 import io.servicetalk.transport.api.ConnectionInfo.Protocol;
 import io.servicetalk.transport.netty.internal.NoopTransportObserver.NoopConnectionObserver;
 
 import io.netty.buffer.ByteBuf;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.Timeout;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -42,15 +39,13 @@ import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 
-public class NettyChannelPublisherRefCountTest {
-    @Rule
-    public final Timeout timeout = new ServiceTalkTestTimeout();
+class NettyChannelPublisherRefCountTest {
 
     private final TestPublisherSubscriber<Object> subscriber = new TestPublisherSubscriber<>();
     private Publisher<Object> publisher;
     private EmbeddedDuplexChannel channel;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         channel = new EmbeddedDuplexChannel(false);
         publisher = DefaultNettyConnection.initChannel(channel, DEFAULT_ALLOCATOR, immediate(), x -> false,
@@ -59,7 +54,7 @@ public class NettyChannelPublisherRefCountTest {
                 .read();
     }
 
-    @After
+    @AfterEach
     public void tearDown() throws Exception {
         if (!channel.close().await(DEFAULT_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
             throw new IllegalStateException("Channel close not finished in 1 second.");
@@ -67,7 +62,7 @@ public class NettyChannelPublisherRefCountTest {
     }
 
     @Test
-    public void testRefCountedLeaked() {
+    void testRefCountedLeaked() {
         toSource(publisher).subscribe(subscriber);
         subscriber.awaitSubscription().request(3);
         ByteBuf buffer = channel.alloc().buffer();
