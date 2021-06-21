516674974b1e8d025fa3bad7e4b23eb558e0f21c
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2021-06-21 13:07:36 -0700

    Remove `append` methods that were deprecated in #1578
    Motivation:
    
    `append` method on each filter factory was deprecated in #1578 and can
    be removed now.
    
    Modifications:
    
    - Remove `append` method from `ConnectionFactoryFilter`,
    `GrpcServiceFilterFactory`, `MultiAddressHttpClientFilterFactory`,
    `StreamingHttpClientFilterFactory`, `StreamingHttpConnectionFilterFactory`,
    `StreamingHttpServiceFilterFactory`.
    
    Result:
    
    No deprecated methods on filter factory API.

--- servicetalk-http-api/src/main/java/io/servicetalk/http/api/StreamingHttpConnectionFilterFactory.java
+++ servicetalk-http-api/src/main/java/io/servicetalk/http/api/StreamingHttpConnectionFilterFactory.java
@@ -15,8 +15,6 @@
  */
 package io.servicetalk.http.api;
 
-import static java.util.Objects.requireNonNull;
-
 /**
  * A factory for {@link StreamingHttpConnectionFilter}.
  */
@@ -30,29 +28,4 @@ public interface StreamingHttpConnectionFilterFactory {
      * @return {@link StreamingHttpConnectionFilter} using the provided {@link FilterableStreamingHttpConnection}.
      */
     StreamingHttpConnectionFilter create(FilterableStreamingHttpConnection connection);
-
-    /**
-     * Returns a composed function that first applies the {@code before} function to its input, and then applies
-     * this function to the result.
-     * <p>
-     * The order of execution of these filters are in order of append. If 3 filters are added as follows:
-     * <pre>
-     *     filter1.append(filter2).append(filter3)
-     * </pre>
-     * making a request to a connection wrapped by this filter chain the order of invocation of these filters will
-     * be:
-     * <pre>
-     *     filter1 =&gt; filter2 =&gt; filter3 =&gt; connection
-     * </pre>
-     *
-     * @deprecated Use {@link HttpClientBuilder#appendConnectionFilter(StreamingHttpConnectionFilterFactory)}
-     * @param before the function to apply before this function is applied
-     * @return a composed function that first applies the {@code before}
-     * function and then applies this function
-     */
-    @Deprecated
-    default StreamingHttpConnectionFilterFactory append(StreamingHttpConnectionFilterFactory before) {
-        requireNonNull(before);
-        return connection -> create(before.create(connection));
-    }
 }
