54c66d2de6c36e6a1c12da43fbed222e381d3113
Scott Mitchell <scott_mitchell@apple.com>
2021-07-19 11:00:36 -0700

    add missing comments for StreamingSerializer

--- servicetalk-serializer-api/src/main/java/io/servicetalk/serializer/api/StreamingDeserializer.java
+++ servicetalk-serializer-api/src/main/java/io/servicetalk/serializer/api/StreamingDeserializer.java
@@ -28,8 +28,20 @@ import static io.servicetalk.concurrent.api.Publisher.fromIterable;
  */
 @FunctionalInterface
 public interface StreamingDeserializer<T> {
+    /**
+     * Deserialize a {@link Publisher} of {@link Buffer} into a {@link Publisher} of {@link T}.
+     * @param serializedData the serialized stream of data represented in a {@link Publisher} of {@link Buffer}.
+     * @param allocator the {@link BufferAllocator} to use if allocation is required.
+     * @return The deserialized {@link Publisher} of {@link T}s.
+     */
     Publisher<T> deserialize(Publisher<Buffer> serializedData, BufferAllocator allocator);
 
+    /**
+     * Deserialize a {@link Iterable} of {@link Buffer} into a {@link Iterable} of {@link T}.
+     * @param serializedData the serialized stream data represented in a {@link Iterable} of {@link Buffer}.
+     * @param allocator the {@link BufferAllocator} to use if allocation is required.
+     * @return The deserialized {@link Iterable} of {@link T}s.
+     */
     default BlockingIterable<T> deserialize(Iterable<Buffer> serializedData, BufferAllocator allocator) {
         return deserialize(fromIterable(serializedData), allocator).toIterable();
     }
