a0a05449f08011354c3275e87073aaca9b05c4de
Mike Duigou <bondolo@users.noreply.github.com>
2021-08-11 09:36:52 -0700

    Add support for nonOffloading HTTP Server Filters (#1710)
    Motivation:
    Some filters may wish to run early in the request process before the
    request has been offloaded.
    Modifications:
    A new API,
    `HttpServerBuilder.appendNonOffloadingServiceFilter(StreamingHttpServiceFilterFactory)`
    is provided for registering filters which will not be offloaded.
    Offloading will be handled in the filter chain by the new
    `OffloadingFilter` which is logically inserted between the
    non-offloading and offloading filters. The new offloading filter also replaces
    one of the `HttpExecutionStrategy.invokeService` methods.
    Result:
    New options for filters.

--- servicetalk-http-router-predicate/src/main/java/io/servicetalk/http/router/predicate/InOrderRouter.java
+++ servicetalk-http-router-predicate/src/main/java/io/servicetalk/http/router/predicate/InOrderRouter.java
@@ -17,6 +17,7 @@ package io.servicetalk.http.router.predicate;
 
 import io.servicetalk.concurrent.api.AsyncCloseable;
 import io.servicetalk.concurrent.api.Completable;
+import io.servicetalk.concurrent.api.Executor;
 import io.servicetalk.concurrent.api.Single;
 import io.servicetalk.http.api.HttpExecutionStrategy;
 import io.servicetalk.http.api.HttpServiceContext;
@@ -67,7 +68,9 @@ final class InOrderRouter implements StreamingHttpService {
                 StreamingHttpService service = pair.service();
                 final HttpExecutionStrategy strategy = pair.routeStrategy();
                 if (strategy != null) {
-                    service = strategy.offloadService(ctx.executionContext().executor(), service);
+                    Executor se = strategy.executor();
+                    Executor executor = null == se ? ctx.executionContext().executor() : se;
+                    service = strategy.offloadService(executor, service);
                 }
                 return service.handle(ctx, request, factory);
             }
