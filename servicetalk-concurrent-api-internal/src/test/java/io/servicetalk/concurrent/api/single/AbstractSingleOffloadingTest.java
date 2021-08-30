7ab361f0eec7cad06fa69fdacb131cdc7c84e80b
Mike Duigou <mike_duigou@apple.com>
2021-08-30 16:56:58 -0700

    Adds stack capture to OffloadingTest Motivation: There have been occasional failures with the offloading test but the test failure reports lack a critical piece of information; the stack dump for the failure. Modifications: Add stack capturing to `AbstractOffloadingTest` Result: Hopefully more information for debugging flaky tests.

--- servicetalk-concurrent-api-internal/src/test/java/io/servicetalk/concurrent/api/single/AbstractSingleOffloadingTest.java
+++ servicetalk-concurrent-api-internal/src/test/java/io/servicetalk/concurrent/api/single/AbstractSingleOffloadingTest.java
@@ -58,55 +58,55 @@ public abstract class AbstractSingleOffloadingTest extends AbstractOffloadingTes
                 // Insert a custom value into AsyncContext map
                 AsyncContext.current().put(ASYNC_CONTEXT_CUSTOM_KEY, ASYNC_CONTEXT_VALUE);
 
-                capture(CaptureSlot.IN_APP);
+                capture(CaptureSlot.APP);
 
                 // Add thread recording test points
                 final Single<String> original = testSingle
                         .liftSync((SingleOperator<String, String>) subscriber -> {
-                            capture(CaptureSlot.IN_OFFLOADED_SUBSCRIBE);
+                            capture(CaptureSlot.OFFLOADED_SUBSCRIBE);
                             return subscriber;
                         })
-                        .beforeOnSubscribe(cancellable -> capture(CaptureSlot.IN_ORIGINAL_ON_SUBSCRIBE))
+                        .beforeOnSubscribe(cancellable -> capture(CaptureSlot.ORIGINAL_ON_SUBSCRIBE))
                         .beforeFinally(new TerminalSignalConsumer() {
 
                             @Override
                             public void onComplete() {
-                                capture(CaptureSlot.IN_ORIGINAL_ON_COMPLETE);
+                                capture(CaptureSlot.ORIGINAL_ON_COMPLETE);
                             }
 
                             @Override
                             public void onError(final Throwable throwable) {
-                                capture(CaptureSlot.IN_ORIGINAL_ON_ERROR);
+                                capture(CaptureSlot.ORIGINAL_ON_ERROR);
                             }
 
                             @Override
                             public void cancel() {
-                                capture(CaptureSlot.IN_OFFLOADED_CANCEL);
+                                capture(CaptureSlot.OFFLOADED_CANCEL);
                             }
                         });
 
                 // Perform offloading and add more thread recording test points
                 Single<String> offloaded = offloadingFunction.apply(original, testExecutor.executor())
                         .liftSync((SingleOperator<String, String>) subscriber -> {
-                            capture(CaptureSlot.IN_ORIGINAL_SUBSCRIBE);
+                            capture(CaptureSlot.ORIGINAL_SUBSCRIBE);
                             return subscriber;
                         })
-                        .beforeOnSubscribe(cancellable -> capture(CaptureSlot.IN_OFFLOADED_ON_SUBSCRIBE))
+                        .beforeOnSubscribe(cancellable -> capture(CaptureSlot.OFFLOADED_ON_SUBSCRIBE))
                         .beforeFinally(new TerminalSignalConsumer() {
 
                             @Override
                             public void onComplete() {
-                                capture(CaptureSlot.IN_OFFLOADED_ON_COMPLETE);
+                                capture(CaptureSlot.OFFLOADED_ON_COMPLETE);
                             }
 
                             @Override
                             public void onError(final Throwable throwable) {
-                                capture(CaptureSlot.IN_OFFLOADED_ON_ERROR);
+                                capture(CaptureSlot.OFFLOADED_ON_ERROR);
                             }
 
                             @Override
                             public void cancel() {
-                                capture(CaptureSlot.IN_ORIGINAL_CANCEL);
+                                capture(CaptureSlot.ORIGINAL_CANCEL);
                             }
                         });
 
@@ -167,15 +167,15 @@ public abstract class AbstractSingleOffloadingTest extends AbstractOffloadingTes
         }
 
         // Ensure that Async Context Map was correctly set during signals
-        AsyncContextMap appMap = capturedContexts.captured(CaptureSlot.IN_APP);
+        AsyncContextMap appMap = capturedContexts.captured(CaptureSlot.APP);
         assertThat(appMap, notNullValue());
-        AsyncContextMap subscribeMap = capturedContexts.captured(CaptureSlot.IN_ORIGINAL_SUBSCRIBE);
+        AsyncContextMap subscribeMap = capturedContexts.captured(CaptureSlot.ORIGINAL_SUBSCRIBE);
         assertThat(subscribeMap, notNullValue());
         assertThat("Map was shared not copied", subscribeMap, not(sameInstance(appMap)));
         assertThat("Missing custom async context entry ",
                 subscribeMap.get(ASYNC_CONTEXT_CUSTOM_KEY), sameInstance(ASYNC_CONTEXT_VALUE));
         EnumSet<CaptureSlot> checkSlots =
-                EnumSet.complementOf(EnumSet.of(CaptureSlot.IN_APP, CaptureSlot.IN_ORIGINAL_SUBSCRIBE));
+                EnumSet.complementOf(EnumSet.of(CaptureSlot.APP, CaptureSlot.ORIGINAL_SUBSCRIBE));
         checkSlots.stream()
                 .filter(slot -> null != capturedContexts.captured(slot))
                 .forEach(slot -> {
