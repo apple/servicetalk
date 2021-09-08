7d5e1919202e32b21195ab2c909be563f0c008d4
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2021-09-08 17:14:02 -0500

    Remove `payloadBody(CloseableIterable<Buffer>)` methods (#1802)
    Motivation:
    
    `payloadBody(CloseableIterable<Buffer>)`
    and `payloadBody(CloseableIterable<Buffer>, HttpSerializer<T>)` methods
    on `BlockingStreamingHttpRequest` and `BlockingStreamingHttpResponse`
    were deprecated in 0.41 branch [1], we can safely remove them from the
    `main` branch.
    
    1. https://github.com/apple/servicetalk/commit/42688df6c00f229292ba48b30b41a83aa96a1415
    
    Modifications:
    
    - Remove `payloadBody(CloseableIterable<Buffer>)`
    and `payloadBody(CloseableIterable<Buffer>, HttpSerializer<T>)` methods
    from `BlockingStreamingHttpRequest` and `BlockingStreamingHttpResponse`;
    
    Result:
    
    Less deprecated API.

--- servicetalk-http-api/src/main/java/io/servicetalk/http/api/DefaultBlockingStreamingHttpRequest.java
+++ servicetalk-http-api/src/main/java/io/servicetalk/http/api/DefaultBlockingStreamingHttpRequest.java
@@ -17,7 +17,6 @@ package io.servicetalk.http.api;
 
 import io.servicetalk.buffer.api.Buffer;
 import io.servicetalk.concurrent.BlockingIterable;
-import io.servicetalk.concurrent.CloseableIterable;
 import io.servicetalk.concurrent.api.Single;
 import io.servicetalk.encoding.api.BufferEncoder;
 import io.servicetalk.encoding.api.ContentCodec;
@@ -173,12 +172,6 @@ final class DefaultBlockingStreamingHttpRequest extends AbstractDelegatingHttpRe
     }
 
     @Override
-    public BlockingStreamingHttpRequest payloadBody(final CloseableIterable<Buffer> payloadBody) {
-        original.payloadBody(fromIterable(payloadBody));
-        return this;
-    }
-
-    @Override
     public BlockingStreamingHttpRequest payloadBody(final InputStream payloadBody) {
         original.payloadBody(fromInputStream(payloadBody)
                 .map(bytes -> original.payloadHolder().allocator().wrap(bytes)));
@@ -219,14 +212,6 @@ final class DefaultBlockingStreamingHttpRequest extends AbstractDelegatingHttpRe
     @Override
     public <T> BlockingStreamingHttpRequest payloadBody(final Iterable<T> payloadBody,
                                                         final HttpStreamingSerializer<T> serializer) {
-        original.payloadBody(fromIterable(payloadBody), serializer);
-        return this;
-    }
-
-    @Deprecated
-    @Override
-    public <T> BlockingStreamingHttpRequest payloadBody(final CloseableIterable<T> payloadBody,
-                                                        final HttpSerializer<T> serializer) {
         original.payloadBody(fromIterable(payloadBody), serializer);
         return this;
     }
