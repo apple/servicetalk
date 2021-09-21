e74da72ed4928963037cf812a7b3fa54c2765872
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2021-09-21 15:54:20 -0700

    Get more visibility into test failures

--- servicetalk-grpc-netty/src/test/java/io/servicetalk/grpc/netty/GrpcLifecycleObserverTest.java
+++ servicetalk-grpc-netty/src/test/java/io/servicetalk/grpc/netty/GrpcLifecycleObserverTest.java
@@ -45,6 +45,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.concurrent.Callable;
@@ -207,6 +208,9 @@ class GrpcLifecycleObserverTest {
                                         GrpcLifecycleObserver lifecycle, GrpcExchangeObserver exchange,
                                         GrpcRequestObserver request, GrpcResponseObserver response,
                                         InOrder inOrder, InOrder requestInOrder) {
+        if (!client && !aggregated) {
+            System.err.println(Mockito.mockingDetails(response).printInvocations());
+        }
         inOrder.verify(lifecycle).onNewExchange();
         if (client) {
             inOrder.verify(exchange).onRequest(any(StreamingHttpRequest.class));
