a93fc9bede7743b3eb07e016e461e5ebd57ba9fc
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2022-03-09 17:03:13 -0600

    Decouple multi-address and partitioned client builders from `HttpClientBuildContext` (#2136)
    Motivation:
    
    `DefaultMultiAddressHttpClientBuilder` and
    `DefaultPartitionedHttpClientBuilder` use
    `DefaultSingleAddressHttpClientBuilder` as a template for underlying
    single-address clients. It was helpful before we introduced a
    `SingleAddressInitializer` in #1387. Before that, all builders had the
    same set of methods and it was nice to reuse the single-address builder
    instead of maintaining a similar state at higher level builders. Now,
    `SingleAddressInitializer` helps to avoid duplication between builders.
    We don't have to use `DefaultSingleAddressHttpClientBuilder` as a
    template and can simplify its state by using a factory for
    `SingleAddressHttpClientBuilder`.
    
    Modifications:
    
    - `DefaultMultiAddressUrlHttpClientBuilder` and
    `DefaultPartitionedHttpClientBuilder` use a factory for
    `SingleAddressHttpClientBuilder` instead of
    `DefaultSingleAddressHttpClientBuilder` as a template and
    `HttpExecutionContextBuilder` for context-related items;
    - Remove and hide methods from `DefaultSingleAddressHttpClientBuilder`
    that are not used by other builders anymore;
    - Add a test to verify `DefaultMultiAddressUrlHttpClientBuilder` sets
    execution context items for underlying single-address builders;
    
    Result:
    
    `DefaultSingleAddressHttpClientBuilder` has less pkg-private API,
    `DefaultMultiAddressUrlHttpClientBuilder` and
    `DefaultPartitionedHttpClientBuilder` create a new single-address
    builder on demand, similar to grpc builders.

--- servicetalk-http-netty/src/main/java/io/servicetalk/http/netty/DefaultMultiAddressUrlHttpClientBuilder.java
+++ servicetalk-http-netty/src/main/java/io/servicetalk/http/netty/DefaultMultiAddressUrlHttpClientBuilder.java
@@ -35,12 +35,12 @@ import io.servicetalk.http.api.HttpRequestMetaData;
 import io.servicetalk.http.api.HttpRequestMethod;
 import io.servicetalk.http.api.MultiAddressHttpClientBuilder;
 import io.servicetalk.http.api.RedirectConfig;
+import io.servicetalk.http.api.SingleAddressHttpClientBuilder;
 import io.servicetalk.http.api.StreamingHttpClient;
 import io.servicetalk.http.api.StreamingHttpRequest;
 import io.servicetalk.http.api.StreamingHttpRequestResponseFactory;
 import io.servicetalk.http.api.StreamingHttpResponse;
 import io.servicetalk.http.api.StreamingHttpResponseFactory;
-import io.servicetalk.http.netty.DefaultSingleAddressHttpClientBuilder.HttpClientBuildContext;
 import io.servicetalk.http.utils.RedirectingHttpRequesterFilter;
 import io.servicetalk.transport.api.ClientSslConfig;
 import io.servicetalk.transport.api.ClientSslConfigBuilder;
@@ -64,6 +64,7 @@ import static io.servicetalk.concurrent.api.AsyncCloseables.toListenableAsyncClo
 import static io.servicetalk.concurrent.api.Single.defer;
 import static io.servicetalk.concurrent.internal.SubscriberUtils.deliverCompleteFromSource;
 import static io.servicetalk.http.api.HttpProtocolVersion.HTTP_1_1;
+import static io.servicetalk.http.netty.DefaultSingleAddressHttpClientBuilder.setExecutionContext;
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -85,7 +86,8 @@ final class DefaultMultiAddressUrlHttpClientBuilder
 
     private static final String HTTPS_SCHEME = HTTPS.toString();
 
-    private final DefaultSingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress> builderTemplate;
+    private final Function<HostAndPort, SingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress>> builderFactory;
+    private final HttpExecutionContextBuilder executionContextBuilder = new HttpExecutionContextBuilder();
 
     @Nullable
     private HttpHeadersFactory headersFactory;
@@ -95,19 +97,17 @@ final class DefaultMultiAddressUrlHttpClientBuilder
     private SingleAddressInitializer<HostAndPort, InetSocketAddress> singleAddressInitializer;
 
     DefaultMultiAddressUrlHttpClientBuilder(
-            final DefaultSingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress> builderTemplate) {
-        this.builderTemplate = requireNonNull(builderTemplate);
+            final Function<HostAndPort, SingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress>> bFactory) {
+        this.builderFactory = requireNonNull(bFactory);
     }
 
     @Override
     public StreamingHttpClient buildStreaming() {
         final CompositeCloseable closeables = newCompositeCloseable();
         try {
-            final HttpClientBuildContext<HostAndPort, InetSocketAddress> buildContext = builderTemplate.copyBuildCtx();
-
-            final ClientFactory clientFactory = new ClientFactory(buildContext.builder, singleAddressInitializer);
-
-            final HttpExecutionContext executionContext = buildContext.builder.executionContextBuilder.build();
+            final HttpExecutionContext executionContext = executionContextBuilder.build();
+            final ClientFactory clientFactory = new ClientFactory(builderFactory, executionContext,
+                    singleAddressInitializer);
             final CachingKeyFactory keyFactory = closeables.prepend(new CachingKeyFactory());
             final HttpHeadersFactory headersFactory = this.headersFactory;
             FilterableStreamingHttpClient urlClient = closeables.prepend(
@@ -120,13 +120,8 @@ final class DefaultMultiAddressUrlHttpClientBuilder
             urlClient = redirectConfig == null ? urlClient :
                     new RedirectingHttpRequesterFilter(redirectConfig).create(urlClient);
 
-            HttpExecutionStrategy computedStrategy =
-                    buildContext.builder.computeChainStrategy(executionContext.executionStrategy());
-
-            LOGGER.debug("Client created with base strategy {} → computed strategy {}",
-                    executionContext.executionStrategy(), computedStrategy);
-
-            return new FilterableClientToClient(urlClient, computedStrategy);
+            LOGGER.debug("Multi-address client created with base strategy {}", executionContext.executionStrategy());
+            return new FilterableClientToClient(urlClient, executionContext.executionStrategy());
         } catch (final Throwable t) {
             closeables.closeAsync().subscribe();
             throw t;
@@ -220,32 +215,36 @@ final class DefaultMultiAddressUrlHttpClientBuilder
 
     private static final class ClientFactory implements Function<UrlKey, FilterableStreamingHttpClient> {
         private static final ClientSslConfig DEFAULT_CLIENT_SSL_CONFIG = new ClientSslConfigBuilder().build();
-        private final DefaultSingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress> builderTemplate;
+        private final Function<HostAndPort, SingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress>>
+                builderFactory;
+        private final HttpExecutionContext executionContext;
         @Nullable
         private final SingleAddressInitializer<HostAndPort, InetSocketAddress> singleAddressInitializer;
 
-        ClientFactory(
-                final DefaultSingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress> builderTemplate,
+        ClientFactory(final Function<HostAndPort, SingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress>>
+                        builderFactory,
+                final HttpExecutionContext executionContext,
                 @Nullable final SingleAddressInitializer<HostAndPort, InetSocketAddress> singleAddressInitializer) {
-            this.builderTemplate = builderTemplate;
+            this.builderFactory = builderFactory;
+            this.executionContext = executionContext;
             this.singleAddressInitializer = singleAddressInitializer;
         }
 
         @Override
         public StreamingHttpClient apply(final UrlKey urlKey) {
-            // Copy existing builder to prevent changes at runtime when concurrently creating clients for new addresses
-            final HttpClientBuildContext<HostAndPort, InetSocketAddress> buildContext =
-                    builderTemplate.copyBuildCtx(urlKey.hostAndPort);
+            final SingleAddressHttpClientBuilder<HostAndPort, InetSocketAddress> builder =
+                    requireNonNull(builderFactory.apply(urlKey.hostAndPort));
 
+            setExecutionContext(builder, executionContext);
             if (HTTPS_SCHEME.equalsIgnoreCase(urlKey.scheme)) {
-                buildContext.builder.sslConfig(DEFAULT_CLIENT_SSL_CONFIG);
+                builder.sslConfig(DEFAULT_CLIENT_SSL_CONFIG);
             }
 
             if (singleAddressInitializer != null) {
-                singleAddressInitializer.initialize(urlKey.scheme, urlKey.hostAndPort, buildContext.builder);
+                singleAddressInitializer.initialize(urlKey.scheme, urlKey.hostAndPort, builder);
             }
 
-            return buildContext.build();
+            return builder.buildStreaming();
         }
     }
 
@@ -330,27 +329,27 @@ final class DefaultMultiAddressUrlHttpClientBuilder
 
     @Override
     public MultiAddressHttpClientBuilder<HostAndPort, InetSocketAddress> ioExecutor(final IoExecutor ioExecutor) {
-        builderTemplate.ioExecutor(ioExecutor);
+        executionContextBuilder.ioExecutor(ioExecutor);
         return this;
     }
 
     @Override
     public MultiAddressHttpClientBuilder<HostAndPort, InetSocketAddress> executor(final Executor executor) {
-        builderTemplate.executor(executor);
+        executionContextBuilder.executor(executor);
         return this;
     }
 
     @Override
     public MultiAddressHttpClientBuilder<HostAndPort, InetSocketAddress> bufferAllocator(
             final BufferAllocator allocator) {
-        builderTemplate.bufferAllocator(allocator);
+        executionContextBuilder.bufferAllocator(allocator);
         return this;
     }
 
     @Override
     public MultiAddressHttpClientBuilder<HostAndPort, InetSocketAddress> executionStrategy(
             final HttpExecutionStrategy strategy) {
-        builderTemplate.executionStrategy(strategy);
+        executionContextBuilder.executionStrategy(strategy);
         return this;
     }
 
--- servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/DefaultMultiAddressUrlHttpClientBuilderTest.java
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/DefaultMultiAddressUrlHttpClientBuilderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018, 2021 Apple Inc. and the ServiceTalk project authors
+ * Copyright © 2018, 2021-2022 Apple Inc. and the ServiceTalk project authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,28 +15,21 @@
  */
 package io.servicetalk.http.netty;
 
-import io.servicetalk.buffer.api.BufferAllocator;
-import io.servicetalk.buffer.netty.BufferAllocators;
 import io.servicetalk.client.api.ServiceDiscoverer;
 import io.servicetalk.client.api.ServiceDiscovererEvent;
-import io.servicetalk.concurrent.api.Executor;
 import io.servicetalk.http.api.BlockingHttpClient;
 import io.servicetalk.http.api.BlockingHttpRequester;
 import io.servicetalk.http.api.BlockingStreamingHttpRequester;
 import io.servicetalk.http.api.DefaultHttpHeadersFactory;
 import io.servicetalk.http.api.HttpExecutionContext;
-import io.servicetalk.http.api.HttpExecutionStrategies;
-import io.servicetalk.http.api.HttpExecutionStrategy;
 import io.servicetalk.http.api.HttpHeaders;
 import io.servicetalk.http.api.HttpHeadersFactory;
 import io.servicetalk.http.api.HttpRequest;
 import io.servicetalk.http.api.HttpRequester;
-import io.servicetalk.http.api.StreamingHttpClient;
+import io.servicetalk.http.api.HttpResponse;
 import io.servicetalk.http.api.StreamingHttpClientFilter;
 import io.servicetalk.http.api.StreamingHttpRequester;
-import io.servicetalk.transport.api.ExecutionStrategy;
 import io.servicetalk.transport.api.HostAndPort;
-import io.servicetalk.transport.api.IoExecutor;
 import io.servicetalk.transport.api.ServerContext;
 import io.servicetalk.transport.netty.internal.ExecutionContextExtension;
 
@@ -47,15 +40,21 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static io.servicetalk.buffer.api.Matchers.contentEqualTo;
+import static io.servicetalk.buffer.netty.BufferAllocators.PREFER_DIRECT_ALLOCATOR;
+import static io.servicetalk.buffer.netty.BufferAllocators.PREFER_HEAP_ALLOCATOR;
 import static io.servicetalk.concurrent.api.Single.succeeded;
 import static io.servicetalk.http.api.HttpExecutionStrategies.defaultStrategy;
+import static io.servicetalk.http.api.HttpExecutionStrategies.offloadAll;
 import static io.servicetalk.http.api.HttpExecutionStrategies.offloadNever;
+import static io.servicetalk.http.api.HttpResponseStatus.OK;
 import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
+import static io.servicetalk.transport.netty.internal.AddressUtils.serverHostAndPort;
 import static io.servicetalk.transport.netty.internal.ExecutionContextExtension.cached;
 import static io.servicetalk.transport.netty.internal.ExecutionContextExtension.immediate;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -138,73 +137,96 @@ class DefaultMultiAddressUrlHttpClientBuilderTest {
     }
 
     @Test
-    void internalClientsUseDifferentExecutionContextWhenConfigured() throws Exception {
-        // Assert prerequisites first.
-        // Use different strategies, as ExecutionContextExtension shares the same strategy.
-        HttpExecutionStrategy internalExecutionStrategy = HttpExecutionStrategies.customStrategyBuilder()
-                .offloadNone().build();
-        HttpExecutionStrategy externalExecutionStrategy = HttpExecutionStrategies.customStrategyBuilder()
-                .offloadAll().build();
-        assertThat(internalExecutionStrategy, not(equalTo(externalExecutionStrategy)));
-
-        BufferAllocator internalBufferAllocator = BufferAllocators.PREFER_DIRECT_ALLOCATOR;
-        BufferAllocator externalBufferAllocator = BufferAllocators.PREFER_HEAP_ALLOCATOR;
-        assertThat(internalBufferAllocator, not(equalTo(externalBufferAllocator)));
-
-        assertThat(CTX.executor(), not(equalTo(INTERNAL_CLIENT_CTX.executor())));
-        assertThat(CTX.ioExecutor(), not(equalTo(INTERNAL_CLIENT_CTX.ioExecutor())));
+    void internalClientsInheritExecutionContext() throws Exception {
+        HttpExecutionContext expectedCtx = new HttpExecutionContextBuilder()
+                .ioExecutor(CTX.ioExecutor())
+                .executor(CTX.executor())
+                .bufferAllocator(PREFER_DIRECT_ALLOCATOR)
+                .executionStrategy(offloadNever())
+                .build();
+        AtomicReference<HttpExecutionContext> actualInternalCtx = new AtomicReference<>();
 
         try (ServerContext serverContext = HttpServers.forAddress(localAddress(0))
                 .executionStrategy(offloadNever())
-                .listenStreamingAndAwait((ctx, request, responseFactory) -> succeeded(responseFactory.ok()))) {
-            AtomicReference<BufferAllocator> actualInternalBufferAllocator = new AtomicReference<>();
-            AtomicReference<IoExecutor> actualInternalIoExecutor = new AtomicReference<>();
-            AtomicReference<Executor> actualInternalExecutor = new AtomicReference<>();
-            AtomicReference<ExecutionStrategy> actualInternalExecutionStrategy = new AtomicReference<>();
-
-            final StreamingHttpClient streamingHttpClient = HttpClients.forMultiAddressUrl()
-                    .initializer((scheme, address, builder) ->
-                            builder.executionStrategy(internalExecutionStrategy)
-                                    .executor(INTERNAL_CLIENT_CTX.executor())
-                                    .ioExecutor(INTERNAL_CLIENT_CTX.ioExecutor())
-                                    .bufferAllocator(internalBufferAllocator)
-                                    .executionStrategy(internalExecutionStrategy)
-                                    .appendClientFilter(client -> {
-                                        HttpExecutionContext internalContext = client.executionContext();
-                                        actualInternalBufferAllocator.set(internalContext.bufferAllocator());
-                                        actualInternalExecutor.set(internalContext.executor());
-                                        actualInternalIoExecutor.set(internalContext.ioExecutor());
-                                        actualInternalExecutionStrategy.set(internalContext.executionStrategy());
-                                        return new StreamingHttpClientFilter(client) {
-                                        };
-                                    }))
-                    .executor(CTX.executor())
-                    .ioExecutor(CTX.ioExecutor())
-                    .executionStrategy(externalExecutionStrategy)
-                    .bufferAllocator(externalBufferAllocator)
-                    .buildStreaming();
-
-            assertNotNull(streamingHttpClient);
+                .listenStreamingAndAwait((ctx, request, responseFactory) -> succeeded(responseFactory.ok()));
+             BlockingHttpClient blockingHttpClient = HttpClients.forMultiAddressUrl()
+                     .initializer((scheme, address, builder) ->
+                             builder.appendClientFilter(client -> {
+                                 actualInternalCtx.set(client.executionContext());
+                                 return new StreamingHttpClientFilter(client) {
+                                 };
+                             }))
+                     .ioExecutor(expectedCtx.ioExecutor())
+                     .executor(expectedCtx.executor())
+                     .bufferAllocator(expectedCtx.bufferAllocator())
+                     .executionStrategy(expectedCtx.executionStrategy())
+                     .buildBlocking()) {
 
             // Check external client
-            final HttpExecutionContext executionContext = streamingHttpClient.executionContext();
-            assertThat(executionContext.executor(), equalTo(CTX.executor()));
-            assertThat(executionContext.executionStrategy(), equalTo(externalExecutionStrategy));
-            assertThat(executionContext.ioExecutor(), equalTo(CTX.ioExecutor()));
-            assertThat(executionContext.bufferAllocator(), equalTo(CTX.bufferAllocator()));
-
+            assertExecutionContext(expectedCtx, blockingHttpClient.executionContext());
             // Make a request to trigger the filter execution that extracts the execution context.
-            final HostAndPort address = HostAndPort.of((InetSocketAddress) serverContext.listenAddress());
-            streamingHttpClient.reserveConnection(streamingHttpClient.get("http://" + address)).toFuture().get();
-
+            HttpResponse response = blockingHttpClient.request(
+                    blockingHttpClient.get("http://" + serverHostAndPort(serverContext)));
+            assertThat(response.status(), is(OK));
             // Check internal client
-            assertThat(actualInternalBufferAllocator.get(), equalTo(internalBufferAllocator));
-            assertThat(actualInternalExecutor.get(), equalTo(INTERNAL_CLIENT_CTX.executor()));
-            assertThat(actualInternalIoExecutor.get(), equalTo(INTERNAL_CLIENT_CTX.ioExecutor()));
-            assertThat(actualInternalExecutionStrategy.get(), equalTo(internalExecutionStrategy));
-
-            streamingHttpClient.closeAsync().toFuture().get();
+            assertExecutionContext(expectedCtx, actualInternalCtx.get());
         }
+    }
+
+    @Test
+    void internalClientsUseDifferentExecutionContextWhenConfigured() throws Exception {
+        HttpExecutionContext externalCtx = new HttpExecutionContextBuilder()
+                .ioExecutor(CTX.ioExecutor())
+                .executor(CTX.executor())
+                .bufferAllocator(PREFER_HEAP_ALLOCATOR)
+                .executionStrategy(offloadAll())
+                .build();
+
+        HttpExecutionContext internalCtx = new HttpExecutionContextBuilder()
+                .ioExecutor(INTERNAL_CLIENT_CTX.ioExecutor())
+                .executor(INTERNAL_CLIENT_CTX.executor())
+                .bufferAllocator(PREFER_DIRECT_ALLOCATOR)
+                .executionStrategy(offloadNever())
+                .build();
+
+        AtomicReference<HttpExecutionContext> actualInternalCtx = new AtomicReference<>();
+        try (ServerContext serverContext = HttpServers.forAddress(localAddress(0))
+                .executionStrategy(offloadNever())
+                .listenStreamingAndAwait((ctx, request, responseFactory) -> succeeded(responseFactory.ok()));
+             BlockingHttpClient blockingHttpClient = HttpClients.forMultiAddressUrl()
+                     .initializer((scheme, address, builder) ->
+                             builder.executionStrategy(internalCtx.executionStrategy())
+                                     .executor(internalCtx.executor())
+                                     .ioExecutor(internalCtx.ioExecutor())
+                                     .bufferAllocator(internalCtx.bufferAllocator())
+                                     .appendClientFilter(client -> {
+                                         actualInternalCtx.set(client.executionContext());
+                                         return new StreamingHttpClientFilter(client) {
+                                         };
+                                     }))
+                     .executor(externalCtx.executor())
+                     .ioExecutor(externalCtx.ioExecutor())
+                     .executionStrategy(externalCtx.executionStrategy())
+                     .bufferAllocator(externalCtx.bufferAllocator())
+                     .buildBlocking()) {
+
+            // Check external client
+            assertExecutionContext(externalCtx, blockingHttpClient.executionContext());
+            // Make a request to trigger the filter execution that extracts the execution context.
+            HttpResponse response = blockingHttpClient.request(
+                    blockingHttpClient.get("http://" + serverHostAndPort(serverContext)));
+            assertThat(response.status(), is(OK));
+            // Check internal client
+            assertExecutionContext(internalCtx, actualInternalCtx.get());
+        }
+    }
+
+    private static void assertExecutionContext(HttpExecutionContext expected, HttpExecutionContext actual) {
+        assertThat(actual, is(notNullValue()));
+        assertThat(actual.ioExecutor(), is(sameInstance(expected.ioExecutor())));
+        assertThat(actual.executor(), is(sameInstance(expected.executor())));
+        assertThat(actual.bufferAllocator(), is(sameInstance(expected.bufferAllocator())));
+        assertThat(actual.executionStrategy(), is(sameInstance(expected.executionStrategy())));
     }
 
     private static class DelegatingHttpHeadersFactory implements HttpHeadersFactory {
