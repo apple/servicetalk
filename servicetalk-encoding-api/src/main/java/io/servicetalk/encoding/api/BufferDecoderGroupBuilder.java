f9e25298cd9ce19914643c83536a88410a7bfed7
Scott Mitchell <scott_mitchell@apple.com>
2022-06-17 11:43:23 -0700

    update javadocs

--- servicetalk-encoding-api/src/main/java/io/servicetalk/encoding/api/BufferDecoderGroupBuilder.java
+++ servicetalk-encoding-api/src/main/java/io/servicetalk/encoding/api/BufferDecoderGroupBuilder.java
@@ -59,12 +59,16 @@ public final class BufferDecoderGroupBuilder {
     /**
      * Add a new {@link BufferDecoder} to the {@link BufferDecoderGroup} built by this builder.
      * @param decoder The decoder to add.
-     * @param advertised {@code true} if the decoder should be included in
-     * {@link BufferDecoderGroup#advertisedMessageEncoding()}. {@code false} means the value will be excluded from
-     * {@link BufferDecoderGroup#advertisedMessageEncoding()} and therefore won't be included in {@code Accept-Encoding}
-     * (or equivalent) metadata headers and the peer won't explicitly be told this decoder is supported. {@code false}
-     * is commonly used to discourage usage of "identity" decoders in favor of other more preferred options, but still
-     * support it in case there are no common decoders.
+     * @param advertised
+     * <ul>
+     *     <li>{@code true} - the decoder should be included in
+     *     {@link BufferDecoderGroup#advertisedMessageEncoding()}</li>
+     *     <li>{@code false} -the decoder is excluded from
+     *     {@link BufferDecoderGroup#advertisedMessageEncoding()} and therefore won't be included in
+     *     {@code Accept-Encoding} (or equivalent) metadata headers. In this case the peer won't be explicitly be told
+     *     this decoder is supported. Commonly used to discourage usage of {@code identity} decoders in favor of other
+     *     more preferred options, but still support it as a fallback if there are no common decoders.</li>
+     * </ul>
      * @return {@code this}.
      */
     public BufferDecoderGroupBuilder add(BufferDecoder decoder, boolean advertised) {
