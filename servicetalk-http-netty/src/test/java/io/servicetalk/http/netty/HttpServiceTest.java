0df96421b0c77f0209823c34f908950054a1d12a
Michael Nitschinger <mnitschinger@apple.com>
2022-12-07 15:53:34 +0100

    Add test case

---
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/HttpServiceTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright © 2022 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.concurrent.api.Single;
+import io.servicetalk.http.api.BlockingHttpService;
+import io.servicetalk.http.api.BlockingStreamingHttpService;
+import io.servicetalk.http.api.HttpExecutionStrategy;
+import io.servicetalk.http.api.HttpServerContext;
+import io.servicetalk.http.api.HttpService;
+import io.servicetalk.http.api.Service;
+import io.servicetalk.http.api.StreamingHttpService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static io.servicetalk.transport.netty.internal.AddressUtils.localAddress;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class HttpServiceTest {
+
+    @ParameterizedTest
+    @MethodSource("serviceProvider")
+    void supportsHttpServiceVariantAtRuntime(Service service) throws Exception {
+        assertNotNull(HttpServers.forAddress(localAddress(0)).listenServiceAndAwait(service));
+    }
+
+    @Test
+    void failsOnUnknownService() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Service service = new Service() {
+                @Override
+                public HttpExecutionStrategy requiredOffloads() {
+                    return Service.super.requiredOffloads();
+                }
+            };
+
+            try (HttpServerContext ctx = HttpServers.forAddress(localAddress(0)).listenServiceAndAwait(service)) {
+                ctx.closeGracefully();
+            }
+        });
+    }
+
+    static Stream<Arguments> serviceProvider() {
+        return Stream.of(
+                arguments(named("BlockingHttpService",
+                        (BlockingHttpService) (ctx, request, responseFactory) -> responseFactory.ok())),
+                arguments(named("BlockingStreamingHttpService",
+                        (BlockingStreamingHttpService) (ctx, request, response) -> { })),
+                arguments(named("HttpService",
+                        (HttpService) (ctx, request, responseFactory) ->
+                                Single.succeeded(responseFactory.ok()))),
+                arguments(named("StreamingHttpService",
+                        (StreamingHttpService) (ctx, request, responseFactory) ->
+                                Single.succeeded(responseFactory.ok())))
+                );
+    }
+}
