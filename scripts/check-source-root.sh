6dc58ec6fee70943d7dbbc44b93e34b744f29a68
Bryce Anderson <bryce.anderson22@gmail.com>
2023-01-23 11:14:04 -0700

    Fix linter errors in script

--- scripts/check-source-root.sh
+++ scripts/check-source-root.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# Copyright © 2023 Apple Inc. and the ServiceTalk project authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 set -eu
 
@@ -62,7 +78,6 @@ function process_all() {
     do
         eval_file $DOCFILE
     done
-
 }
 
 while getopts ":rh" arg; do
@@ -72,7 +87,7 @@ while getopts ":rh" arg; do
            usage
            exit 1
            ;;
-        ?) 
+        ?)
            echo "Invalid options: ${OPTARG}."
            exit_abnormal
            ;;
@@ -92,4 +107,3 @@ then
     echo "Found errors. Exiting."
     exit 1
 fi
-
