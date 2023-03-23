0e10cdad34f5ca6a47b438e335b92250da6ec0d3
Bryce Anderson <bryce_anderson@apple.com>
2023-03-23 14:08:44 -0600

    Testing feedback

--- servicetalk-concurrent-api/src/test/java/io/servicetalk/concurrent/api/publisher/SkipWhileTest.java
+++ servicetalk-concurrent-api/src/test/java/io/servicetalk/concurrent/api/publisher/SkipWhileTest.java
@@ -20,12 +20,12 @@ import io.servicetalk.concurrent.api.TestPublisher;
 import io.servicetalk.concurrent.api.TestSubscription;
 import io.servicetalk.concurrent.test.internal.TestPublisherSubscriber;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static io.servicetalk.concurrent.api.SourceAdapters.toSource;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -35,37 +35,23 @@ class SkipWhileTest {
     private final TestPublisherSubscriber<String> subscriber = new TestPublisherSubscriber<>();
     private final TestSubscription subscription = new TestSubscription();
 
-    @Test
-    void skipWhile() {
-        Publisher<String> p = publisher.skipWhile(s -> !s.equals("Hello2"));
-        toSource(p).subscribe(subscriber);
-        publisher.onSubscribe(subscription);
-        subscriber.awaitSubscription().request(4);
-        publisher.onNext("Hello1", "Hello2", "Hello3");
-        assertThat(subscriber.takeOnNext(2), contains("Hello2", "Hello3"));
-        assertFalse(subscription.isCancelled());
-    }
-
-    @Test
-    void skipWhileComplete() {
-        Publisher<String> p = publisher.skipWhile(s -> !s.equals("Hello2"));
-        toSource(p).subscribe(subscriber);
-        publisher.onSubscribe(subscription);
-        subscriber.awaitSubscription().request(4);
-        publisher.onNext("Hello1", "Hello2");
-        publisher.onComplete();
-        assertEquals("Hello2", subscriber.takeOnNext());
-    }
-
-    @Test
-    void skipWhileCancelled() {
+    @ParameterizedTest(name = "doComplete={0} doCancel={1}")
+    @CsvSource(value = {"false,false", "false,true", "true,false"})
+    void skipWhile(boolean doComplete, boolean doCancel) {
         Publisher<String> p = publisher.skipWhile(s -> !s.equals("Hello2"));
         toSource(p).subscribe(subscriber);
         publisher.onSubscribe(subscription);
         subscriber.awaitSubscription().request(3);
-        publisher.onNext("Hello1", "Hello2");
-        assertEquals("Hello2", subscriber.takeOnNext());
-        subscriber.awaitSubscription().cancel();
-        assertTrue(subscription.isCancelled());
+        publisher.onNext("Hello1", "Hello2", "Hello3");
+        assertThat(subscriber.takeOnNext(2), contains("Hello2", "Hello3"));
+        if (doComplete) {
+            publisher.onComplete();
+            assertFalse(subscription.isCancelled());
+            subscriber.awaitOnComplete();
+        }
+        if (doCancel) {
+            subscriber.awaitSubscription().cancel();
+            assertTrue(subscription.isCancelled());
+        }
     }
 }
