7874f542ff86f0861ec7fc9465c3d835ae0a7eba
Tomas Fernandez Lobbe <tflobbe@apple.com>
2023-11-08 17:03:28 -0800

    Fix bug in http/grpc logging observer that prevented request Throwables to be logged
    Motivation:
    
    LoggingHttpLifecycleObserver and LoggingGrpcLifecycleObserver included a bug that prevented request exceptions to be logged.
    
    Modifications:
    
    - Update LoggingHttpLifecycleObserver and LoggingGrpcLifecycleObserver to include the Throwable as the last argument in the logger call
    - Add tests for the two components
    
    Result:
    
    The Logger object will be called with a Throwable as last argument instead of null in case of request errors. Most logging frameworks implementing slf4j will print the full stacktraces in this situation.

---
+++ servicetalk-grpc-utils/src/test/java/io/servicetalk/grpc/utils/LoggingGrpcLifecycleObserverTest.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright © 2022 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.grpc.utils;
+
+import io.servicetalk.grpc.api.GrpcLifecycleObserver;
+import io.servicetalk.http.api.HttpHeaders;
+import io.servicetalk.http.api.HttpProtocolVersion;
+import io.servicetalk.http.api.HttpRequestMetaData;
+import io.servicetalk.http.api.HttpRequestMethod;
+import io.servicetalk.http.api.HttpResponseMetaData;
+import io.servicetalk.http.api.HttpResponseStatus;
+import io.servicetalk.logging.api.LogLevel;
+import io.servicetalk.logging.slf4j.internal.FixedLevelLogger;
+import io.servicetalk.transport.api.ConnectionInfo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class LoggingGrpcLifecycleObserverTest {
+
+    private HttpRequestMetaData mockRequestMetadata;
+    private HttpResponseMetaData mockResponseMetadata;
+    private FixedLevelLogger mockLogger;
+    private GrpcLifecycleObserver.GrpcExchangeObserver observer;
+
+    @BeforeEach
+    public void setUp() {
+        mockRequestMetadata = mock(HttpRequestMetaData.class);
+        when(mockRequestMetadata.headers()).thenReturn(mock(HttpHeaders.class));
+        when(mockRequestMetadata.method()).thenReturn(HttpRequestMethod.GET);
+        when(mockRequestMetadata.requestTarget()).thenReturn("/foo/bar");
+        when(mockRequestMetadata.version()).thenReturn(HttpProtocolVersion.HTTP_2_0);
+
+        mockResponseMetadata = mock(HttpResponseMetaData.class);
+        when(mockResponseMetadata.status()).thenReturn(HttpResponseStatus.OK);
+        when(mockResponseMetadata.headers()).thenReturn(mock(HttpHeaders.class));
+
+        mockLogger = mock(FixedLevelLogger.class);
+        when(mockLogger.logLevel()).thenReturn(LogLevel.INFO);
+
+        observer = new LoggingGrpcLifecycleObserver(mockLogger).onNewExchange();
+    }
+
+    @Test
+    void testFactoryMethod() {
+        assertThat(observer, isA(GrpcLifecycleObserver.GrpcRequestObserver.class));
+        assertThat(observer, isA(GrpcLifecycleObserver.GrpcResponseObserver.class));
+    }
+
+    @Test
+    void testOnRequestError() {
+        observer.onConnectionSelected(mock(ConnectionInfo.class));
+        Throwable requestError = new RuntimeException("fake request error");
+        observer.onRequest(mockRequestMetadata);
+        ((GrpcLifecycleObserver.GrpcRequestObserver) observer).onRequestError(requestError);
+        observer.onResponseCancel();
+        observer.onExchangeFinally();
+        verify(mockLogger).log(anyString(),
+                any(), // conn info
+                eq(HttpRequestMethod.GET), eq("/foo/bar"), eq(HttpProtocolVersion.HTTP_2_0), eq(0),
+                eq(0L), // request size
+                eq(0), // requestTrailersCount
+                any(), // unwrappedRequestResult
+                any(), // unwrappedResponseResult
+                anyLong(), // responseTimeMs
+                anyLong(), // duration
+                eq(requestError));
+    }
+
+    @Test
+    void testOnResponseError() {
+        observer.onConnectionSelected(mock(ConnectionInfo.class));
+        observer.onRequest(mockRequestMetadata);
+        Throwable responseError = new RuntimeException("fake response error");
+        observer.onResponseError(responseError);
+        observer.onExchangeFinally();
+        verify(mockLogger).log(anyString(),
+                any(), // conn info
+                eq(HttpRequestMethod.GET), eq("/foo/bar"), eq(HttpProtocolVersion.HTTP_2_0), eq(0),
+                eq(0L), // request size
+                eq(0), // requestTrailersCount
+                any(), // unwrappedRequestResult
+                any(), // unwrappedResponseResult
+                anyLong(), // responseTimeMs
+                anyLong(), // duration
+                eq(responseError));
+    }
+
+    @Test
+    void testCombinedError() {
+        observer.onConnectionSelected(mock(ConnectionInfo.class));
+        Throwable requestError = new RuntimeException("fake request error");
+        Throwable responseError = new RuntimeException("fake response error");
+        observer.onRequest(mockRequestMetadata);
+        ((GrpcLifecycleObserver.GrpcRequestObserver) observer).onRequestError(requestError);
+        observer.onResponseError(responseError);
+        observer.onExchangeFinally();
+        verify(mockLogger).log(anyString(),
+                any(), // conn info
+                eq(HttpRequestMethod.GET), eq("/foo/bar"), eq(HttpProtocolVersion.HTTP_2_0), eq(0),
+                eq(0L), // request size
+                any(), // requestTrailersCount
+                any(), // unwrappedRequestResult
+                any(), // unwrappedResponseResult
+                anyLong(), // responseTimeMs
+                anyLong(), // duration
+                eq(responseError));
+        assertThat(responseError.getSuppressed()[0], is(requestError));
+    }
+
+    @Test
+    void testOnResponseErrorWithMetadata() {
+        observer.onConnectionSelected(mock(ConnectionInfo.class));
+        observer.onRequest(mockRequestMetadata);
+        Throwable responseError = new RuntimeException("fake response error");
+        observer.onResponse(mockResponseMetadata);
+        observer.onResponseError(responseError);
+        observer.onExchangeFinally();
+        verify(mockLogger).log(anyString(),
+                any(), // conn info
+                eq(HttpRequestMethod.GET), eq("/foo/bar"), eq(HttpProtocolVersion.HTTP_2_0), eq(0),
+                eq(0L), // request size
+                eq(0), // requestTrailersCount
+                any(), // unwrappedRequestResult
+                eq(HttpResponseStatus.OK.code()), // responseCode
+                eq(0), // response Header Size
+                eq(0L), // responseSize
+                eq(0), // responseTrailersCount
+                any(), // grpcStatus
+                any(), // unwrappedResponseResult
+                anyLong(), // responseTimeMs
+                anyLong(), // duration
+                eq(responseError));
+    }
+}
