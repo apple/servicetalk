0f03d747583f97bbb224c00641f48d08b5836400
Bryce Anderson <bryce_anderson@apple.com>
2024-02-13 09:06:48 -0700

    loadbalancer-experimental: Fix some issues with the xds health checker (#2831)
    Motivation:
    
    There are a few minor bugs to address and we could use a few more
    debug log messages for the future to help debug.
    
    Modifications:
    
    - Fix the enforcing percentage calculation so that 0% enforcement
      means it never happens.
    - Reorder initialization of the `healthChecker` field in
      DefaultLoadBalancer so if we subscribe to an eager service
      discoverer we already have the field initialized.
    - Adjust log messages.

--- servicetalk-loadbalancer-experimental/src/main/java/io/servicetalk/loadbalancer/DefaultLoadBalancer.java
+++ servicetalk-loadbalancer-experimental/src/main/java/io/servicetalk/loadbalancer/DefaultLoadBalancer.java
@@ -149,8 +149,10 @@ final class DefaultLoadBalancer<ResolvedAddress, C extends LoadBalancedConnectio
         this.asyncCloseable = toAsyncCloseable(this::doClose);
         // Maintain a Subscriber so signals are always delivered to replay and new Subscribers get the latest signal.
         eventStream.ignoreElements().subscribe();
-        subscribeToEvents(false);
         this.healthChecker = healthCheckerFactory == null ? null : healthCheckerFactory.apply(lbDescription);
+        // We subscribe to events as the very last step so that if we subscribe to an eager service discoverer
+        // we already have all the fields initialized.
+        subscribeToEvents(false);
     }
 
     private void subscribeToEvents(boolean resubscribe) {
