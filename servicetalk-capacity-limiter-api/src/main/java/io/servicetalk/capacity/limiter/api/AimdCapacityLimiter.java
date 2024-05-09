b10c48464e01a151b7dd184a431f5267e5d5e017
Thomas Kountis <thomas_kountis@apple.com>
2024-05-09 11:13:17 -0700

    line

--- servicetalk-capacity-limiter-api/src/main/java/io/servicetalk/capacity/limiter/api/AimdCapacityLimiter.java
+++ servicetalk-capacity-limiter-api/src/main/java/io/servicetalk/capacity/limiter/api/AimdCapacityLimiter.java
@@ -28,7 +28,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.servicetalk.capacity.limiter.api;
 
 import io.servicetalk.capacity.limiter.api.AimdCapacityLimiterBuilder.StateObserver;
