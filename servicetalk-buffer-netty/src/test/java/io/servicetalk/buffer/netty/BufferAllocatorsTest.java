139640f69ee8aee191640b42c7b28ca55c1f42d5
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2024-06-11 21:08:51 -0700

    Reduce allocations for `ServiceTalkBufferAllocator.wrap(byte[], int, int)` (#2951)
    Motivation:
    
    Default impl in `BufferAllocator` takes a slice of a wrapped original byte-array. We can avoid double wrapping with `NettyBuffer` if we implement this method in `ServiceTalkBufferAllocator`.
    
    Modifications:
    
    - Override `BufferAllocator.wrap(byte[], int, int)` to wrap with `NettyBuffer` only one time;
    - Enhance `BufferAllocatorsTest` to validate wrapping and ensure the wrapped `Buffer` always has readerIndex set to 0;
    
    Result:
    
    Minus 2 object allocations when users wrap a byte-array partially.

--- servicetalk-buffer-netty/src/test/java/io/servicetalk/buffer/netty/BufferAllocatorsTest.java
+++ servicetalk-buffer-netty/src/test/java/io/servicetalk/buffer/netty/BufferAllocatorsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018, 2021 Apple Inc. and the ServiceTalk project authors
+ * Copyright © 2018-2021, 2024 Apple Inc. and the ServiceTalk project authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,30 +20,39 @@ import io.servicetalk.buffer.api.BufferAllocator;
 
 import io.netty.buffer.ByteBuf;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
-import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static io.servicetalk.buffer.netty.BufferAllocators.DEFAULT_ALLOCATOR;
 import static io.servicetalk.buffer.netty.BufferAllocators.PREFER_DIRECT_ALLOCATOR;
 import static io.servicetalk.buffer.netty.BufferAllocators.PREFER_HEAP_ALLOCATOR;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BufferAllocatorsTest {
 
     private static final String TEST_NAME_FORMAT = "{index}: allocator = {0}";
 
     @SuppressWarnings("unused")
-    private static Collection<BufferAllocator> allocators() {
-        return asList(DEFAULT_ALLOCATOR, PREFER_DIRECT_ALLOCATOR, PREFER_HEAP_ALLOCATOR);
+    private static List<Arguments> allocators() {
+        return asList(arguments(named("DEFAULT_ALLOCATOR", DEFAULT_ALLOCATOR)),
+                arguments(named("PREFER_DIRECT_ALLOCATOR", PREFER_DIRECT_ALLOCATOR)),
+                arguments(named("PREFER_HEAP_ALLOCATOR", PREFER_HEAP_ALLOCATOR)));
     }
 
     @ParameterizedTest(name = TEST_NAME_FORMAT)
@@ -73,7 +82,81 @@ class BufferAllocatorsTest {
     @ParameterizedTest(name = TEST_NAME_FORMAT)
     @MethodSource("allocators")
     void testReadOnlyDirectBuffer(BufferAllocator allocator) {
-        assertBuffer(allocator.wrap(ByteBuffer.allocateDirect(16).asReadOnlyBuffer()), true);
+        Buffer wrapped = allocator.wrap(ByteBuffer.allocateDirect(16).asReadOnlyBuffer());
+        assertBuffer(wrapped, true);
+        assertThat(wrapped.isReadOnly(), is(true));
+    }
+
+    @ParameterizedTest(name = TEST_NAME_FORMAT)
+    @MethodSource("allocators")
+    void testWrapDirectByteBufferThenMakeItReadOnly(BufferAllocator allocator) {
+        Buffer wrapped = allocator.wrap(ByteBuffer.allocateDirect(16)).asReadOnly();
+        assertBuffer(wrapped, true);
+        assertThat(wrapped.isReadOnly(), is(true));
+    }
+
+    @ParameterizedTest(name = TEST_NAME_FORMAT)
+    @MethodSource("allocators")
+    void testReadOnlyHeapBuffer(BufferAllocator allocator) {
+        Buffer wrapped = allocator.wrap(ByteBuffer.allocate(16).asReadOnlyBuffer());
+        assertBuffer(wrapped, false);
+        assertThat(wrapped.isReadOnly(), is(true));
+    }
+
+    @ParameterizedTest(name = TEST_NAME_FORMAT)
+    @MethodSource("allocators")
+    void testWrapHeapByteBufferThenMakeItReadOnly(BufferAllocator allocator) {
+        Buffer wrapped = allocator.wrap(ByteBuffer.allocate(16)).asReadOnly();
+        assertBuffer(wrapped, false);
+        assertThat(wrapped.isReadOnly(), is(true));
+    }
+
+    @ParameterizedTest(name = TEST_NAME_FORMAT)
+    @MethodSource("allocators")
+    void testWrapByteArray(BufferAllocator allocator) {
+        byte[] array = new byte[16];
+        ThreadLocalRandom.current().nextBytes(array);
+        Buffer wrapped = allocator.wrap(array);
+        assertBuffer(wrapped, false);
+        assertThat(wrapped.isReadOnly(), is(false));
+        assertThat(wrapped.readableBytes(), is(equalTo(array.length)));
+        byte[] dst = new byte[array.length];
+        wrapped.readBytes(dst);
+        assertThat(wrapped.readableBytes(), is(0));
+        assertThat(dst, is(equalTo(array)));
+    }
+
+    @ParameterizedTest(name = TEST_NAME_FORMAT)
+    @MethodSource("allocators")
+    void testWrapPartialByteArray(BufferAllocator allocator) {
+        byte[] array = new byte[16];
+        ThreadLocalRandom.current().nextBytes(array);
+        int newLength = array.length - 4;
+        Buffer wrapped = allocator.wrap(array, 2, newLength);
+        assertBuffer(wrapped, false);
+        assertThat(wrapped.isReadOnly(), is(false));
+        assertThat(wrapped.readableBytes(), is(equalTo(newLength)));
+        assertThat(wrapped.readerIndex(), is(0));
+        assertThat(wrapped.writerIndex(), is(equalTo(newLength)));
+        byte[] dst = new byte[array.length];
+        wrapped.readBytes(dst, 0, newLength);
+        assertThat(wrapped.readableBytes(), is(0));
+        for (int i = 0, j = 2; i < newLength; i++, j++) {
+            assertThat("Mismatch at index: " + i, dst[i], is(equalTo(array[j])));
+        }
+        for (int i = newLength; i < array.length; i++) {
+            assertThat("Mismatch at index: " + i, dst[i], is((byte) 0));
+        }
+    }
+
+    @ParameterizedTest(name = TEST_NAME_FORMAT)
+    @MethodSource("allocators")
+    void testWrapPartialByteArrayWithIllegalArguments(BufferAllocator allocator) {
+        byte[] array = new byte[16];
+        assertThrows(IndexOutOfBoundsException.class, () -> allocator.wrap(array, -1, array.length));
+        assertThrows(IndexOutOfBoundsException.class, () -> allocator.wrap(array, array.length + 1, 1));
+        assertThrows(IndexOutOfBoundsException.class, () -> allocator.wrap(array, 2, array.length + 1));
+        assertThrows(IllegalArgumentException.class, () -> allocator.wrap(array, 2, -1));
     }
 
     @ParameterizedTest(name = TEST_NAME_FORMAT)
@@ -161,9 +244,9 @@ class BufferAllocatorsTest {
     }
 
     private static void assertBuffer(Buffer buffer, boolean direct) {
-        if (direct) {
+        if (direct || buffer.isReadOnly()) {
             assertFalse(buffer.hasArray());
-            assertTrue(buffer.isDirect());
+            assertThat(buffer.isDirect(), is(direct));
         } else {
             assertTrue(buffer.hasArray());
             assertFalse(buffer.isDirect());
