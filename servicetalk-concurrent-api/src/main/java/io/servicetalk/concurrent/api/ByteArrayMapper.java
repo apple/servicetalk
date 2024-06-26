d787686db89c56a687a7624851b62ca396c9bac6
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2024-06-26 14:07:26 -0700

    Clarify map javadoc

--- servicetalk-concurrent-api/src/main/java/io/servicetalk/concurrent/api/ByteArrayMapper.java
+++ servicetalk-concurrent-api/src/main/java/io/servicetalk/concurrent/api/ByteArrayMapper.java
@@ -31,9 +31,9 @@ public interface ByteArrayMapper<T> {
     /**
      * Maps a specified {@code byte[]} buffer region into a {@code T}.
      * <p>
-     * The mapper can operate only with the specified region of the {@code buffer}. Access to other parts of the buffer
-     * may lead to unexpected results. The specified region can be safely used by the mapper without a need to copy
-     * data.
+     * The mapper can operate only with the specified region of the {@code buffer}, which can be safely used without a
+     * need to copy data. Access to other parts of the buffer may lead to unexpected results and due care should be
+     * taken to avoid leaking that data through the returned type {@code T}.
      *
      * @param buffer {@code byte[]} buffer with data
      * @param offset the offset of the region
