f31147ca90e8a05f3ffd36ad93d4e12a39946e1f
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2025-03-28 16:19:12 -0700

    Fix `AsyncContext` visibility for OTEL filters
    Motivation:
    
    `AbstractOpenTelemetryFilter` manually subscribes to response and its
    payload body but does not share context on subscribe. In result,
    `AsyncContext` propagation is incorrect when OTEL filters are in place.
    
    Modifications:
    
    - Add `shareContextOnSubscribe()` before subscribing response single and
    its payload Publisher.
    - Add a test that uses our utility to verify correct AsyncContext
    propagation.
    
    Result:
    
    `AsyncContext` state is correctly propagated through stages of
    response processing when OTEL filters are in place.

--- servicetalk-opentelemetry-http/src/test/java/io/servicetalk/opentelemetry/http/OpenTelemetryHttpServerFilterTest.java
+++ servicetalk-opentelemetry-http/src/test/java/io/servicetalk/opentelemetry/http/OpenTelemetryHttpServerFilterTest.java
@@ -45,6 +45,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import static io.servicetalk.concurrent.api.Single.succeeded;
+import static io.servicetalk.http.netty.AsyncContextHttpFilterVerifier.verifyServerFilterAsyncContextVisibility;
 import static io.servicetalk.http.netty.HttpClients.forSingleAddress;
 import static io.servicetalk.opentelemetry.http.OpenTelemetryHttpRequestFilterTest.verifyTraceIdPresentInLogs;
 import static io.servicetalk.opentelemetry.http.TestUtils.SPAN_STATE_SERIALIZER;
@@ -233,6 +234,11 @@ class OpenTelemetryHttpServerFilterTest {
                     });
             }
         }
+    }
+
+    @Test
+    void verifyAsyncContextVisibility() throws Exception {
+        verifyServerFilterAsyncContextVisibility(new OpenTelemetryHttpServerFilter());
     }
 
     private static ServerContext buildServer(OpenTelemetry givenOpentelemetry,
