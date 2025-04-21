apple/servicetalk

commit aad031ef1e10a68b0de196611bd89539c7e094ca

http-utils: add AfterFinallyHttpOperator (#3231)
Motivation

In some situations we need to have after-finally type behavior
for listening to the response complete events. This includes things
like OpenTelemetry where we want to close the span after all
other listeners have completed.

Modifications

- Generalize the BeforeFinallyOperator so that it displays before
  and after finally based behavior based on a flag.
- Make BeforeFinallyOperator and AfterFinallyOperators based
  on the base class.

servicetalk-http-utils/src/main/java/io/servicetalk/http/utils/AfterFinallyHttpOperator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019-2021 Apple Inc. and the ServiceTalk project authors
+ * Copyright © 2025 Apple Inc. and the ServiceTalk project authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,48 +43,48 @@ import io.servicetalk.http.api.StreamingHttpResponse;
  *     // coarse grained, any terminal signal calls the provided `Runnable`
  *     return requester.request(strategy, request)
  *                     .beforeOnSubscribe(__ -> tracker.requestStarted())
- *                     .liftSync(new BeforeFinallyHttpOperator(tracker::requestFinished));
+ *                     .liftSync(new AfterFinallyHttpOperator(tracker::requestFinished));
  *
  *     // fine grained, `tracker` implements `TerminalSignalConsumer`, terminal signal indicated by the callback method
  *     return requester.request(strategy, request)
  *                     .beforeOnSubscribe(__ -> tracker.requestStarted())
- *                     .liftSync(new BeforeFinallyHttpOperator(tracker));
+ *                     .liftSync(new AfterFinallyHttpOperator(tracker));
  * }</pre>
  *
- * @see AfterFinallyHttpOperator
+ * @see BeforeFinallyHttpOperator
  */
-public final class BeforeFinallyHttpOperator extends WhenFinallyHttpOperator {
+public final class AfterFinallyHttpOperator extends WhenFinallyHttpOperator {
 
     /**
      * Create a new instance.
      *
-     * @param beforeFinally the callback which is executed just once whenever the sources reach a terminal state
+     * @param afterFinally the callback which is executed just once whenever the sources reach a terminal state
      * across both sources.
      */
-    public BeforeFinallyHttpOperator(final TerminalSignalConsumer beforeFinally) {
-        this(beforeFinally, false);
+    public AfterFinallyHttpOperator(final Runnable afterFinally) {
+        this(TerminalSignalConsumer.from(afterFinally));
     }
 
     /**
      * Create a new instance.
      *
-     * @param beforeFinally the callback which is executed just once whenever the sources reach a terminal state
+     * @param afterFinally the callback which is executed just once whenever the sources reach a terminal state
      * across both sources.
      */
-    public BeforeFinallyHttpOperator(final Runnable beforeFinally) {
-        this(TerminalSignalConsumer.from(beforeFinally));
+    public AfterFinallyHttpOperator(final TerminalSignalConsumer afterFinally) {
+        this(afterFinally, false);
     }
 
     /**
      * Create a new instance.
      *
-     * @param beforeFinally the callback which is executed just once whenever the sources reach a terminal state
+     * @param afterFinally the callback which is executed just once whenever the sources reach a terminal state
      * across both sources.
      * @param discardEventsAfterCancel if {@code true} further events will be discarded if those arrive after
      * {@link TerminalSignalConsumer#cancel()} is invoked. Otherwise, events may still be delivered if they race with
      * cancellation.
      */
-    public BeforeFinallyHttpOperator(final TerminalSignalConsumer beforeFinally, boolean discardEventsAfterCancel) {
-        super(beforeFinally, discardEventsAfterCancel, false);
+    public AfterFinallyHttpOperator(final TerminalSignalConsumer afterFinally, boolean discardEventsAfterCancel) {
+        super(afterFinally, discardEventsAfterCancel, true);
     }
 }

servicetalk-http-utils/src/test/java/io/servicetalk/http/utils/AfterFinallyHttpOperatorTckTest.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright © 2025 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.utils;
+
+import io.servicetalk.buffer.api.Buffer;
+import io.servicetalk.concurrent.api.Publisher;
+import io.servicetalk.concurrent.api.Single;
+import io.servicetalk.concurrent.reactivestreams.tck.AbstractPublisherOperatorTckTest;
+import io.servicetalk.http.api.DefaultHttpHeadersFactory;
+import io.servicetalk.http.api.StreamingHttpResponse;
+import io.servicetalk.http.api.StreamingHttpResponses;
+
+import org.testng.annotations.Test;
+
+import static io.servicetalk.buffer.netty.BufferAllocators.DEFAULT_ALLOCATOR;
+import static io.servicetalk.http.api.HttpProtocolVersion.HTTP_1_1;
+import static io.servicetalk.http.api.HttpResponseStatus.OK;
+
+@Test
+public class AfterFinallyHttpOperatorTckTest extends AbstractPublisherOperatorTckTest<Buffer> {
+
+    @Override
+    protected Publisher<Buffer> composePublisher(final Publisher<Integer> publisher, final int elements) {
+        return Single.succeeded(StreamingHttpResponses.newResponse(OK, HTTP_1_1,
+                                DefaultHttpHeadersFactory.INSTANCE.newHeaders(), DEFAULT_ALLOCATOR,
+                                DefaultHttpHeadersFactory.INSTANCE)
+                        .payloadBody(publisher.map(Integer::toHexString).map(DEFAULT_ALLOCATOR::fromAscii)))
+                .liftSync(new AfterFinallyHttpOperator(() -> { /* noop */ }))
+                .flatMapPublisher(StreamingHttpResponse::payloadBody);
+    }
+}

servicetalk-http-utils/src/test/java/io/servicetalk/http/utils/AfterFinallyHttpOperatorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019-2025 Apple Inc. and the ServiceTalk project authors
+ * Copyright © 2025 Apple Inc. and the ServiceTalk project authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +19,10 @@ import io.servicetalk.concurrent.api.SingleOperator;
 import io.servicetalk.concurrent.api.TerminalSignalConsumer;
 import io.servicetalk.http.api.StreamingHttpResponse;
 
-class BeforeFinallyHttpOperatorTest extends FinallyHttpOperatorTest {
+class AfterFinallyHttpOperatorTest extends FinallyHttpOperatorTest {
     @Override
     protected SingleOperator<StreamingHttpResponse, StreamingHttpResponse> newWhenFinallyOperator(
             TerminalSignalConsumer whenFinally, boolean discardEventsAfterCancel) {
-        return new BeforeFinallyHttpOperator(whenFinally, discardEventsAfterCancel);
+        return new AfterFinallyHttpOperator(whenFinally, discardEventsAfterCancel);
     }
 }

commit 7f2c5ea38c9f3052698774e45558e11e7341f3a7

`BeforeFinallyHttpOperator`: restore binary compatibility
Motivation:

Its binary compatibility was broken in #3231.

Modifications:

- Remove `final` modifier from `apply` method of the super class.
- Make its super class `abstract`.

Result:

`japicmp` is happy.

servicetalk-http-utils/src/main/java/io/servicetalk/http/utils/AbstractWhenFinallyHttpOperator.java
@@ -34,21 +34,22 @@ import static io.servicetalk.utils.internal.ThrowableUtils.addSuppressed;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.atomic.AtomicIntegerFieldUpdater.newUpdater;
 
-class WhenFinallyHttpOperator implements SingleOperator<StreamingHttpResponse, StreamingHttpResponse> {
+abstract class AbstractWhenFinallyHttpOperator implements SingleOperator<StreamingHttpResponse, StreamingHttpResponse> {
 
     private final TerminalSignalConsumer whenFinally;
     private final boolean discardEventsAfterCancel;
     private final boolean afterFinallyBehavior;
 
-    WhenFinallyHttpOperator(final TerminalSignalConsumer whenFinally, boolean discardEventsAfterCancel,
-                            boolean afterFinallyBehavior) {
+    AbstractWhenFinallyHttpOperator(final TerminalSignalConsumer whenFinally, boolean discardEventsAfterCancel,
+                                    boolean afterFinallyBehavior) {
         this.whenFinally = requireNonNull(whenFinally);
         this.discardEventsAfterCancel = discardEventsAfterCancel;
         this.afterFinallyBehavior = afterFinallyBehavior;
     }
 
     @Override
-    public final SingleSource.Subscriber<? super StreamingHttpResponse> apply(
+    // FIXME: 0.43 - consider making this method final (breaks binary compatibility)
+    public SingleSource.Subscriber<? super StreamingHttpResponse> apply(
             final SingleSource.Subscriber<? super StreamingHttpResponse> subscriber) {
         // Here we avoid calling beforeFinally#onCancel when we get a cancel() on the Single after we have handed out
         // the StreamingHttpResponse to the subscriber. In case, we do get an StreamingHttpResponse after we got
@@ -274,8 +275,7 @@ class WhenFinallyHttpOperator implements SingleOperator<StreamingHttpResponse, S
                 }
 
                 private void tryWhenFinallyCancel() {
-                    if (stateUpdater.compareAndSet(WhenFinallyHttpOperator.MessageBodySubscriber.this,
-                            PROCESSING_PAYLOAD, TERMINATED)) {
+                    if (stateUpdater.compareAndSet(MessageBodySubscriber.this, PROCESSING_PAYLOAD, TERMINATED)) {
                         whenFinally.cancel();
                     }
                 }
@@ -300,13 +300,13 @@ class WhenFinallyHttpOperator implements SingleOperator<StreamingHttpResponse, S
                     }
 
                     for (;;) {
-                        final int state = WhenFinallyHttpOperator.MessageBodySubscriber.this.state;
+                        final int state = MessageBodySubscriber.this.state;
                         if (state == PROCESSING_PAYLOAD) {
                             if (tryCancelFromState(PROCESSING_PAYLOAD)) {
                                 break;
                             }
                         } else if (state == DELIVERING_PAYLOAD) {
-                            if (stateUpdater.compareAndSet(WhenFinallyHttpOperator.MessageBodySubscriber.this,
+                            if (stateUpdater.compareAndSet(MessageBodySubscriber.this,
                                     DELIVERING_PAYLOAD, AWAITING_CANCEL)) {
                                 break;
                             }
@@ -473,8 +473,7 @@ class WhenFinallyHttpOperator implements SingleOperator<StreamingHttpResponse, S
         }
 
         private boolean tryCancelFromState(final int expectedState) {
-            if (stateUpdater.compareAndSet(WhenFinallyHttpOperator.MessageBodySubscriber.this,
-                    expectedState, TERMINATED)) {
+            if (stateUpdater.compareAndSet(this, expectedState, TERMINATED)) {
                 assert subscription != null;
                 if (afterFinallyBehavior) {
                     try {

servicetalk-http-utils/src/main/java/io/servicetalk/http/utils/AfterFinallyHttpOperator.java
@@ -53,7 +53,7 @@ import io.servicetalk.http.api.StreamingHttpResponse;
  *
  * @see BeforeFinallyHttpOperator
  */
-public final class AfterFinallyHttpOperator extends WhenFinallyHttpOperator {
+public final class AfterFinallyHttpOperator extends AbstractWhenFinallyHttpOperator {
 
     /**
      * Create a new instance.

servicetalk-http-utils/src/main/java/io/servicetalk/http/utils/BeforeFinallyHttpOperator.java
@@ -53,7 +53,7 @@ import io.servicetalk.http.api.StreamingHttpResponse;
  *
  * @see AfterFinallyHttpOperator
  */
-public final class BeforeFinallyHttpOperator extends WhenFinallyHttpOperator {
+public final class BeforeFinallyHttpOperator extends AbstractWhenFinallyHttpOperator {
 
     /**
      * Create a new instance.