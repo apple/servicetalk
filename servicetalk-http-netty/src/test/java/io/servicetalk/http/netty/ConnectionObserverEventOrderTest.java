1f64e6e91fa298795bd7f5b50ff36874fb01417b
Bryce Anderson <bl_anderson@apple.com>
2025-04-29 15:25:17 -0600

    transport-netty-internal: properly order observer events if connection establishment fails (#3238)
    Motivation:
    
    When establishing a TCP connection, we add a listener to the `closeFuture()`
    of the channel to let us know when close events happen. This works fine
    until we consider the tcp connect: it turns out that the connect future
    listeners will typically be added before the attachment of channel
    closure-on-failure. This cases the Subscriber.onError pathway to be
    triggered before the ConnectionObserver.connectionClosed(failed)
    pathway because the former has its subscriber added to the promise
    first.
    
    Modifications:
    
    Refactor the code to consider the connect pathway and add an early
    listener to the promise that will trigger the connectionClosed(..)
    callback eagerly. If the connection succeeds we then attach a listener
    to the `closeFuture()` as usual.

---
+++ servicetalk-http-netty/src/test/java/io/servicetalk/http/netty/ConnectionObserverEventOrderTest.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright © 2025 Apple Inc. and the ServiceTalk project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.servicetalk.http.netty;
+
+import io.servicetalk.client.api.TransportObserverConnectionFactoryFilter;
+import io.servicetalk.http.api.BlockingHttpClient;
+import io.servicetalk.http.api.HttpLifecycleObserver;
+import io.servicetalk.http.api.HttpRequestMetaData;
+import io.servicetalk.http.api.HttpResponseMetaData;
+import io.servicetalk.transport.api.ConnectionInfo;
+import io.servicetalk.transport.api.ConnectionObserver;
+import io.servicetalk.transport.api.ServerContext;
+import io.servicetalk.transport.api.TransportObserver;
+import io.servicetalk.transport.netty.internal.ExecutionContextExtension;
+import io.servicetalk.transport.netty.internal.NoopTransportObserver;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import javax.annotation.Nullable;
+
+import static io.servicetalk.http.api.HttpSerializers.textSerializerUtf8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConnectionObserverEventOrderTest {
+
+    @RegisterExtension
+    static final ExecutionContextExtension EXECUTION_CONTEXT =
+            ExecutionContextExtension.cached("server-io", "server-executor")
+                    .setClassLevel(true);
+
+    private final BlockingQueue<String> eventQueue = new LinkedBlockingQueue<>();
+
+    @Test
+    void repro() throws Exception {
+        final ServerContext serverContext = BuilderUtils.newServerBuilder(EXECUTION_CONTEXT)
+                .listenBlockingAndAwait((ctx, request, responseFactory) ->
+                        responseFactory.ok().payloadBody("Test server response", textSerializerUtf8()));
+        assertThrows(Exception.class, () -> {
+            try (BlockingHttpClient client = BuilderUtils.newClientBuilder(serverContext, EXECUTION_CONTEXT)
+                    .appendConnectionFactoryFilter(new TransportObserverConnectionFactoryFilter<>(
+                            new TestTransportObserver()))
+                    .appendClientFilter(new HttpLifecycleObserverRequesterFilter(new HttpLifecycleObserverImpl()))
+                    .appendClientFilter(new RetryingHttpRequesterFilter.Builder()
+                            .retryRetryableExceptions((req, ex) ->
+                                    RetryingHttpRequesterFilter.BackOffPolicy.ofNoRetries())
+                            .build())
+                    .buildBlocking()) {
+                serverContext.close(); // causes connection establishment to fail.
+                client.request(client.get("/sayHello"));
+            }
+        });
+
+        List<String> expectedEvents = Arrays.asList("onNewExchange", "onRequest", "onNewConnection",
+                "connectionClosed", "onResponseError", "onExchangeFinally");
+        for (String expected : expectedEvents) {
+            assertEquals(expected, eventQueue.take());
+        }
+    }
+
+    private final class HttpLifecycleObserverImpl implements HttpLifecycleObserver {
+        @Override
+        public HttpExchangeObserver onNewExchange() {
+            addEvent();
+            return new HttpExchangeObserver() {
+                @Override
+                public void onConnectionSelected(ConnectionInfo info) {
+                    addEvent();
+                }
+
+                @Override
+                public HttpRequestObserver onRequest(HttpRequestMetaData requestMetaData) {
+                    addEvent();
+                    return NoopHttpLifecycleObserver.NoopHttpRequestObserver.INSTANCE;
+                }
+
+                @Override
+                public HttpResponseObserver onResponse(HttpResponseMetaData responseMetaData) {
+                    addEvent();
+                    return NoopHttpLifecycleObserver.NoopHttpResponseObserver.INSTANCE;
+                }
+
+                @Override
+                public void onResponseError(Throwable cause) {
+                    addEvent();
+                }
+
+                @Override
+                public void onResponseCancel() {
+                    addEvent();
+                }
+
+                @Override
+                public void onExchangeFinally() {
+                    addEvent();
+                }
+            };
+        }
+    }
+
+    private final class TestTransportObserver implements TransportObserver {
+
+        @Override
+        public ConnectionObserver onNewConnection(@Nullable Object localAddress, Object remoteAddress) {
+            addEvent();
+            return new ConnectionObserverImpl();
+        }
+
+        private final class ConnectionObserverImpl implements ConnectionObserver {
+            @Override
+            public void onDataRead(int size) {
+                addEvent();
+            }
+
+            @Override
+            public void onDataWrite(int size) {
+                addEvent();
+            }
+
+            @Override
+            public void onFlush() {
+                addEvent();
+            }
+
+            @Override
+            public DataObserver connectionEstablished(ConnectionInfo info) {
+                addEvent();
+                return NoopTransportObserver.NoopDataObserver.INSTANCE;
+            }
+
+            @Override
+            public MultiplexedObserver multiplexedConnectionEstablished(ConnectionInfo info) {
+                addEvent();
+                return NoopTransportObserver.NoopMultiplexedObserver.INSTANCE;
+            }
+
+            @Override
+            public void connectionClosed(Throwable error) {
+                addEvent();
+            }
+
+            @Override
+            public void connectionClosed() {
+                addEvent();
+            }
+        }
+    }
+
+    private void addEvent() {
+        eventQueue.add(new Exception().getStackTrace()[2].getMethodName());
+    }
+}
--- servicetalk-transport-netty-internal/src/main/java/io/servicetalk/transport/netty/internal/ConnectionObserverInitializer.java
+++ servicetalk-transport-netty-internal/src/main/java/io/servicetalk/transport/netty/internal/ConnectionObserverInitializer.java
@@ -25,6 +25,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufHolder;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
@@ -109,14 +110,6 @@ public final class ConnectionObserverInitializer implements ChannelInitializer {
 
     @Override
     public void init(final Channel channel) {
-        channel.closeFuture().addListener((ChannelFutureListener) future -> {
-            Throwable t = channelError(channel);
-            if (t == null) {
-                observer.connectionClosed();
-            } else {
-                observer.connectionClosed(t);
-            }
-        });
         channel.pipeline().addLast(new ConnectionObserverHandler(observer, connectionInfoFactory,
                 sslConfig != null, isFastOpen(channel), sslConfig));
     }
@@ -132,6 +125,7 @@ public final class ConnectionObserverInitializer implements ChannelInitializer {
         private final Function<Channel, ConnectionInfo> connectionInfoFactory;
         private final boolean handshakeOnActive;
         private boolean tcpHandshakeComplete;
+        private boolean addedCloseListener;
         @Nullable
         private SecurityHandshakeObserver handshakeObserver;
         @Nullable
@@ -155,20 +149,66 @@ public final class ConnectionObserverInitializer implements ChannelInitializer {
         public void handlerAdded(final ChannelHandlerContext ctx) {
             final Channel channel = ctx.channel();
             if (channel.isActive()) {
-                reportTcpHandshakeComplete(channel);
-                if (handshakeOnActive) {
-                    reportSecurityHandshakeStarting(sslConfig);
-                }
+                whenChannelActive(channel);
             }
         }
 
         @Override
+        public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress,
+                            ChannelPromise promise) throws Exception {
+            // The connect promise is the first to be notified of problems in the connect phase, even before the
+            // channel.closeFuture(). We typically want to know about closure before we get to returning the failed
+            // response because it is the cause of the failed response, so we eagerly propagate that in the event of
+            // a failed connect promise. We add our listener to the promise before forwarding the call so we can be
+            // sure this callback is fired early in the failure pathway.
+            promise.addListener((ChannelFuture future) -> {
+                if (future.isSuccess()) {
+                    maybeAddChannelClosedListener(ctx.channel());
+                } else {
+                    assert ctx.channel().eventLoop().inEventLoop();
+                    addedCloseListener = true;
+                    Throwable t = channelError(future.channel());
+                    if (t == null) {
+                        t = future.cause();
+                    }
+                    if (t == null) {
+                        observer.connectionClosed();
+                    } else {
+                        observer.connectionClosed(t);
+                    }
+                }
+            });
+            super.connect(ctx, remoteAddress, localAddress, promise);
+        }
+
+        @Override
         public void channelActive(final ChannelHandlerContext ctx) {
-            reportTcpHandshakeComplete(ctx.channel());
+            whenChannelActive(ctx.channel());
+            ctx.fireChannelActive();
+        }
+
+        private void whenChannelActive(final Channel channel) {
+            maybeAddChannelClosedListener(channel);
+            reportTcpHandshakeComplete(channel);
             if (handshakeOnActive) {
                 reportSecurityHandshakeStarting(sslConfig);
             }
-            ctx.fireChannelActive();
+        }
+
+        private void maybeAddChannelClosedListener(Channel channel) {
+            assert channel.eventLoop().inEventLoop();
+            if (addedCloseListener) {
+                return;
+            }
+            addedCloseListener = true;
+            channel.closeFuture().addListener((ChannelFutureListener) future -> {
+                Throwable t = channelError(channel);
+                if (t == null) {
+                    observer.connectionClosed();
+                } else {
+                    observer.connectionClosed(t);
+                }
+            });
         }
 
         private void reportTcpHandshakeComplete(final Channel channel) {
