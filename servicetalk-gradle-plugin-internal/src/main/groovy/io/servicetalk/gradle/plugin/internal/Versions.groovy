8d1b56c09d9b473367ff4fef002796376100c2ca
Michael Nitschinger <mnitschinger@apple.com>
2026-03-17 09:47:14 +0100

    Add on-demand support for jacoco code coverage

--- servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
+++ servicetalk-gradle-plugin-internal/src/main/groovy/io/servicetalk/gradle/plugin/internal/Versions.groovy
@@ -21,6 +21,7 @@ import static org.gradle.api.JavaVersion.VERSION_1_8
 
 final class Versions {
   static final String CHECKSTYLE_VERSION = "9.2"
+  static final String JACOCO_VERSION = "0.8.14"
   static final String PMD_VERSION = "6.55.0"
   static final String SPOTBUGS_VERSION = "4.9.8"
   static final JavaVersion TARGET_VERSION = VERSION_1_8
