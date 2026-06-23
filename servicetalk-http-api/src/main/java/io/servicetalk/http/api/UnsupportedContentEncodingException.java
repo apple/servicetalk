d6fd9718a8d3e1fe67c24e36e929ef8d9029a886
Idel Pivnitskiy <ipivnitskiy@apple.com>
2026-06-23 15:30:30 -0700

    Make `UnsupportedContentEncodingException` public
    Motivation:
    
    All other `SerializationException`(s) are public. Currently, users can
    not read its `encoding()` field because the class is pkg-private.

--- servicetalk-http-api/src/main/java/io/servicetalk/http/api/UnsupportedContentEncodingException.java
+++ servicetalk-http-api/src/main/java/io/servicetalk/http/api/UnsupportedContentEncodingException.java
@@ -20,7 +20,7 @@ import io.servicetalk.serializer.api.SerializationException;
 /**
  * Exception thrown when a payload was encoded with an unsupported encoder.
  */
-final class UnsupportedContentEncodingException extends SerializationException {
+public final class UnsupportedContentEncodingException extends SerializationException {
 
     private static final long serialVersionUID = 5645078707423180235L;
 
